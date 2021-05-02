4240958f39d798a213e4790eaa26c2e8c10c9448
Richard <richard.yang.tong@gmail.com>
2021-05-02 13:55:56 -0400

    Added notes.

---
+++ Day_02/Notes/src/com/dragontalker/BinaryTest.java
@@ -0,0 +1,15 @@
+package com.dragontalker;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        int num1 = 0b110;
+        int num2 = 110;
+        int num3 = 0127;
+        int num4 = 0x110A;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("num3 = " + num3);
+        System.out.println("num4 = " + num4);
+    }
+}
--- Day_02/Notes/src/com/dragontalker/Main.java
+++ Day_02/Notes/src/com/dragontalker/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         String s1 = "Hello World!";
         System.out.println(s1);
-        
+
     }
 }
