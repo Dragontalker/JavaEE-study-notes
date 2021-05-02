9cfdec5b3220e21c477bc04c382b719a6204c0db
Richard <richard.yang.tong@gmail.com>
2021-05-02 14:58:20 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/AriTest.java
+++ Day_03/Notes/src/com/dragontalker/AriTest.java
@@ -3,6 +3,14 @@ package com.dragontalker;
 public class AriTest {
 
     public static void main(String[] args) {
-        
+
+        // division:
+        int num1 = 12;
+        int num2 = 5;
+        int result1 = num1 / num2;
+        System.out.println(result1); // 2
+
+        int result2 = num1 / num2 * num2;
+        System.out.println(result2); // 10
     }
 }
