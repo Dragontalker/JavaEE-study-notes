36d9dfc5804f0ac3087132361c2675d011057d12
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:58:03 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/BitTest.java
+++ Day_03/Notes/src/com/dragontalker/BitTest.java
@@ -7,5 +7,7 @@ public class BitTest {
         int i = 21;
         System.out.println("i << 2 : " + (i << 2)); // 21 * 2 * 2 = 84
         System.out.println("i << 3 : " + (i << 3)); // 21 * 2 * 2 * 2 = 168
+        System.out.println("i << 26 : " + (i << 26));
+        System.out.println("i << 27 : " + (i << 27)); // becomes negative
     }
 }
