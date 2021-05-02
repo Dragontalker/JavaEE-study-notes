a55f0d88e6ca3e13477053e5fe99005178dac51d
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:56:05 -0400

    Added notes.

---
+++ Day_03/Notes/src/com/dragontalker/BitTest.java
@@ -0,0 +1,10 @@
+package com.dragontalker;
+
+public class BitTest {
+
+    public static void main(String[] args) {
+
+        int i = 21;
+        System.out.println("i << 2 : " + (i << 2));
+    }
+}
