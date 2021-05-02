433b1123876e04649956a77587dfc5a930141354
Richard <richard.yang.tong@gmail.com>
2021-05-02 15:02:56 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/AriTest.java
+++ Day_03/Notes/src/com/dragontalker/AriTest.java
@@ -21,5 +21,8 @@ public class AriTest {
 
         double result5 = num1 / (num2 + 0.0);
         System.out.println(result5); // 2.4
+
+        double result6 = (double)num1 / num2;
+        System.out.println(result6); // 2.4
     }
 }
