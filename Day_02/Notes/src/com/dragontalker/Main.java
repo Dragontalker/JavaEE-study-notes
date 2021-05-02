02bdfc4f235ff68feea9aaa0039b3c5b819d6d21
Richard <richard.yang.tong@gmail.com>
2021-05-02 11:00:39 -0400

    Added notes.

--- Day_02/Notes/src/com/dragontalker/Main.java
+++ Day_02/Notes/src/com/dragontalker/Main.java
@@ -3,17 +3,9 @@ package com.dragontalker;
 public class Main {
 
     public static void main(String[] args) {
-
-        // 变量的定义
-        int myAge = 12;
-        // 变量的使用
-        System.out.println(myAge);
-
-        // 变量的声明
-        int myNumber;
-        // 变量的赋值
-        myNumber = 1001;
-
-        System.out.println(myNumber);
+        byte b1 = 2;
+        int i1 = 12;
+        // the sum should be int
+        int sum1 = b1 + i1;
     }
 }
