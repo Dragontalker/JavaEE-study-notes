c1e9b7ceb1d2ba0668e48b61061d2c4313ec8c54
Richard <richard.yang.tong@gmail.com>
2021-05-02 18:01:08 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/BitTest.java
+++ Day_03/Notes/src/com/dragontalker/BitTest.java
@@ -4,6 +4,10 @@ public class BitTest {
 
     public static void main(String[] args) {
 
+        // 结论：
+        // 1. 位运算符操作的都是整形的数据
+        // 2. << : 在一定范围内， 每向左移1位，相当于 * 2
+        //    >> : 在一定范围内， 每向右移1位， 相当于 / 2
         int i = 21;
         System.out.println("i << 2 : " + (i << 2)); // 21 * 2 * 2 = 84
         System.out.println("i << 3 : " + (i << 3)); // 21 * 2 * 2 * 2 = 168
