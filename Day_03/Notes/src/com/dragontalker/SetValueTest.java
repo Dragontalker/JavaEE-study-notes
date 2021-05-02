4c4a4cada4e8f7a9ba51e0013c4dbb116e593f48
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:05:50 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/SetValueTest.java
+++ Day_03/Notes/src/com/dragontalker/SetValueTest.java
@@ -1,4 +1,15 @@
 package com.dragontalker;
 
 public class SetValueTest {
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int j1 = 10;
+
+        int i2, j2;
+        i2 = j2 = 10;
+
+        int i3 = 10, j3 = 10;
+    }
 }
