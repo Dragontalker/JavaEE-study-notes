d4872d48b32e0e3d2b8b6ea12394a10c3f623b34
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:57:08 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/BitTest.java
+++ Day_03/Notes/src/com/dragontalker/BitTest.java
@@ -5,6 +5,7 @@ public class BitTest {
     public static void main(String[] args) {
 
         int i = 21;
-        System.out.println("i << 2 : " + (i << 2));
+        System.out.println("i << 2 : " + (i << 2)); // 21 * 2 * 2 = 84
+        System.out.println("i << 3 : " + (i << 3)); // 21 * 2 * 2 * 2 = 168
     }
 }
