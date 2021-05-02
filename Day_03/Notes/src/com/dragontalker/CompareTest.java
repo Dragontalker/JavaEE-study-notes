32ac82b267b48d16d55dbd2575e1991c5f98eb7c
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:22:40 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/CompareTest.java
+++ Day_03/Notes/src/com/dragontalker/CompareTest.java
@@ -1,4 +1,11 @@
 package com.dragontalker;
 
 public class CompareTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        System.out.println(i == j); // false
+        System.out.println(i = j); // 20
+    }
 }
