f96b182ddec21fb851e6d6a0941ffcc5b65d56e3
Richard <richard.yang.tong@gmail.com>
2021-05-02 15:07:33 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/AriTest.java
+++ Day_03/Notes/src/com/dragontalker/AriTest.java
@@ -33,5 +33,19 @@ public class AriTest {
         int m1 = 12;
         int n1 = 5;
         System.out.println("m1 % n1 = " + m1 % n1); // 2
+
+        int m2 = -12;
+        int n2 = 5;
+        System.out.println("m2 % n2 = " + m2 % n2); // -2
+
+        int m3 = 12;
+        int n3 = -5;
+        System.out.println("m3 % n3 = " + m3 % n3); // 2
+
+        int m4 = -12;
+        int n4 = -5;
+        System.out.println("m4 % n4 = " + m4 % n4); // -2
+
+
     }
 }
