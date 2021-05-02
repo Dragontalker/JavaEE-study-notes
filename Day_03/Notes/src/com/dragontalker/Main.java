73857c3290e082bb9945b61ded04773f8b148a89
Richard <richard.yang.tong@gmail.com>
2021-05-02 16:58:08 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/Main.java
+++ Day_03/Notes/src/com/dragontalker/Main.java
@@ -3,6 +3,17 @@ package com.dragontalker;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Exercise
+        int num = 187;
+        int baiWei = num / 100;
+        System.out.println("百位为" + baiWei);
+
+        int shiWei = num % 100 / 10;
+        System.out.println("十位为" + shiWei );
+
+        int geWei = num % 10;
+        System.out.println("个位为" + geWei);
+
     }
 }
