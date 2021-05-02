a0c9d19a3a2415d530ff0d1c880fda70ca877eaf
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:36:38 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/LogicTest.java
+++ Day_03/Notes/src/com/dragontalker/LogicTest.java
@@ -5,6 +5,9 @@ public class LogicTest {
     public static void main(String[] args) {
 
         // 区分& 与 &&
+        // 相同点1：& 与 && 的运算结果相同
+        // 相同点2：当符号左边是true时，二者都会执行符号右边的运算
+        // 不同点：当符号左边是false时，&继续执行符号右边的运算， &&不再执行符号右边的运算
         boolean b1 = true;
         int num1 = 10;
         if(b1 & (num1++ > 0)) {
@@ -15,7 +18,7 @@ public class LogicTest {
 
         boolean b2 = false;
         int num2 = 10;
-        if(b2 & (num2++ > 0)) {
+        if(b2 && (num2++ > 0)) {
             System.out.println("我现在在北京");
         } else {
             System.out.println("我现在在南京");
