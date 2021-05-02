b052b78658113a52c72994264e0ae7b0ccf5bbdc
Richard <richard.yang.tong@gmail.com>
2021-05-02 17:04:45 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/Main.java
+++ Day_03/Notes/src/com/dragontalker/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("百位为" + baiWei);
 
         int shiWei = num % 100 / 10;
-        System.out.println("十位为" + shiWei );
+        System.out.println("十位为" + shiWei);
 
         int geWei = num % 10;
         System.out.println("个位为" + geWei);
