bcc1afa6692307dfa8738b533f6fbe3b12819fac
Richard <richard.yang.tong@gmail.com>
2021-05-02 11:31:05 -0400

    Added notes.

--- Day_02/Notes/src/com/dragontalker/Main.java
+++ Day_02/Notes/src/com/dragontalker/Main.java
@@ -3,15 +3,8 @@ package com.dragontalker;
 public class Main {
 
     public static void main(String[] args) {
-        byte b1 = 2;
-        int i1 = 12;
-        // the sum should be int
-        // this won't work: byte b2 = b1 + i1;
-        int i2 = b1 + i1;
-        System.out.println(i2);
-
-        float f = b1 + i1;
-        System.out.println(f);
-
+        String s1 = "Hello World!";
+        System.out.println(s1);
+        
     }
 }
