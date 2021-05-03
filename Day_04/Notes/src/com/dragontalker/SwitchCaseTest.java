077e80d74f1a21b49bd4077b8afcd969de2e28d0
Richard <richard.yang.tong@gmail.com>
2021-05-03 10:34:49 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
+++ Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
@@ -22,6 +22,9 @@ switch(表达式) {
 2. 说明:
     a. 根据switch表达式中的值，依次匹配各个case中的常量。一旦匹配成功，则进入相应的case结构中，调用其执行语句。
         当调用完执行语句以后，则仍然继续向下执行，直到遇到break关键字。
+    b. break, 可以使用在switch-case结构中，表示一旦执行到此关键字，就跳出switch-case结构
+    c. switch结构中的表达式，只能是如下的6种数据类型之一：
+        byte, short, char, int, 枚举类型(jdk5.0新增)， string类型(jdk7.0新增)
 */
 
 public class SwitchCaseTest {
