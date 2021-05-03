d730c85eae8d370e7bf31e9529fa2aef2613d7be
Richard <richard.yang.tong@gmail.com>
2021-05-02 21:00:05 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/TernaryTest.java
+++ Day_03/Notes/src/com/dragontalker/TernaryTest.java
@@ -7,5 +7,14 @@ public class TernaryTest {
         int n = 5;
         int max = (m > n) ? m : n;
         System.out.println(max);
+
+        // 获取三个数的最大值
+        int n1 = 12;
+        int n2 = 30;
+        int n3 = -43;
+
+        int max1 = (n1 > n2) ? n1 : n2;
+        int max2 = (max1 > n3) ? max1 : n3;
+        System.out.println("三个数中的最大值为: " + max2);
     }
 }
