44020dacd73853a89b51cef3336b516174c39209
Richard <richard.yang.tong@gmail.com>
2021-05-03 09:14:46 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ScannerTest.java
+++ Day_04/Notes/src/com/dragontalker/ScannerTest.java
@@ -15,7 +15,20 @@ public class ScannerTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        System.out.println(num);
+        System.out.println("请输入你的姓名：");
+        String name = scan.next();
+        System.out.println(name);
+
+        System.out.println("请输入你的芳龄：");
+        int age = scan.nextInt();
+        System.out.println(age);
+
+        System.out.println("请输入你的体重：");
+        double weight = scan.nextDouble();
+        System.out.println(weight);
+
+        System.out.println("你是否单身？");
+        boolean isSingle = scan.nextBoolean();
+        System.out.println(isSingle);
     }
 }
