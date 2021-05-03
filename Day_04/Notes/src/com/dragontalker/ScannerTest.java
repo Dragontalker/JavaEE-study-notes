86ca540908d7021799b7da6b3d2d7785465c6d3f
Richard <richard.yang.tong@gmail.com>
2021-05-03 09:27:13 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ScannerTest.java
+++ Day_04/Notes/src/com/dragontalker/ScannerTest.java
@@ -7,6 +7,10 @@ package com.dragontalker;
 1. 导包： import java.util.Scanner;
 2. Scanner的实例化
 3. 调用Scanner类的相关方法( next() / nextXxx())， 来获取指定类型的变量
+
+注意：
+需要根据相应的方法，来输入指定类型的值。如果输入的数据类型与要求的类型不匹配时，
+会报异常：InputMismatch导致程序终止。
 */
 
 // 1. 导包： import java.util.Scanner;
