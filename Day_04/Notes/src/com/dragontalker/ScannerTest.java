96204bbe8da80bef33f345bee8f70e5543e2fa4f
Richard <richard.yang.tong@gmail.com>
2021-05-03 09:09:26 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ScannerTest.java
+++ Day_04/Notes/src/com/dragontalker/ScannerTest.java
@@ -15,5 +15,7 @@ public class ScannerTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(num);
     }
 }
