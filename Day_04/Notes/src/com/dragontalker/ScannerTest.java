9a9b69df4918a4b55eeea1436c6ff1222b48dff0
Richard <richard.yang.tong@gmail.com>
2021-05-03 09:07:14 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ScannerTest.java
+++ Day_04/Notes/src/com/dragontalker/ScannerTest.java
@@ -1,5 +1,14 @@
 package com.dragontalker;
 
+/*
+如何从键盘获取不同类型的变量：需要使用Scanner类
+
+具体实现步骤：
+1. 导包： import java.util.Scanner;
+2. Scanner的实例化
+
+*/
+
 public class ScannerTest {
 
     public static void main(String[] args) {
