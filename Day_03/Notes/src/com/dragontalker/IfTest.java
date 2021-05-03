f0b66145bf63532ac3db86ed88f6efa8f25847ea
Richard <richard.yang.tong@gmail.com>
2021-05-02 21:46:26 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/IfTest.java
+++ Day_03/Notes/src/com/dragontalker/IfTest.java
@@ -19,5 +19,20 @@ public class IfTest {
         } else {
             System.out.println("你可以看成人电影了");
         }
+
+        // 举例3
+        if (age < 0) {
+            System.out.println("您输入的数据非法");
+        } else if (age < 18) {
+            System.out.println("青少年时期");
+        } else if (age < 35) {
+            System.out.println("青壮年时期");
+        } else if (age < 60) {
+            System.out.println("中年时期");
+        } else if (age < 120){
+            System.out.println("老年时期");
+        } else {
+            System.out.println("你是要成仙啊~~");
+        }
     }
 }
