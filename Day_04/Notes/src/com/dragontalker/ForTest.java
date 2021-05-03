1c12b119a3fef21ae4acc19d1b3553ec4fdd05ab
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:56:13 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ForTest.java
+++ Day_04/Notes/src/com/dragontalker/ForTest.java
@@ -7,5 +7,18 @@ public class ForTest {
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello world!");
         }
+
+        // exercise
+        int num = 1;
+        for(System.out.print('a'); num <= 3; System.out.print('c'), num++) {
+            System.out.print('b');
+        }
+
+        // 例题: 遍历100以内的偶数
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
