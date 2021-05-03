81675bfaae4cbe6998959ba4fcec6cea67b3a4bc
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:51:01 -0400

    Added notes.

---
+++ Day_04/Notes/src/com/dragontalker/ForTest.java
@@ -0,0 +1,11 @@
+package com.dragontalker;
+
+public class ForTest {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello world!");
+        }
+    }
+}
