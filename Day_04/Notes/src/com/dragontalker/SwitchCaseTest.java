0ac175484523cbc302af66eadd7e32a40e2ef609
Richard <richard.yang.tong@gmail.com>
2021-05-03 10:14:41 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
+++ Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
@@ -18,6 +18,10 @@ switch(表达式) {
         执行语句n;
         // break;
 }
+
+2. 说明:
+    a. 根据switch表达式中的值，依次匹配各个case中的常量。一旦匹配成功，则进入相应的case结构中，调用其执行语句。
+        当调用完执行语句以后，则仍然继续向下执行，直到遇到break关键字。
 */
 
 public class SwitchCaseTest {
@@ -28,12 +32,16 @@ public class SwitchCaseTest {
         switch(number) {
             case 0:
                 System.out.println("zero");
+                break;
             case 1:
                 System.out.println("one");
+                break;
             case 2:
                 System.out.println("two");
+                break;
             case 3:
                 System.out.println("three");
+                break;
             default:
                 System.out.println("other");
         }
