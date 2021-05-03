a8b21756fe72ac7f1c9f376836276e13f9378dda
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:13:26 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/SwitchTest2.java
+++ Day_04/Notes/src/com/dragontalker/SwitchTest2.java
@@ -13,5 +13,34 @@ public class SwitchTest2 {
 
         System.out.println("请输入2019年的day");
         int day = scan.nextInt();
+
+        int sumDays = 0;
+        switch (month) {
+            case 12:
+                sumDays += 30;
+            case 11:
+                sumDays += 31;
+            case 10:
+                sumDays += 31;
+            case 9:
+                sumDays += 30;
+            case 8:
+                sumDays += 31;
+            case 7:
+                sumDays += 31;
+            case 6:
+                sumDays += 30;
+            case 5:
+                sumDays += 31;
+            case 4:
+                sumDays += 30;
+            case 3:
+                sumDays += 31;
+            case 2:
+                sumDays += 28;
+            case 1:
+                sumDays += day;
+        }
+        System.out.println("2019年" + month + "月" + day + "日是当年的第" + sumDays + "天");
     }
 }
