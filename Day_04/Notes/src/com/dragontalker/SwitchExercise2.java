7cf3dcc22f68d022cb4f76bc465f085682ffac3f
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:41:30 -0400

    Added notes.

---
+++ Day_04/Notes/src/com/dragontalker/SwitchExercise2.java
@@ -0,0 +1,14 @@
+package com.dragontalker;
+
+import java.util.Scanner;
+
+public class SwitchExercise2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入学生成绩");
+        int score = scan.nextInt();
+
+    }
+}
