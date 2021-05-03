f502c1affe6feadd11975ecf61447090b5e154b3
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:20:38 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/SwitchTest3.java
+++ Day_04/Notes/src/com/dragontalker/SwitchTest3.java
@@ -6,5 +6,10 @@ public class SwitchTest3 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("请输入年份");
+        int year = scan.nextInt();
+
+
     }
 }
