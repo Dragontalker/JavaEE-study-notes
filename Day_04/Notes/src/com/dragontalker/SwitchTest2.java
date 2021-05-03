1a9ed0bab3ab6437d6018090c7ee4b28947b48d3
Richard <richard.yang.tong@gmail.com>
2021-05-03 10:56:16 -0400

    Added notes.

---
+++ Day_04/Notes/src/com/dragontalker/SwitchTest2.java
@@ -0,0 +1,17 @@
+package com.dragontalker;
+
+import java.util.Scanner;
+
+public class SwitchTest2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("请输入2019年的month");
+        int month = scan.nextInt();
+
+        System.out.println("请输入2019年的day");
+        int day = scan.nextInt();
+    }
+}
