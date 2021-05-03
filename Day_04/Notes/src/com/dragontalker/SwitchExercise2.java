7e5cd414746f2b2de28d4df3cd8cd4c6dc380a4d
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:47:44 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/SwitchExercise2.java
+++ Day_04/Notes/src/com/dragontalker/SwitchExercise2.java
@@ -10,5 +10,22 @@ public class SwitchExercise2 {
         System.out.println("请输入学生成绩");
         int score = scan.nextInt();
 
+        int rank = score / 10;
+        switch (rank) {
+            case 10:
+                System.out.println("等级: S");
+                break;
+            case 9:
+                System.out.println("等级: A");
+                break;
+            case 7:
+                System.out.println("等级: B");
+                break;
+            case 6:
+                System.out.println("等级: C");
+                break;
+            default:
+                System.out.println("等级: D");
+        }
     }
 }
