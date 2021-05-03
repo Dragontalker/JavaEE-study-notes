c7596c464d9a90b29ee92088ad5cdb00cae7f212
Richard <richard.yang.tong@gmail.com>
2021-05-02 21:41:56 -0400

    Added notes.

--- Day_03/Notes/src/com/dragontalker/IfTest.java
+++ Day_03/Notes/src/com/dragontalker/IfTest.java
@@ -3,6 +3,13 @@ package com.dragontalker;
 public class IfTest {
 
     public static void main(String[] args) {
-        
+
+        // 举例1
+        int heartBeats = 79;
+        if (heartBeats < 60 || heartBeats > 100) {
+            System.out.println("需要做进一步检查");
+        } else {
+            System.out.println("检查结束");
+        }
     }
 }
