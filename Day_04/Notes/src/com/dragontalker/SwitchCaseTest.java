a3edb03f2a366419de654650a021b0791830d986
Richard <richard.yang.tong@gmail.com>
2021-05-03 10:09:13 -0400

    Added notes.

---
+++ Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
@@ -0,0 +1,24 @@
+package com.dragontalker;
+
+/*
+分支结构之二: switch-case
+
+1. 格式
+switch(表达式) {
+    case 常量1:
+        执行语句1;
+        // break;
+    case 常量2:
+        执行语句2;
+        // break;
+
+    ...
+
+    default:
+        执行语句n;
+        // break;
+}
+*/
+
+public class SwitchCaseTest {
+}
