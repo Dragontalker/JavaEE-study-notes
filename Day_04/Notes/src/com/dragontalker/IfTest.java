71f25c558a9eb48681ed01fc33aae0b607241cf5
Richard <richard.yang.tong@gmail.com>
2021-05-03 09:33:46 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/IfTest.java
+++ Day_04/Notes/src/com/dragontalker/IfTest.java
@@ -10,6 +10,14 @@ public class IfTest {
         System.out.println("请输入岳小鹏期末成绩： ");
         int score = scan.nextInt();
 
-
+        if (score == 100) {
+            System.out.println("奖励一辆兰博基尼");
+        } else if (score >= 80 && score < 100) {
+            System.out.println("奖励一台iphone 12 Max");
+        } else if (score >= 60 && score < 80) {
+            System.out.println("奖励一个iPad");
+        } else {
+            System.out.println("什么奖励也没有");
+        }
     }
 }
