562f2c13473e37a7ecd9b8b94193b2d0cc637bcb
Richard <richard.yang.tong@gmail.com>
2021-05-03 10:11:43 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
+++ Day_04/Notes/src/com/dragontalker/SwitchCaseTest.java
@@ -21,4 +21,21 @@ switch(表达式) {
 */
 
 public class SwitchCaseTest {
+
+    public static void main(String[] args) {
+
+        int number = 2;
+        switch(number) {
+            case 0:
+                System.out.println("zero");
+            case 1:
+                System.out.println("one");
+            case 2:
+                System.out.println("two");
+            case 3:
+                System.out.println("three");
+            default:
+                System.out.println("other");
+        }
+    }
 }
