1bb7ef4c3819e407ec54ac8ec4f26dba7db614b8
Richard <richard.yang.tong@gmail.com>
2021-05-02 20:55:30 -0400

    Added notes.

---
+++ Day_03/Notes/src/com/dragontalker/TernaryTest.java
@@ -0,0 +1,11 @@
+package com.dragontalker;
+
+public class TernaryTest {
+
+    public static void main(String[] args) {
+        int m = 12;
+        int n = 5;
+        int max = (m > n) ? m : n;
+        System.out.println(max);
+    }
+}
