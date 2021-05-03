fa4f1db33363a07ef02ec115e1306e857e738cbd
Richard <richard.yang.tong@gmail.com>
2021-05-03 09:29:21 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/IfTest.java
+++ Day_04/Notes/src/com/dragontalker/IfTest.java
@@ -6,5 +6,10 @@ public class IfTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("请输入岳小鹏期末成绩： ");
+        int score = scan.nextInt();
+
+
     }
 }
