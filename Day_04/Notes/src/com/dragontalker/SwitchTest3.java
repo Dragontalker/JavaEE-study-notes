2fd490c4e6dfa72b3bebcabdd85f9310314173e2
Richard <richard.yang.tong@gmail.com>
2021-05-03 11:19:56 -0400

    Added notes.

---
+++ Day_04/Notes/src/com/dragontalker/SwitchTest3.java
@@ -0,0 +1,10 @@
+package com.dragontalker;
+
+import java.util.Scanner;
+
+public class SwitchTest3 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
