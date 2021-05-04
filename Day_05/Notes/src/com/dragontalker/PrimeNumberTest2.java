73e45c09a3d4c3b85d209f7b7bcdbc1ab5113797
Richard <richard.yang.tong@gmail.com>
2021-05-04 11:25:47 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/PrimeNumberTest2.java
+++ Day_05/Notes/src/com/dragontalker/PrimeNumberTest2.java
@@ -11,27 +11,18 @@ public class PrimeNumberTest2 {
 
     public static void main(String[] args) {
 
-        // 获取当前时间距离1970-01-01 00:00:00的毫秒数
         long start = System.currentTimeMillis();
 
         int count = 0;
-
         label: for (int i = 2; i < 100000; i++) {
-
-            boolean isFlag = true;
-
-            // 优化二: 对本身时质数的自然数是有效的.
             for (int j = 2; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     continue label;
                 }
             }
-
             count++;
         }
 
-
-        // 获取当前时间距离1970-01-01 00:00:00的毫秒数
         long end = System.currentTimeMillis();
 
         System.out.println("质数的个数为: " + count);
