2eddbb45b6389b41db50a25213a16f65a90ff5c2
Richard <richard.yang.tong@gmail.com>
2021-05-04 11:11:10 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/BreakContinueTest.java
+++ Day_05/Notes/src/com/dragontalker/BreakContinueTest.java
@@ -9,6 +9,12 @@ continue: 只能在循环结构中使用， 结束当次循环
 public class BreakContinueTest {
 
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 10; i++) {
+            if (i % 4 == 0) {
+                //break; // output: 123
+                continue; // output: 123567910
+            }
+            System.out.print(i);
+        }
     }
 }
