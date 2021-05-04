8b6bcacf5728bf7c02eb3f9aa7ccd0c60e73537b
Richard <richard.yang.tong@gmail.com>
2021-05-04 14:32:22 -0400

    Added notes.

---
+++ Day_06/Notes/src/com/dragontalker/ArrayMemoryTest.java
@@ -0,0 +1,12 @@
+package com.dragontalker;
+
+public class ArrayMemoryTest {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3};
+        String[] arr1 = new String[4];
+        arr1[1] = "刘德华";
+        arr1[2] = "张学友";
+        arr1 = new String[3];
+    }
+}
