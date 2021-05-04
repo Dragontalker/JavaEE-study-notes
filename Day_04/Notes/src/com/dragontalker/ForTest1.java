74e6c85f1f58b669eac0f31bb38d9ef978b709da
Richard <richard.yang.tong@gmail.com>
2021-05-03 21:51:07 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ForTest1.java
+++ Day_04/Notes/src/com/dragontalker/ForTest1.java
@@ -4,5 +4,23 @@ public class ForTest1 {
 
     public static void main(String[] args) {
 
+        for (int i = 1; i < 151; i++) {
+            System.out.print(i + " ");
+
+            if (i % 3 == 0) {
+                System.out.print("foo ");
+            }
+
+            if (i % 5 == 0) {
+                System.out.print("biz ");
+            }
+
+            if (i % 7 == 0) {
+                System.out.print("baz ");
+            }
+
+            // 换行
+            System.out.print("\n");
+        }
     }
 }
