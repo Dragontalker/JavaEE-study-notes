c4f664953cb29a25daac90f614f24c98e8cb5fe9
Richard <richard.yang.tong@gmail.com>
2021-05-04 11:49:01 -0400

    Added notes.

--- Day_06/Notes/src/com/dragontalker/ArrayTest.java
+++ Day_06/Notes/src/com/dragontalker/ArrayTest.java
@@ -1,5 +1,13 @@
 package com.dragontalker;
 
+/*
+数组的特点:
+    1. 数组是有序排列的;
+    2. 数组属于引用数据类型的变量. 数组的元素，既可以是基本数据类型，也可以是引用数据类型;
+    3. 创建数组对象会在内存中开辟一整块连续的空间;
+    4. 数组的长度一旦确定，就不能修改.
+ */
+
 public class ArrayTest {
 
     public static void main(String[] args) {
