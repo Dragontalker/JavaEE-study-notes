57fd783a47298c78ea3b1a9c83872b2dc9658003
Richard <richard.yang.tong@gmail.com>
2021-05-04 09:25:20 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/PrimeNumberTest.java
+++ Day_05/Notes/src/com/dragontalker/PrimeNumberTest.java
@@ -9,5 +9,19 @@ public class PrimeNumberTest {
 
     public static void main(String[] args) {
 
+        for (int i = 2; i < 100; i++) {
+
+            int factorCount = 0;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    factorCount++;
+                }
+            }
+
+            if (factorCount == 0) {
+                System.out.println(i + "是一个质数");
+            }
+        }
     }
 }
