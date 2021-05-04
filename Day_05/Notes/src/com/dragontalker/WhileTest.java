722d3292923a80c6ad7f1f16b7442661135ad2b0
Richard <richard.yang.tong@gmail.com>
2021-05-03 22:23:05 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/WhileTest.java
+++ Day_05/Notes/src/com/dragontalker/WhileTest.java
@@ -2,11 +2,15 @@ package com.dragontalker;
 
 /*
 While 循环的使用
+
+算法: 有限性
 */
 
 public class WhileTest {
 
     public static void main(String[] args) {
+
+        // 遍历100以内所有的偶数
         int i = 1;
         while(i <= 100) {
             if (i % 2 == 0) {
@@ -14,5 +18,8 @@ public class WhileTest {
             }
             i++;
         }
+
+        // 出了while循环以后， i为101
+        System.out.println("i现在是: " + i);
     }
 }
