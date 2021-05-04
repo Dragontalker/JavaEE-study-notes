46db5f1da18b2e97ec352a39c2387beadce52e2f
Richard <richard.yang.tong@gmail.com>
2021-05-03 22:26:18 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/DoWhileTest.java
+++ Day_05/Notes/src/com/dragontalker/DoWhileTest.java
@@ -4,5 +4,22 @@ public class DoWhileTest {
 
     public static void main(String[] args) {
 
+        // 遍历100以内的偶数，并计算所有的偶数的和以及偶数的个数
+        int num = 1;
+        int sum = 0;
+        int count = 0;
+
+        do {
+            if (num % 2 == 0) {
+                System.out.println(num);
+                sum += num;
+                count++;
+            }
+
+            num++;
+        } while (num <= 100);
+
+        System.out.println("总和为: " + sum);
+        System.out.println("个数为: " + count);
     }
 }
