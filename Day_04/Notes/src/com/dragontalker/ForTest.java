7b6d96755c5c767684cfe5c5732ac2ac57245ac1
Richard <richard.yang.tong@gmail.com>
2021-05-03 21:45:20 -0400

    Added notes.

--- Day_04/Notes/src/com/dragontalker/ForTest.java
+++ Day_04/Notes/src/com/dragontalker/ForTest.java
@@ -14,11 +14,17 @@ public class ForTest {
             System.out.print('b');
         }
 
-        // 例题: 遍历100以内的偶数
+        // 例题: 遍历100以内的偶数， 输出所有偶数的和
+        int sum = 0;
+        int count = 0;
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
+                sum += i;
+                count++;
             }
         }
+        System.out.println("总和为: " + sum);
+        System.out.println("个数为: " + count);
     }
 }
---
+++ Day_04/Notes/src/com/dragontalker/ForTest1.java
@@ -0,0 +1,8 @@
+package com.dragontalker;
+
+public class ForTest1 {
+
+    public static void main(String[] args) {
+
+    }
+}
