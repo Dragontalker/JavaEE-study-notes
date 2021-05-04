d8f4171ab52c8bba8440f58c63389e43d854c59d
Richard <richard.yang.tong@gmail.com>
2021-05-03 21:58:37 -0400

    Added notes.

---
+++ Day_05/Notes/src/com/dragontalker/ForTest.java
@@ -0,0 +1,15 @@
+package com.dragontalker;
+
+/*
+题目: 输入两个正整数m和n，求其最大公约数和最小公倍数。
+比如: 12和20的最大公约数是4， 最小公倍数是60。
+
+说明: break关键字的使用
+*/
+
+public class ForTest {
+
+    public static void main(String[] args) {
+
+    }
+}
