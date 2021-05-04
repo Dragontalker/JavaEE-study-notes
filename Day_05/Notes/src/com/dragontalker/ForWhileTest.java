c32e5e7b29aec90c1395e291b213c171ce556626
Richard <richard.yang.tong@gmail.com>
2021-05-04 08:56:52 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/ForWhileTest.java
+++ Day_05/Notes/src/com/dragontalker/ForWhileTest.java
@@ -5,9 +5,14 @@ package com.dragontalker;
 从键盘读入个数不确定的整数，并判断读入的正数和负数的个数，输入为0时结束程序.
 */
 
+import java.util.Scanner;
+
 public class ForWhileTest {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
 
     }
 }
