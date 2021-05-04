02394f393b4c06c57e2cf669abd406dab57bd9c1
Richard <richard.yang.tong@gmail.com>
2021-05-04 11:16:39 -0400

    Added notes.

--- Day_05/Notes/src/com/dragontalker/BreakContinueTest.java
+++ Day_05/Notes/src/com/dragontalker/BreakContinueTest.java
@@ -8,6 +8,8 @@ continue: 只能在循环结构中使用， 结束当次循环.
 相同点: 关键字后面不能声明执行语句.
  */
 
+import javax.swing.plaf.IconUIResource;
+
 public class BreakContinueTest {
 
     public static void main(String[] args) {
@@ -17,6 +19,21 @@ public class BreakContinueTest {
                 continue; // output: 123567910
             }
             System.out.print(i);
+        }
+        System.out.println();
+
+        //****
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if (j % 4 == 0) {
+                    //break; // 默认跳出包裹此关键字最近的一层循环
+                    continue;
+                }
+
+                System.out.print(j);
+            }
+
+            System.out.println();
         }
     }
 }
