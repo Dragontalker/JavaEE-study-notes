b76927696069919c0a39994272dba0bf76b93f48
Richard <richard.yang.tong@gmail.com>
2021-05-05 17:10:09 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/SearchTest.java
+++ Day_07/Notes/src/com/dragontalker/SearchTest.java
@@ -7,6 +7,25 @@ package com.dragontalker;
 public class SearchTest {
 
     public static void main(String[] args) {
+        String[] arr = new String[]{"JJ", "DD", "MM", "BB", "GG", "AA"};
+
+        // 数组的复制(区别于数组变量的赋值)
+        String[] arr1 = new String[arr.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[i];
+        }
+
+        // 数组的反转
+        for (int i = 0; i < arr.length / 2; i++) {
+            String temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+
+        // 遍历
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "\t");
+        }
 
     }
 }
