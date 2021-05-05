c7ff7deb94d2e7b411e67e0017dad4d01de4decd
Richard <richard.yang.tong@gmail.com>
2021-05-04 21:21:06 -0400

    Added notes.

--- Day_06/Notes/src/com/dragontalker/ArrayTest3.java
+++ Day_06/Notes/src/com/dragontalker/ArrayTest3.java
@@ -13,7 +13,8 @@ public class ArrayTest3 {
 
     public static void main(String[] args) {
         int[][] arr = new int[4][3];
-        System.out.println(arr[0]); // 内存地址
+        System.out.println(arr[0]); // 内存地址 [I@e9e54c2
         System.out.println(arr[0][0]); // 0
+        System.out.println(arr); // 内存地址 [[I@65ab7765
     }
 }
