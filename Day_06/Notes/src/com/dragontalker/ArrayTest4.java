271f5e2df0fbffbdd5263919dff42c37b30d1b79
Richard <richard.yang.tong@gmail.com>
2021-05-05 08:57:02 -0400

    Added notes.

--- Day_06/Notes/src/com/dragontalker/ArrayTest4.java
+++ Day_06/Notes/src/com/dragontalker/ArrayTest4.java
@@ -4,5 +4,9 @@ public class ArrayTest4 {
 
     public static void main(String[] args) {
 
+        int[][] arr1 = new int[4][];
+        arr1[1] = new int[]{1, 2, 3};
+        arr1[2] = new int[4];
+        arr1[2][1] = 30;
     }
 }
