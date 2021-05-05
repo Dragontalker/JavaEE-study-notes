59d31be6cab4db2bedceaacbae318b8f5614be67
Richard <richard.yang.tong@gmail.com>
2021-05-04 20:57:12 -0400

    Added notes.

---
+++ Day_06/Notes/src/com/dragontalker/ArrayTest2.java
@@ -0,0 +1,17 @@
+package com.dragontalker;
+
+/*
+二维数组的使用
+ */
+
+public class ArrayTest2 {
+
+    public static void main(String[] args) {
+
+        // 1. 二维数组的声明和初始化
+        int [] arr = new int[]{1, 2, 3}; // 一维数组
+
+        // 静态初始化
+        int[][] arr1 = new int[][]{{1, 2, 3}, {4, 5}, {6, 7, 8}};
+    }
+}
