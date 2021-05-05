566aa97696ebacb67d4a1ac1bd454e04b74f3d7f
Richard <richard.yang.tong@gmail.com>
2021-05-05 16:27:10 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/ArrayTest2.java
+++ Day_07/Notes/src/com/dragontalker/ArrayTest2.java
@@ -6,11 +6,18 @@ package com.dragontalker;
 定义一个int型的一维数组, 包含10个元素, 分别赋一些随机整数,
 然后求出所有元素的最大值, 最小值, 和值, 平均值, 并输出出来.
 要求: 所有随机数都是两位数.
+
+[10, 99]
+公式: (int)(Math.random() * (99 - 10 + 1) + 10)
  */
 
 public class ArrayTest2 {
 
     public  static void main(String[] args) {
+        int[] arr = new int[10];
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * (99 - 10 + 1) + 10);
+        }
     }
 }
