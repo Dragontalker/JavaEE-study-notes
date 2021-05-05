bf9374a811117a71beec51889e6cfab8f8f3e8df
Richard <richard.yang.tong@gmail.com>
2021-05-05 16:25:51 -0400

    Added notes.

---
+++ Day_07/Notes/src/com/dragontalker/ArrayTest2.java
@@ -0,0 +1,16 @@
+package com.dragontalker;
+
+/*
+算法的考查, 求数值型数组中元素的最大值, 最小值, 平均数, 总和等.
+
+定义一个int型的一维数组, 包含10个元素, 分别赋一些随机整数,
+然后求出所有元素的最大值, 最小值, 和值, 平均值, 并输出出来.
+要求: 所有随机数都是两位数.
+ */
+
+public class ArrayTest2 {
+
+    public  static void main(String[] args) {
+
+    }
+}
