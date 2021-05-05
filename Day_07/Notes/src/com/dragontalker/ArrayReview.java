32f7386ec59542a4ef77da2c2f199fd61f7f40e4
Richard <richard.yang.tong@gmail.com>
2021-05-05 09:07:44 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/ArrayReview.java
+++ Day_07/Notes/src/com/dragontalker/ArrayReview.java
@@ -4,8 +4,8 @@ public class ArrayReview {
 
     public static void main(String[] args) {
 
-        String[] strs = new String[5]; // {null, null, null, null, null}
-        strs[2] = "Tom"; // {null, null, "Tom", null, null}
-        strs = new String[3]; // {null, null, null}
+        String[] strs = new String[5]; // {null, null, null, null, null} => 0x89ab
+        strs[2] = "Tom"; // {null, null, "Tom", null, null} => 0x89ab
+        strs = new String[3]; // {null, null, null} => new address: 0x778
     }
 }
