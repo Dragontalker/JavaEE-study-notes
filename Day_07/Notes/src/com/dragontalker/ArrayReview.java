970cc05f1c6c29926692206e4699cf0b0e503f39
Richard <richard.yang.tong@gmail.com>
2021-05-05 10:20:14 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/ArrayReview.java
+++ Day_07/Notes/src/com/dragontalker/ArrayReview.java
@@ -1,5 +1,13 @@
 package com.dragontalker;
 
+/*
+数据结构:
+    1. 数据与数据之间的逻辑关系: 集合, 一对一, 一对多, 多对多
+    2. 数据的储存结构:
+        线性表: 顺序表(比如: 数组), 链表, 栈, 队列
+        树形结构: 二叉树
+ */
+
 public class ArrayReview {
 
     public static void main(String[] args) {
