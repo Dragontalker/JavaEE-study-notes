2e2702d69bc199bd9359fa846b3cc9565eb21e4f
Richard <richard.yang.tong@gmail.com>
2021-05-05 16:33:09 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/ArrayTest2.java
+++ Day_07/Notes/src/com/dragontalker/ArrayTest2.java
@@ -29,9 +29,26 @@ public class ArrayTest2 {
         }
 
         // 求数组元素的最小值
+        int minValue = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (minValue > arr[i]) {
+                minValue = arr[i];
+            }
+        }
 
         // 求数组元素的总和
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
 
         // 求数组元素的平均数
+        double avg = sum / arr.length;
+
+        System.out.println("最大值为: " + maxValue);
+        System.out.println("最小值为: " + minValue);
+        System.out.println("总和为: " + sum);
+        System.out.println("平均数为: " + avg);
+
     }
 }
