238003627411da8f9869e8783cff0087c962901f
Richard <richard.yang.tong@gmail.com>
2021-05-04 20:22:19 -0400

    Added notes.

---
+++ Day_06/Notes/src/com/dragontalker/ArrayExercise2.java
@@ -0,0 +1,11 @@
+package com.dragontalker;
+
+import java.util.Scanner;
+
+public class ArrayExercise2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+    }
+}
