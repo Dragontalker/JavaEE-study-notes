87d81988ea7dfd91ad89821d10c1d2a9b3d95e3e
Richard <richard.yang.tong@gmail.com>
2021-05-05 10:30:08 -0400

    Added notes.

---
+++ Day_07/Notes/src/com/dragontalker/YangHuiTriangle.java
@@ -0,0 +1,18 @@
+package com.dragontalker;
+
+/*
+使用二维数组打印一个10行的杨辉三角.
+
+提示:
+    1. 第一行有1个元素, 第n行有n个元素
+    2. 第一行的第一个元素和最后一个元素都是1
+    3. 第三行开始, 对于非第一个元素和最后一个元素的元素, 即:
+        yangHui[i][j] = yangHui[i-1][j-1] + yangHui[i-1][j]
+ */
+
+public class YangHuiTriangle {
+
+    public static void main(String[] args) {
+
+    }
+}
