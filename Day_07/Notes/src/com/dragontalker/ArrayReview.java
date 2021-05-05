8d4cdd40b77a9d62154b00cf4ab8e201990eafd7
Richard <richard.yang.tong@gmail.com>
2021-05-05 09:05:44 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/ArrayReview.java
+++ Day_07/Notes/src/com/dragontalker/ArrayReview.java
@@ -4,5 +4,8 @@ public class ArrayReview {
 
     public static void main(String[] args) {
 
+        String[] strs = new String[5];
+        strs[2] = "Tom";
+        strs = new String[3];
     }
 }
