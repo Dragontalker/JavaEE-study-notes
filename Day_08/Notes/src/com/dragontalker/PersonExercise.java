197f97e8571188c872885e51fd2b7ea2d5571467
Richard <richard.yang.tong@gmail.com>
2021-05-06 19:27:28 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/PersonExercise.java
+++ Day_08/Notes/src/com/dragontalker/PersonExercise.java
@@ -3,5 +3,8 @@ package com.dragontalker;
 public class PersonExercise {
     public static void main(String[] args) {
         Person p1 = new Person();
+        p1.name = "Tom";
+        p1.age = 18;
+        p1.isMale = true;
     }
 }
