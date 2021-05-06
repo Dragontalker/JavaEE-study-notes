815feb42d72c2b7b784830065a4f9d029edb3a9f
Richard <richard.yang.tong@gmail.com>
2021-05-06 10:36:34 -0400

    Added notes.

---
+++ Day_08/Notes/src/com/dragontalker/OOPTest.java
@@ -0,0 +1,15 @@
+package com.dragontalker;
+
+/*
+Java面向对象学些的三条主线:
+    1. Java类及类的成员:
+        主要：属性, 方法, 构造器
+        次要: 代码块, 内部类
+    2. 面向对象的三大特征:
+        封装性, 继承性, 多态性, (抽象性)
+    3. 其他关键字:
+        this, super, static, final, abstract, interface, package, import
+ */
+
+public class OOPTest {
+}
