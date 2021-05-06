a42010bcf23cbf79773eb6844b701a637aa1f9c5
Richard <richard.yang.tong@gmail.com>
2021-05-06 10:51:10 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/OOPTest.java
+++ Day_08/Notes/src/com/dragontalker/OOPTest.java
@@ -1,7 +1,7 @@
 package com.dragontalker;
 
 /*
-Java面向对象学些的三条主线:
+一、Java面向对象学些的三条主线:
     1. Java类及类的成员:
         主要：属性, 方法, 构造器
         次要: 代码块, 内部类
@@ -9,6 +9,20 @@ Java面向对象学些的三条主线:
         封装性, 继承性, 多态性, (抽象性)
     3. 其他关键字:
         this, super, static, final, abstract, interface, package, import
+
+二、"人把大象装进冰箱"
+
+    1. 面向过程: 强调的是功能行为, 以函数为最小单位, 考虑怎么做.
+        Step 1: 把冰箱门打开
+        Step 2: 抬起大象, 塞进冰箱
+        Step 3: 把冰箱门关闭
+
+    2. 面向对象: 强调具备了功能的对象, 以类/对象为最小单位, 考虑谁来做
+        人 {
+            打开(冰箱) {
+                冰箱.开门();
+            }
+        }
  */
 
 public class OOPTest {
