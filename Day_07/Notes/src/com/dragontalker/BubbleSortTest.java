4d7d96e250e3c25cc19fddce8c4639cf5dc61a5b
Richard <richard.yang.tong@gmail.com>
2021-05-05 20:24:30 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/BubbleSortTest.java
+++ Day_07/Notes/src/com/dragontalker/BubbleSortTest.java
@@ -12,7 +12,13 @@ public class BubbleSortTest {
         // 冒泡排序
 
         for (int i = 0; i < arr.length - 1; i++) {
-            System.out.println(arr[i]);
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
     }
 }
