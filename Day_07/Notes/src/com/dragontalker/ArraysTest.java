0efa339873697f750b57d1774deb7cf7bb045f57
Richard <richard.yang.tong@gmail.com>
2021-05-05 20:46:35 -0400

    Added notes.

--- Day_07/Notes/src/com/dragontalker/ArraysTest.java
+++ Day_07/Notes/src/com/dragontalker/ArraysTest.java
@@ -1,8 +1,19 @@
 package com.dragontalker;
 
+/*
+java.util.Arrays: 操作数组的工具类, 里面定义了很多操作数组的方法
+ */
+
+import java.util.Arrays;
+
 public class ArraysTest {
 
     public static void main(String[] args) {
 
+        // 1. Arrays.equals()
+        int [] arr1 = new int[]{1, 2, 3, 4};
+        int [] arr2 = new int[]{1, 3, 2, 4};
+        boolean isEquals = Arrays.equals(arr1, arr2);
+        System.out.println(isEquals);
     }
 }
