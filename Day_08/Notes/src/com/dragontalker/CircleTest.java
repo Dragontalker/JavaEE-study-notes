7159832cade92d597a2524216c40b0598a96f368
Richard <richard.yang.tong@gmail.com>
2021-05-06 19:41:57 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/CircleTest.java
+++ Day_08/Notes/src/com/dragontalker/CircleTest.java
@@ -1,6 +1,11 @@
 package com.dragontalker;
 
 public class CircleTest {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.radius = 2;
+        System.out.println(c1.findArea());
+    }
 }
 
 // 圆
