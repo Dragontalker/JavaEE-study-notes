941bdf4bddcd1c650fec66afaae7e09e28dddb8a
Richard <richard.yang.tong@gmail.com>
2021-05-06 18:05:29 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/CustomerTest.java
+++ Day_08/Notes/src/com/dragontalker/CustomerTest.java
@@ -20,5 +20,21 @@ class Customer {
     int age;
     boolean isMale;
 
+    // 方法
+    public void eat() {
+        System.out.println("客户吃饭");
+    }
 
+    public void sleep(int hour) {
+        System.out.println("休息了" + hour + "个小时");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNation(String nation) {
+        String info = "我的国籍是: " + nation;
+        return info;
+    }
 }
