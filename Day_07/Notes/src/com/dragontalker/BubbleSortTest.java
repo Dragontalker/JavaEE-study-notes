9c0a144c1b7eea92a7868e5ca98ccb3b4eb0271f
Richard <richard.yang.tong@gmail.com>
2021-05-05 20:22:35 -0400

    Added notes.

---
+++ Day_07/Notes/src/com/dragontalker/BubbleSortTest.java
@@ -0,0 +1,18 @@
+package com.dragontalker;
+
+/*
+数组的冒泡排序的实现
+ */
+
+public class BubbleSortTest {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{43, 32, 76, -98, 0, 64, 33, -21, 32, 99};
+
+        // 冒泡排序
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
