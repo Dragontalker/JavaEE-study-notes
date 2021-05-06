21c553e36ab1d0c9822aaa419946bc301dee7087
Richard <richard.yang.tong@gmail.com>
2021-05-06 19:49:21 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/MatrixTest.java
+++ Day_08/Notes/src/com/dragontalker/MatrixTest.java
@@ -4,4 +4,13 @@ public class MatrixTest {
     public static void main(String[] args) {
 
     }
+
+    public void method() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
