2f87a7d01bf1280cd75a4d07dec2cc0bea6949f8
Richard <richard.yang.tong@gmail.com>
2021-05-06 17:50:13 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/UserTest.java
+++ Day_08/Notes/src/com/dragontalker/UserTest.java
@@ -13,6 +13,9 @@ package com.dragontalker;
     2.1. 在类中声明的位置的不同
         属性: 直接定义在类的一对{}内
         局部变量: 声明在方法内、方法形参、代码快内、构造器形参、构造器内部的变量
+    2.2. 关于权限修饰符的不同
+        属性: 可以在声明属性时, 指明其权限, 使用权限修饰符.
+        常用的权限修饰符: private, public, 缺省, protected
  */
 
 public class UserTest {
@@ -21,8 +24,9 @@ public class UserTest {
 
 class User {
     // 属性(或成员变量)
-    String name;
-    int age;
+    private String name;
+    public int age;
+    protected boolean isMale;
 
     public void talk(String language) { // language: 形参, 也是局部变量
         System.out.println("我们使用" + language + "进行交流");
