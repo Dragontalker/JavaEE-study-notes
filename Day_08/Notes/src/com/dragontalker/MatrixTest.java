24679c912e451c5ba153ac39c95b274173dcb1f3
Richard <richard.yang.tong@gmail.com>
2021-05-06 19:51:27 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/MatrixTest.java
+++ Day_08/Notes/src/com/dragontalker/MatrixTest.java
@@ -3,15 +3,20 @@ package com.dragontalker;
 public class MatrixTest {
     public static void main(String[] args) {
         MatrixTest test = new MatrixTest();
-        test.method();
+        test.printMatrix();
+        System.out.println("面积为: "+ test.getArea());
     }
 
-    public void method() {
+    public void printMatrix() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 8; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+    }
+
+    public int getArea() {
+        return 10 * 8;
     }
 }
