0f6b99a86eace993fd04fb48d8ae1d5537897155
Richard <richard.yang.tong@gmail.com>
2021-05-06 17:01:17 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/UserTest.java
+++ Day_08/Notes/src/com/dragontalker/UserTest.java
@@ -10,6 +10,7 @@ package com.dragontalker;
 2. 不同点
     2.1. 在类中声明的位置的不同
         属性: 直接定义在类的一对{}内
+        局部变量: 声明在方法内、方法形参、代码快内、构造器形参、构造器内部的变量
  */
 
 public class UserTest {
@@ -20,4 +21,8 @@ class User {
     // 属性(或成员变量)
     String name;
     int age;
+
+    public void talk(String language) { // language: 形参
+        System.out.println("我们使用" + language + "进行交流");
+    }
 }
