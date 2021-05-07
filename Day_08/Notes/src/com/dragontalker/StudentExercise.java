4d218b8954fbaacc62a2593f3206aaf780a90339
Richard <richard.yang.tong@gmail.com>
2021-05-06 20:16:25 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/StudentExercise.java
+++ Day_08/Notes/src/com/dragontalker/StudentExercise.java
@@ -1,10 +1,19 @@
 package com.dragontalker;
 
 public class StudentExercise {
+    public static void main(String[] args) {
+
+        Student[] students = new Student[20];
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student();
+        }
+
+    }
 }
 
 class Student {
-    int number;
-    int state;
-    int score;
+    int number; // 学号
+    int state; // 年级
+    int score; // 成绩
 }
