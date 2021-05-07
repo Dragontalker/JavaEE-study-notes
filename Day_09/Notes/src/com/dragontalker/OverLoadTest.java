d0a4536d150079a5acfb63bb73ce7b79f143f637
Richard <richard.yang.tong@gmail.com>
2021-05-07 10:09:00 -0400

    Added notes.

---
+++ Day_09/Notes/src/com/dragontalker/OverLoadTest.java
@@ -0,0 +1,17 @@
+package com.dragontalker;
+
+/*
+方法的重载(overload)
+    1. 定义: 在同一个类中, 允许存在一个以上的同名方法, 只要它们的参数
+
+        “两同一不同”: 同一个类、相同方法名
+            - 参数列表不同
+            - 参数个数不同
+            - 参数类型不同
+
+    2. 举例:
+        - Arrays类中的sort(), binarySearch()
+ */
+
+public class OverLoadTest {
+}
