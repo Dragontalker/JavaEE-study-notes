b7348574f7c6ae41ba003b320ee288ab4f09c7ab
Richard <richard.yang.tong@gmail.com>
2021-05-07 10:10:18 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/OverLoadTest.java
+++ Day_09/Notes/src/com/dragontalker/OverLoadTest.java
@@ -14,4 +14,15 @@ package com.dragontalker;
  */
 
 public class OverLoadTest {
+
+    // 如下的4个方法构成了重载
+    public void getSum(int i, int j) {
+        System.out.println(i + j);
+    }
+
+    public void getSum(double d1, double d2) {}
+
+    public void getSum(String s, int i) {}
+
+    public void getSum(int i, String s) {}
 }
