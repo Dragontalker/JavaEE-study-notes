16c67de40450420ab99fe35325155fdf541d7d75
Richard <richard.yang.tong@gmail.com>
2021-05-07 18:46:17 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/ValueTransferTest1.java
+++ Day_09/Notes/src/com/dragontalker/ValueTransferTest1.java
@@ -3,10 +3,30 @@ package com.dragontalker;
 /*
 方法形参的传递机制: 值传递
     1. 形参: 方法定义时, 声明的小括号内的参数
-    2. 实参: 方法调用时, 实际传递给形参的数据
+       实参: 方法调用时, 实际传递给形参的数据
+
+    2. 值传递机制
+
 
 
  */
 
 public class ValueTransferTest1 {
+    public static void main(String[] args) {
+        // 基本数据类型
+        int m = 10;
+        int n = 20;
+        System.out.println(" m = " + m + " n = " + n);
+
+        ValueTransferTest1 test = new ValueTransferTest1();
+        test.swap(m, n);
+        System.out.println(" m = " + m + " n = " + n);
+
+    }
+
+    public void swap(int m, int n) {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
 }
