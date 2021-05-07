a5314752551b8a81485dd63b1cf87bb7262a872e
Richard <richard.yang.tong@gmail.com>
2021-05-07 18:10:01 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/OverloadExercise.java
+++ Day_09/Notes/src/com/dragontalker/OverloadExercise.java
@@ -17,4 +17,18 @@ public class OverloadExercise {
     public void mOL(String s) {
         System.out.println(s);
     }
+
+    // 2. 如下的三个方法构成重载
+    public int max(int i, int j) {
+        return (i > j) ? i : j;
+    }
+
+    public double max(double d1, double d2) {
+        return (d1 > d2) ? d1: d2;
+    }
+
+    public double max(double d1, double d2, double d3) {
+        double max = (d1 > d2) ? d1 : d2;
+        return (max > d3) ? max : d3;
+    }
 }
