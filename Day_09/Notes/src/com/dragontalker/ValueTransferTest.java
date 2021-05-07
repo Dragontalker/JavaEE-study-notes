21dbdbf424686302805ce53bf0553514bc5f5b67
Richard <richard.yang.tong@gmail.com>
2021-05-07 18:34:25 -0400

    Added notes.

---
+++ Day_09/Notes/src/com/dragontalker/ValueTransferTest.java
@@ -0,0 +1,13 @@
+package com.dragontalker;
+
+public class ValueTransferTest {
+    public static void main(String[] args) {
+         int m = 10;
+         int n = m;
+
+        System.out.println(" m = " + m + " n = " + n);
+        n = 20;
+
+        System.out.println(" m = " + m + " n = " + n);
+    }
+}
