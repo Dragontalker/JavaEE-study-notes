6a4f97740a9e20bb9762c08fda94959a8eb76b34
Richard <richard.yang.tong@gmail.com>
2021-05-07 18:40:21 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/ValueTransferTest.java
+++ Day_09/Notes/src/com/dragontalker/ValueTransferTest.java
@@ -1,5 +1,11 @@
 package com.dragontalker;
 
+/*
+关于变量的赋值:
+    - 如果变量是基本数据类型, 此时赋值的是变量所保存的数据值.
+    - 如果变量是引用数据类型, 此时赋值的是变量所保存的数据的地址值.
+ */
+
 public class ValueTransferTest {
     public static void main(String[] args) {
 
@@ -14,7 +20,7 @@ public class ValueTransferTest {
         Order o1 = new Order();
         o1.orderId = 1001;
 
-        Order o2 = o1;
+        Order o2 = o1; // 赋值以后, o1和o2的地址值相同, 都指向了堆空间中的同一个对象实体.
         System.out.println("o1.orderId = " + o1.orderId + ", o2.orderId = " + o2.orderId);
 
         o2.orderId = 1002;
