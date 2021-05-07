2d2c48d02aa490b2a0b3d9878bf779e2ad534df2
Richard <richard.yang.tong@gmail.com>
2021-05-07 18:37:24 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/ValueTransferTest.java
+++ Day_09/Notes/src/com/dragontalker/ValueTransferTest.java
@@ -2,12 +2,26 @@ package com.dragontalker;
 
 public class ValueTransferTest {
     public static void main(String[] args) {
-         int m = 10;
-         int n = m;
 
+        // 基本数据类型
+        int m = 10;
+        int n = m;
         System.out.println(" m = " + m + " n = " + n);
-        n = 20;
+        m = 20;
+        System.out.println(" m = " + m + " n = " + n);
 
-        System.out.println(" m = " + m + " n = " + n);
+        // 引用数据类型
+        Order o1 = new Order();
+        o1.orderId = 1001;
+
+        Order o2 = o1;
+        System.out.println("o1.orderId = " + o1.orderId + ", o2.orderId = " + o2.orderId);
+
+        o2.orderId = 1002;
+        System.out.println("o1.orderId = " + o1.orderId + ", o2.orderId = " + o2.orderId);
     }
+}
+
+class Order {
+    int orderId;
 }
