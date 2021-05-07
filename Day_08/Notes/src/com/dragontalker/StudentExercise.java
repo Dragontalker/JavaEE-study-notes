b6273868128e8823455a552d70560a76798effd4
Richard <richard.yang.tong@gmail.com>
2021-05-06 20:24:08 -0400

    Added notes.

--- Day_08/Notes/src/com/dragontalker/StudentExercise.java
+++ Day_08/Notes/src/com/dragontalker/StudentExercise.java
@@ -13,7 +13,9 @@ public class StudentExercise {
         }
 
         for (int i = 0; i < students.length; i++) {
-            System.out.println(students[i].info());
+            if (students[i].state == 3) {
+                System.out.println(students[i].info());
+            }
         }
 
     }
