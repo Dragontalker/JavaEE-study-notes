7068bdb62e535ad72965830fb5d42b515464faf8
Richard <richard.yang.tong@gmail.com>
2021-05-07 18:06:43 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/OverloadExercise.java
+++ Day_09/Notes/src/com/dragontalker/OverloadExercise.java
@@ -5,7 +5,16 @@ public class OverloadExercise {
 
     }
 
+    // 1. 如下的三个方法构成重载
     public void mOL(int i) {
         System.out.println(i * i);
+    }
+
+    public void mOL(int i, int j) {
+        System.out.println(i * j);
+    }
+
+    public void mOL(String s) {
+        System.out.println(s);
     }
 }
