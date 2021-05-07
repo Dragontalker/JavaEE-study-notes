a8e4d616b065a03e00b180d7685f3ad8ba58e5ad
Richard <richard.yang.tong@gmail.com>
2021-05-07 09:25:53 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/ArrayUtil.java
+++ Day_09/Notes/src/com/dragontalker/ArrayUtil.java
@@ -13,10 +13,19 @@ public class ArrayUtil {
     }
 
     // 求数组的最小值
+    public int getMin(int[] arr) {
+        return 0;
+    }
 
     // 求数组的总和
+    public int getSum(int[] arr) {
+        return 0;
+    }
 
     // 求数组的平均值
+    public double getAvg(int[] args) {
+        return 0.0;
+    }
 
     // 反转数组
 
