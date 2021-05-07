b6883adc28fdcb41a919744df8686df8b9fedcb9
Richard <richard.yang.tong@gmail.com>
2021-05-07 09:25:00 -0400

    Added notes.

---
+++ Day_09/Notes/src/com/dragontalker/ArrayUtil.java
@@ -0,0 +1,28 @@
+package com.dragontalker;
+
+/*
+自定义数组的工具类
+
+ */
+
+public class ArrayUtil {
+
+    // 求数组的最大值
+    public int getMax(int[] arr) {
+        return 0;
+    }
+
+    // 求数组的最小值
+
+    // 求数组的总和
+
+    // 求数组的平均值
+
+    // 反转数组
+
+    // 复制数组
+
+    // 数组排序
+
+    // 遍历数组
+}
