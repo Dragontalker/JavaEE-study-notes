54c54c6de20bf41384122e845d378dbc9eef13fd
Richard <richard.yang.tong@gmail.com>
2021-05-07 21:51:55 -0400

    Added notes.

---
+++ Day_09/Notes/src/com/dragontalker/Circle.java
@@ -0,0 +1,10 @@
+package com.dragontalker;
+
+public class Circle {
+    double radius; // 半径
+
+    public double findArea() {
+        // 求圆的面积
+        return Math.PI * radius * radius;
+    }
+}
