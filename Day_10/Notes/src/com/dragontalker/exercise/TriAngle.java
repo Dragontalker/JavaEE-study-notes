5809b8b7f17d578dcc4ac20418df0a21fc4181dd
Richard <richard.yang.tong@gmail.com>
2021-05-08 15:54:05 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/exercise/TriAngle.java
+++ Day_10/Notes/src/com/dragontalker/exercise/TriAngle.java
@@ -1,5 +1,13 @@
 package com.dragontalker.exercise;
 
+/*
+总结: 属性赋值的先后顺序
+    1. 默认初始化值
+    2. 显式初始化值
+    3. 构造器中赋值
+    4. 通过“对象.方法”或“对象.属性”的方式赋值
+ */
+
 public class TriAngle {
     private double base;
     private double height;
