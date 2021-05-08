f770b176b776b560eacf5a311dc259d96050e6f4
Richard <richard.yang.tong@gmail.com>
2021-05-08 15:46:53 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/exercise/TriAngle.java
+++ Day_10/Notes/src/com/dragontalker/exercise/TriAngle.java
@@ -5,4 +5,20 @@ package com.dragontalker.exercise;
 public class TriAngle {
     private double base;
     private double height;
+
+    public void setBase(double b) {
+        base = b;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setHeight(double h) {
+        height = h;
+    }
+
+    public double getHeight() {
+        return height;
+    }
 }
