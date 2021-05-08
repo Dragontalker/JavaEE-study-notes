0435956ad0cc9c32360295f4462a7a30b17ee1ed
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:35:46 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
+++ Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
@@ -1,5 +1,11 @@
 package com.dragontalker.java2;
 
+/*
+this关键字的使用:
+    1. this可以用来修饰: 属性、方法、构造器
+    2. this修饰
+ */
+
 public class PersonTest {
     public static void main(String[] args) {
         Person p = new Person();
@@ -7,6 +13,7 @@ public class PersonTest {
         p.setName("Tom");
         System.out.println(p.getAge());
         System.out.println(p.getName());
+        p.eat();
     }
 }
 
@@ -28,5 +35,14 @@ class Person {
 
     public int getAge() {
         return this.age;
+    }
+
+    public void eat() {
+        System.out.println("人吃饭");
+        this.study();
+    }
+
+    public void study() {
+        System.out.println("人学习");
     }
 }
