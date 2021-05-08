a9eec7eb8a5f63e79dcf6ec107738acebcf9481a
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:10:27 -0400

    Added notes.

---
+++ Day_10/Notes/src/com/dragontalker/Customer.java
@@ -0,0 +1,17 @@
+package com.dragontalker;
+
+/*
+JavaBean
+ */
+
+public class Customer {
+    private String name;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
