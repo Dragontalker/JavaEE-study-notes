b5624cc4713e7f89414433614f8da8b191144270
Richard <richard.yang.tong@gmail.com>
2021-05-08 17:07:34 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/exercise/Girl.java
+++ Day_10/Notes/src/com/dragontalker/exercise/Girl.java
@@ -1,4 +1,24 @@
 package com.dragontalker.exercise;
 
 public class Girl {
+    private String name;
+    private int age;
+
+    public Girl(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void marry(Boy boy) {
+        System.out.println("我想嫁给" + boy.getName());
+        boy.marry(this);
+    }
 }
