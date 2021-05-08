e3f40cadba69372cf2b711ddc36022426244769f
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:38:32 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
+++ Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
@@ -3,7 +3,11 @@ package com.dragontalker.java2;
 /*
 this关键字的使用:
     1. this可以用来修饰: 属性、方法、构造器
-    2. this修饰
+    2. this理解为: 当前对象
+
+    - 在类的方法中, 我们可以使用“this.属性”或“this.方法”
+    - 通常情况下, 我们都选择省略"this"
+    - 特殊情况下, 如果方法的形参和类的属性同名时, 我们必须显式的使用"this.变量"的方式, 表明此变量是属性, 而不是形参
  */
 
 public class PersonTest {
