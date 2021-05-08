eb6b4f0e772d6dfb2ecb3482a2a68b7bee330bd1
Richard <richard.yang.tong@gmail.com>
2021-05-08 17:50:39 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/bank/Bank.java
+++ Day_10/Notes/src/com/dragontalker/bank/Bank.java
@@ -7,7 +7,9 @@ public class Bank {
     public Bank() {}
 
     public void addCustomer(String firstName, String lastName) {
-
+        Customer customer = new Customer(firstName, lastName);
+        customers[numberOfCustomers] = customer;
+        numberOfCustomers++;
     }
 
     public int getNumberOfCustomers() {
