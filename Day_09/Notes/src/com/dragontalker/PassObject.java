b06d2873409fc66e7caaf175b96fa282ba81c731
Richard <richard.yang.tong@gmail.com>
2021-05-07 21:56:54 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/PassObject.java
+++ Day_09/Notes/src/com/dragontalker/PassObject.java
@@ -3,7 +3,9 @@ package com.dragontalker;
 public class PassObject {
 
     public static void main(String[] args) {
+        PassObject test = new PassObject();
 
+        test.printAreas(new Circle(), 5);
     }
 
     public void printAreas(Circle c, int time) {
