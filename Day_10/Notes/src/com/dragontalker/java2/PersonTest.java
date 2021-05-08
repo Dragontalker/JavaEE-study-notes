02d59aff0cac2d13019aeb84e2edf52c78c1c96b
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:20:31 -0400

    Added notes.

---
+++ Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
@@ -0,0 +1,25 @@
+package com.dragontalker.java2;
+
+public class PersonTest {
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
