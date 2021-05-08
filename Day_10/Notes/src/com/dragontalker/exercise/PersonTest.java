92ae2b22cbca4acc769d4308fc740cccf9585252
Richard <richard.yang.tong@gmail.com>
2021-05-08 15:12:32 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/exercise/PersonTest.java
+++ Day_10/Notes/src/com/dragontalker/exercise/PersonTest.java
@@ -1,4 +1,9 @@
 package com.dragontalker.exercise;
 
 public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.setAge(20);
+        System.out.println("年龄为: " + p1.getAge());
+    }
 }
