d464d9865422ede141c444a92a40379133859997
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:39:14 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
+++ Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
@@ -25,6 +25,16 @@ class Person {
     private String name;
     private int age;
 
+    public Person() {}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
