de7fe73c543344b349d3b604ebab561b949d8a73
Richard <richard.yang.tong@gmail.com>
2021-05-08 15:48:53 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/exercise/TriAngleTest.java
+++ Day_10/Notes/src/com/dragontalker/exercise/TriAngleTest.java
@@ -4,5 +4,9 @@ public class TriAngleTest {
     public static void main(String[] args) {
         TriAngle t1 = new TriAngle();
         System.out.println("base: " + t1.getBase() + ", height: " + t1.getHeight());
+
+        t1.setBase(2.0);
+        t1.setHeight(2.4);
+        System.out.println("base: " + t1.getBase() + ", height: " + t1.getHeight());
     }
 }
