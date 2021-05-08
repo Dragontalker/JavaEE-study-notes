43904dcc1ec1a4bc29a2162881043ac6fd8ada4b
Richard <richard.yang.tong@gmail.com>
2021-05-07 21:56:01 -0400

    Added notes.

---
+++ Day_09/Notes/src/com/dragontalker/PassObject.java
@@ -0,0 +1,20 @@
+package com.dragontalker;
+
+public class PassObject {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void printAreas(Circle c, int time) {
+
+        System.out.println("Radius\t\tArea");
+
+        // 设置圆的半径
+        for (int i = 1; i <= time; i++) {
+            // 设置圆的半径
+            c.radius = i;
+            System.out.println(c.radius + "\t\t" + c.findArea());
+        }
+    }
+}
