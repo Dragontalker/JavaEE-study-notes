b809bbeb17bb07c309a1925552f7e9c8f391e131
Richard <richard.yang.tong@gmail.com>
2021-05-07 21:41:28 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/BonusQuestion.java
+++ Day_09/Notes/src/com/dragontalker/BonusQuestion.java
@@ -1,5 +1,7 @@
 package com.dragontalker;
 
+import java.io.PrintStream;
+
 public class BonusQuestion {
     public static void main(String[] args) {
         int a = 10;
@@ -10,11 +12,27 @@ public class BonusQuestion {
     }
 
     // 方法一:
+    //public static void method(int a, int b) {
+        //a = a * 10;
+        //b = b * 20;
+        //System.out.println("a=" + a);
+        //System.out.println("b=" + b);
+        //System.exit(0);
+    //}
+
+    // 方法二
     public static void method(int a, int b) {
-        a = a * 10;
-        b = b * 20;
-        System.out.println("a=" + a);
-        System.out.println("b=" + b);
-        System.exit(0);
+        PrintStream ps = new PrintStream(System.out) {
+            @Override
+            public void println(String x) {
+                if ("a=10".equals(x)) {
+                    x = "a=100";
+                } else if ("b=10".equals(x)) {
+                    x = "b=200";
+                }
+                super.println(x);
+            }
+        };
+        System.setOut(ps);
     }
 }
