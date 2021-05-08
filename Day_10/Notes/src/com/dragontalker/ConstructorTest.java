fa169acefc806cb14f60397df4069eb173f46d42
Richard <richard.yang.tong@gmail.com>
2021-05-08 15:21:59 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/ConstructorTest.java
+++ Day_10/Notes/src/com/dragontalker/ConstructorTest.java
@@ -4,11 +4,25 @@ package com.dragontalker;
 类的结构之三: 构造器(Constructor)
 
 一、构造器的作用:
-    - 创建对象
+    - 创建对象: new + 构造器
+
+二、说明:
+    1. 如果没有显式的定义类的构造器的话, 则系统默认提供一个空参的构造器
+    2. 定义构造器的格式: 权限修饰符 类名(形参列表)
  */
 
 public class ConstructorTest {
     public static void main(String[] args) {
+        Person p = new Person();
+    }
+}
 
+class Person {
+    String name;
+    int age;
+
+    // 构造器
+    public Person() {
+        System.out.println("Person().....");
     }
 }
