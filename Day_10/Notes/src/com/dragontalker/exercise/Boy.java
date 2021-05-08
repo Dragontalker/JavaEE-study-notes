91885bd2f6d06dae551a9473c1f4026de46388d0
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:58:41 -0400

    Added notes.

---
+++ Day_10/Notes/src/com/dragontalker/exercise/Boy.java
@@ -0,0 +1,22 @@
+package com.dragontalker.exercise;
+
+public class Boy {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
