3e716ded31d4c1eec1f609a9c2f05ac1449a0195
Richard <richard.yang.tong@gmail.com>
2021-05-07 21:09:16 -0400

    Added notes.

--- Day_09/Notes/src/com/dragontalker/ValueTransferTest2.java
+++ Day_09/Notes/src/com/dragontalker/ValueTransferTest2.java
@@ -3,7 +3,18 @@ package com.dragontalker;
 public class ValueTransferTest2 {
     public static void main(String[] args) {
 
+        Data data = new Data();
 
+        data.m = 10;
+        data.n = 20;
+
+        System.out.println("m = " + data.m + ", n = " + data.n);
+    }
+
+    public void swap (Data data) {
+        int temp = data.m;
+        data.m = data.n;
+        data.n = temp;
     }
 }
 
