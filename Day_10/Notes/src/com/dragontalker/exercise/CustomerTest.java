1be71c78a6ef569c59ed40b09d6cbfe7a73f1b1b
Richard <richard.yang.tong@gmail.com>
2021-05-08 17:39:35 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/exercise/CustomerTest.java
+++ Day_10/Notes/src/com/dragontalker/exercise/CustomerTest.java
@@ -5,5 +5,8 @@ public class CustomerTest {
         Customer customer = new Customer("Jane", "Smith");
         Account account = new Account(1000, 2000, 1.23);
         customer.setAccount(account);
+        customer.getAccount().deposit(100);
+        customer.getAccount().withdraw(960);
+        customer.getAccount().withdraw(2000);
     }
 }
