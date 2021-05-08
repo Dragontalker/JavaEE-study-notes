8e9c23b880911535650cf1caa028d1cebb533e91
Richard <richard.yang.tong@gmail.com>
2021-05-08 16:23:06 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
+++ Day_10/Notes/src/com/dragontalker/java2/PersonTest.java
@@ -1,25 +1,32 @@
 package com.dragontalker.java2;
 
 public class PersonTest {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setAge(12);
+        p.setName("Tom");
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
+    }
 }
 
 class Person {
     private String name;
     private int age;
 
-    public void setName(String n) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public void setAge(int a) {
-        age = a;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public int getAge() {
-        return age;
+        return this.age;
     }
 }
