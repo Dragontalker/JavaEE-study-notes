6cdd1c0c574be11775d3f2ca9c54468cc5907408
Richard <richard.yang.tong@gmail.com>
2021-05-08 17:49:09 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/bank/Bank.java
+++ Day_10/Notes/src/com/dragontalker/bank/Bank.java
@@ -3,4 +3,18 @@ package com.dragontalker.bank;
 public class Bank {
     private Customer[] customers;
     private int numberOfCustomers;
+
+    public Bank() {}
+
+    public void addCustomer(String firstName, String lastName) {
+
+    }
+
+    public int getNumberOfCustomers() {
+
+    }
+
+    public Customer getCustomer(int index) {
+
+    }
 }
