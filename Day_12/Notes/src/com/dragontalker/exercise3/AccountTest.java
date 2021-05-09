fa8b911432e0886e46c722635df58b592b55266f
Richard <richard.yang.tong@gmail.com>
2021-05-09 15:07:05 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/exercise3/AccountTest.java
+++ Day_12/Notes/src/com/dragontalker/exercise3/AccountTest.java
@@ -5,9 +5,11 @@ public class AccountTest {
         Account account = new Account(1122, 20000, 0.045);
         account.withdraw(30000);
         System.out.println("您的账户余额为￥" + account.getBalance());
-        
+
         account.withdraw(2500);
         account.deposit(3000);
         System.out.println("您的账户余额为￥" + account.getBalance());
+
+        System.out.println("您的月利率为" + account.getAnnualInterestRate() + "%");
     }
 }
