77d841bb5906138c14550028aec7675f8678e87e
Richard <richard.yang.tong@gmail.com>
2021-05-09 14:21:18 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/example3/Student.java
+++ Day_12/Notes/src/com/dragontalker/example3/Student.java
@@ -1,4 +1,19 @@
 package com.dragontalker.example3;
 
-public class Student {
+public class Student extends Person{
+    private String major;
+
+    public Student() {}
+
+    public Student(String major) {
+        this.major = major;
+    }
+
+    public void eat() {
+        System.out.println("学生: 多吃有营养的食物");
+    }
+
+    public void study() {
+        System.out.println("学生: 学习知识");
+    }
 }
