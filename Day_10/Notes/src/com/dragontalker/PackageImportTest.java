ec9b5430f22801c7ef9316021181efe699665b34
Richard <richard.yang.tong@gmail.com>
2021-05-08 20:37:57 -0400

    Added notes.

--- Day_10/Notes/src/com/dragontalker/PackageImportTest.java
+++ Day_10/Notes/src/com/dragontalker/PackageImportTest.java
@@ -10,7 +10,18 @@ package com.dragontalker;
     补充:
         - 同一个包下, 不能命名同名的接口、类
         - 不同的包下, 可以命名同名的接口、类
+
+二、import关键字的使用
+    1. import: 导入
+    2. 在源文件中显式的使用import结构导入指定包下的类、接口
+    3. 声明在包的声明和类的声明之间
  */
 
+import java.util.Arrays;
+
 public class PackageImportTest {
+    public static void main(String[] args) {
+        String info = Arrays.toString(new int[]{1, 2, 3});
+        System.out.println(info);
+    }
 }
