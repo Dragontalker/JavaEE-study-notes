f78475af8ec0ab00cad8c147a7213752b40e5c5e
Richard <richard.yang.tong@gmail.com>
2021-05-09 10:51:49 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/exercise/ManKind.java
+++ Day_12/Notes/src/com/dragontalker/exercise/ManKind.java
@@ -4,5 +4,15 @@ public class ManKind {
     private int sex;
     private int salary;
 
+    public void setSex(int sex) {
+        if (sex < 0 || sex > 1) {
+            System.out.println("错误! Sex必须为0或者1!");
+        } else {
+            this.sex = sex;
+        }
+    }
 
+    public int getSex() {
+        return this.sex;
+    }
 }
