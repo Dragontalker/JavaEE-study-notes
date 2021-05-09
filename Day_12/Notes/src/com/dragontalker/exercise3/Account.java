2145dc292e7f8bd800dc6798f6bbab2b6148fa25
Richard <richard.yang.tong@gmail.com>
2021-05-09 15:01:53 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/exercise3/Account.java
+++ Day_12/Notes/src/com/dragontalker/exercise3/Account.java
@@ -22,4 +22,16 @@ public class Account {
     public double getAnnualInterestRate() {
         return this.annualInterestRate;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
 }
