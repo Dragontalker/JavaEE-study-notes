ed986cb996897cde75d39849adda8d80f1092dd2
Richard <richard.yang.tong@gmail.com>
2021-05-09 15:00:30 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/exercise3/Account.java
+++ Day_12/Notes/src/com/dragontalker/exercise3/Account.java
@@ -4,4 +4,10 @@ public class Account {
     private int id;
     private double balance;
     private double annualInterestRate;
+
+    public Account(int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+    }
 }
