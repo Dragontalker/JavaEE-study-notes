b117894ee592ca8d9faff76e8b33edf3da54c9a2
Richard <richard.yang.tong@gmail.com>
2021-05-09 15:09:33 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/exercise3/CheckAccount.java
@@ -0,0 +1,10 @@
+package com.dragontalker.exercise3;
+
+public class CheckAccount extends Account{
+    private double overdraft;
+
+    public CheckAccount(int id, double balance, double annualInterestRate, double overdraft) {
+        super(id, balance, annualInterestRate);
+        this.overdraft = overdraft;
+    }
+}
