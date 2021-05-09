1882adda34844a728360895a4cda3c1f7ce0a324
Richard <richard.yang.tong@gmail.com>
2021-05-09 14:02:36 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/example2/Order.java
@@ -0,0 +1,14 @@
+package com.dragontalker.example2;
+
+/*
+体会4种不同的权限修饰
+ */
+
+public class Order {
+    private int orderPrivate;
+    int orderDefault;
+    protected int orderProtected;
+    public int orderPublic;
+
+
+}
