b9690e90687decf465cdee27ee8dfeeaa3803fd4
Richard <richard.yang.tong@gmail.com>
2021-05-09 09:59:07 -0400

    Added notes.

--- Day_11/Notes/src/com/dragontalker/ExtendsTest.java
+++ Day_11/Notes/src/com/dragontalker/ExtendsTest.java
@@ -1,12 +1,16 @@
 package com.dragontalker;
 
 /*
-面向对象的特征之二: 继承性
+面向对象的特征之二: 继承性 Why?
 
 一、继承性的好处:
     1. 减少了代码的重复性, 提高了代码的复用性
     2. 便于功能的扩展
     3. 为之后多态性的使用, 提供了前提
+
+二、继承性的格式: class A extends B {}
+    A: 子类、派生类、subclass
+    B: 父类、超类、基类、superclass
  */
 
 public class ExtendsTest {
