4b0d1e462ae1cb8dc160e7b127515baa2dda556e
Richard <richard.yang.tong@gmail.com>
2021-05-09 19:58:21 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/java4/Man.java
+++ Day_12/Notes/src/com/dragontalker/java4/Man.java
@@ -1,9 +1,19 @@
 package com.dragontalker.java4;
 
-public class Man {
+public class Man extends Person{
     private boolean isSmoking;
 
     public void earnMoney() {
         System.out.println("男人负责挣钱养家");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("男人多吃肉, 长肌肉");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("男人霸气的走路");
     }
 }
