ae88071468629a7a22f947ac671bed085c0d992b
Richard <richard.yang.tong@gmail.com>
2021-05-09 13:56:56 -0400

    Added notes.

--- Day_12/Notes/src/com/dragontalker/exercise1/Kids.java
+++ Day_12/Notes/src/com/dragontalker/exercise1/Kids.java
@@ -20,4 +20,8 @@ public class Kids extends ManKind{
     public void printAge() {
         System.out.println("I am " + yearsOld + " years old.");
     }
+
+    public void employed() {
+        System.out.println("Kids should study and no job");
+    }
 }
