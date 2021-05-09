605bfbe339d6bbe8739a7d628a8451f92f30066d
Richard <richard.yang.tong@gmail.com>
2021-05-09 11:54:35 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/example1/StudentTest.java
@@ -0,0 +1,10 @@
+package com.dragontalker.example1;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.eat();
+        s.walk(10);
+        s.study();
+    }
+}
