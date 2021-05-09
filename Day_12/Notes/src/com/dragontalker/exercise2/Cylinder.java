922adea847596cce273e1af0cf1c227891591471
Richard <richard.yang.tong@gmail.com>
2021-05-09 11:07:13 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/exercise2/Cylinder.java
@@ -0,0 +1,18 @@
+package com.dragontalker.exercise2;
+
+public class Cylinder extends Circle{
+
+    private double length;
+
+    public Cylinder() {
+        this.length = 1.0;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+}
