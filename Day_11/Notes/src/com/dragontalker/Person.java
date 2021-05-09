45a86a9c8c16b56c65c0893eb8a9a02e74696f77
Richard <richard.yang.tong@gmail.com>
2021-05-09 09:44:49 -0400

    Added notes.

---
+++ Day_11/Notes/src/com/dragontalker/Person.java
@@ -0,0 +1,21 @@
+package com.dragontalker;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person() {}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void eat() {
+        System.out.println("吃饭");
+    }
+
+    public void sleep() {
+        System.out.println("睡觉");
+    }
+}
