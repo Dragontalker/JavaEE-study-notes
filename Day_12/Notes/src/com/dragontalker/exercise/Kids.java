a7a047968c271e92f479eea133efeb14693ff6ac
Richard <richard.yang.tong@gmail.com>
2021-05-09 10:55:43 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/exercise/Kids.java
@@ -0,0 +1,9 @@
+package com.dragontalker.exercise;
+
+public class Kids {
+    private int yearsOld;
+
+    public void printAge() {
+        System.out.println("I am " + yearsOld + "years old.");
+    }
+}
