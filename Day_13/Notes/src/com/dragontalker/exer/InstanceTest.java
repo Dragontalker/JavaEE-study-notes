1f0a464e80ca6e8ec81b18682a44b227c10dde58
Richard <richard.yang.tong@gmail.com>
2021-05-10 11:04:12 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/exer/InstanceTest.java
+++ Day_13/Notes/src/com/dragontalker/exer/InstanceTest.java
@@ -1,10 +1,27 @@
 package com.dragontalker.exer;
 
 public class InstanceTest {
+    public static void main(String[] args) {
+        InstanceTest test = new InstanceTest();
+        test.method(new Student());
+    }
+
     public void method(Person e) {
         String info = e.getInfo(); // 虚拟方法调用
         System.out.println(info);
+
+        if (e instanceof Graduate) {
+            System.out.println("a graduated student");
+        }
+
+        if (e instanceof Student) {
+            System.out.println("a student");
+        }
+
+        System.out.println("a person");
     }
+
+
 }
 
 class Person {
