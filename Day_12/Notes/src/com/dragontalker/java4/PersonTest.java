205a295946f193ebdad4d8973c8fa44b9e40cb81
Richard <richard.yang.tong@gmail.com>
2021-05-09 20:01:47 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/java4/PersonTest.java
@@ -0,0 +1,15 @@
+package com.dragontalker.java4;
+
+/*
+面向对象特征之三: 多态性
+
+理解多态性: 可以理解为一个事物的多种形态.
+
+ */
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.eat();
+    }
+}
