8f2adbf0d1fdf79e0c4fb967118737be9535982e
Richard <richard.yang.tong@gmail.com>
2021-05-09 20:48:28 -0400

    Added notes.

---
+++ Day_12/Notes/src/com/dragontalker/java5/InterviewTest.java
@@ -0,0 +1,15 @@
+package com.dragontalker.java5;
+
+// 面试题: 多态是编译行为还是运行时行为?
+// 证明如下:
+
+public class InterviewTest {
+}
+
+class Animal {
+    protected void eat() {
+        System.out.println("animal eat food");
+    }
+}
+
+
