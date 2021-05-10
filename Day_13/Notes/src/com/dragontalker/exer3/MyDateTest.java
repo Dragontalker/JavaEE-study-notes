770e1748092a3ed5b7bb7da9e4f13e3058a7354c
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:17:43 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/exer3/MyDateTest.java
+++ Day_13/Notes/src/com/dragontalker/exer3/MyDateTest.java
@@ -20,3 +20,56 @@ public class MyDateTest {
         }
     }
 }
+
+class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj instanceof MyDate) {
+            MyDate myDate = (MyDate)obj;
+            boolean sameDay = this.day == myDate.day;
+            boolean sameMonth = this.month == myDate.month;
+            boolean sameYear = this.year == myDate.year;
+            return sameDay && sameMonth && sameYear;
+        }
+
+        return false;
+    }
+}
