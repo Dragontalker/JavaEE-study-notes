c82cc833eb02cc5f6f58fe9b81322c8342e541f7
Richard <richard.yang.tong@gmail.com>
2021-05-10 10:00:31 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/java/PersonTest.java
+++ Day_13/Notes/src/com/dragontalker/java/PersonTest.java
@@ -9,5 +9,10 @@ public class PersonTest {
         System.out.println(p2.id); // 1001
         //p2.earnMoney(); 不能调用子类特有的方法, p2现在是Person类型
         //p2.isSmoking = true; 子类特有的属性也不能调用
+        // 思考: 内存空间里是否加载过earnMoney()和isSmoking()？Yes, 但是被屏蔽了.
+
+        // 有了对象的多态性以后, 内存中实际上是加载了子类特有的属性和方法的
+        // 但是由于变量声明为父类类型, 编译时, 只能调用父类中声明的属性和方法
+        // 子类特有的属性和方法不能调用
     }
 }
