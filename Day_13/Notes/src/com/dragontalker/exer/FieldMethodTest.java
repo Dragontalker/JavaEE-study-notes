59e8fb8834e47c9280be7aca9566ddded9390085
Richard <richard.yang.tong@gmail.com>
2021-05-10 10:30:05 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/exer/FieldMethodTest.java
@@ -0,0 +1,12 @@
+package com.dragontalker.exer;
+
+class Base {
+    int count = 10;
+
+    public void display() {
+        System.out.println(this.count);
+    }
+}
+
+public class FieldMethodTest {
+}
