47058062e6f15a2cd0e1a2379c5f111f3fdaa8a6
Richard <richard.yang.tong@gmail.com>
2021-05-10 17:04:50 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/interview1/InterviewTest.java
+++ Day_13/Notes/src/com/dragontalker/interview1/InterviewTest.java
@@ -32,11 +32,15 @@ public class InterviewTest {
         Integer j = new Integer(1);
         System.out.println(i == j); // false
 
+        // Integer内部定义了IntegerCache结构, IntegerCache中定了Integer[],
+        // 保存了从-128~127范围内的整数. 如果我们使用自动装箱的方式, 给Integer赋值的范围在
+        // -128~127范围内时, 可以直接使用数组中的元素, 不用再去new了.
+        // 目的: 提高效率
         Integer m = 1;
         Integer n = 1;
         System.out.println(m == n); // true
 
-        Integer x = 128;
+        Integer x = 128; // 128超出了缓存范围, 所以new了Integer对象
         Integer y = 128;
         System.out.println(x == y); // false
     }
