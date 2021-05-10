cc5e8edcbbe2ad0bff8762bc1b76f2aabef51ed6
Richard <richard.yang.tong@gmail.com>
2021-05-10 16:59:22 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/interview1/InterviewTest.java
+++ Day_13/Notes/src/com/dragontalker/interview1/InterviewTest.java
@@ -25,4 +25,19 @@ public class InterviewTest {
         System.out.println(o2); // 1
         // 没有要求类型统一
     }
+
+    @Test
+    public void test3() {
+        Integer i = new Integer(1);
+        Integer j = new Integer(1);
+        System.out.println(i == j); // false
+
+        Integer m = 1;
+        Integer n = 1;
+        System.out.println(m == n); // true
+
+        Integer x = 128;
+        Integer y = 128;
+        System.out.println(x == y); // false
+    }
 }
