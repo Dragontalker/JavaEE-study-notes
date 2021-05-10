b34ea441df48810a6efe56b69ad66eb0d3b15ac4
Richard <richard.yang.tong@gmail.com>
2021-05-10 10:36:00 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/exer/FieldMethodTest.java
+++ Day_13/Notes/src/com/dragontalker/exer/FieldMethodTest.java
@@ -19,12 +19,13 @@ class Sub extends Base {
 public class FieldMethodTest {
     public static void main(String[] args) {
         Sub s = new Sub();
-        System.out.println(s.count);
-        s.display();
+        System.out.println(s.count); //20
+        s.display(); //20
 
         Base b = s;
-        System.out.println(b == s);
-        System.out.println(b.count);
-        b.display();
+        // ==: 对于引用数据类型来讲, 比较的是两个引用数据类型变量的地址值是否相同
+        System.out.println(b == s); //true 内存指向同一个Sub()
+        System.out.println(b.count); //10 多态性不适用于属性
+        b.display(); // 20 虚拟方法调用: 调出的还是20
     }
 }
