d3b1c1fca09c2a60764514261a9724b3256bfff3
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:34:33 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/exer4/GeometricObject.java
+++ Day_13/Notes/src/com/dragontalker/exer4/GeometricObject.java
@@ -1,6 +1,19 @@
 package com.dragontalker.exer4;
 
 public class GeometricObject {
+    protected String color;
+    protected double weight;
+
+    public GeometricObject() {
+        this.color = "white";
+        this.weight = 1.0;
+    }
+
+    public GeometricObject(String color, double weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
     public String getColor() {
         return color;
     }
@@ -16,19 +29,5 @@ public class GeometricObject {
     public void setWeight(double weight) {
         this.weight = weight;
     }
-
-    protected String color;
-    protected double weight;
-
-    public GeometricObject() {
-        this.color = "white";
-        this.weight = 1.0;
-    }
-
-    public GeometricObject(String color, double weight) {
-        this.color = color;
-        this.weight = weight;
-    }
-
 
 }
