323a51290e68a6541b6f504e4ceb8f3f2c7d676f
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:28:28 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/java1/ToStringTest.java
+++ Day_13/Notes/src/com/dragontalker/java1/ToStringTest.java
@@ -3,6 +3,15 @@ package com.dragontalker.java1;
 /*
 Object类中toString()的使用:
 
+1. 当我们输出一个对象的引用时, 实际上就是调用当前对象的toString()
+
+2. Object类中toString()的定义:
+    public String toString() {
+        return getClass().getName() + "@" + Integer.toHexString(hashCode());
+    }
+
+3. 像String, Date, File, 包装类等都重写了Object类中的toString()方法
+
  */
 
 import java.util.Date;
