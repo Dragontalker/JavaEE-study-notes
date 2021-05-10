7148c7b5f3f146805629ef0500c607a89e266dac
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:25:25 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/java1/ToStringTest.java
+++ Day_13/Notes/src/com/dragontalker/java1/ToStringTest.java
@@ -1,9 +1,17 @@
 package com.dragontalker.java1;
 
+/*
+Object类中toString()的使用:
+
+ */
+
 public class ToStringTest {
     public static void main(String[] args) {
         Customer c1 = new Customer("Tom", 22);
         System.out.println(c1.toString());
         System.out.println(c1);
+
+        String str = new String("MM");
+        System.out.println(str);
     }
 }
