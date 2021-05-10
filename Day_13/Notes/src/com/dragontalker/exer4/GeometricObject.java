a461475308e81012eca29fa53c207c3515678340
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:34:00 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/exer4/GeometricObject.java
@@ -0,0 +1,18 @@
+package com.dragontalker.exer4;
+
+public class GeometricObject {
+    protected String color;
+    protected double weight;
+
+    public GeometricObject() {
+        this.color = "white";
+        this.weight = 1.0;
+    }
+
+    public GeometricObject(String color, double weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+
+}
