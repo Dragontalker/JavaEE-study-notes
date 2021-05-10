7086b0f6382d562557618942e786ce095fa4508c
Richard <richard.yang.tong@gmail.com>
2021-05-10 10:41:36 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/exer/InstanceTest.java
@@ -0,0 +1,13 @@
+package com.dragontalker.exer;
+
+public class InstanceTest {
+}
+
+class Person {
+    protected String name = "person";
+    protected int age = 50;
+
+    public String getInfo() {
+        return "Name: " + name + "\n" + "age: " + age;
+    }
+}
