216371c8ef37d9eed9cc4090ba52e76929599186
Richard <richard.yang.tong@gmail.com>
2021-05-10 17:11:32 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/exer5/ScoreTest.java
+++ Day_13/Notes/src/com/dragontalker/exer5/ScoreTest.java
@@ -2,6 +2,16 @@ package com.dragontalker.exer5;
 
 public class ScoreTest {
     public static void main(String[] args) {
+        // 1. 实例化Scanner, 用于从键盘获取学生成绩
 
+        // 2. 创建Vector对象: Vector v = new Vector(); 相当于数组
+
+        // 3. 通过for(;;)或while(true)方式, 给Vector添加数组
+
+        // 3.1 添加操作, v.addElement(Object obj)
+
+        // 4. 获取学生成绩的最大值
+
+        // 5. 遍历Vector, 得到每个学生的成绩, 并与最大成绩比较, 得到每个学生的成绩
     }
 }
