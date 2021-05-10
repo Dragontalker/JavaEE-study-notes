a64d4677445d132ca836ba9b99d12313eb4f57d3
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:20:14 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/java1/ToStringTest.java
@@ -0,0 +1,9 @@
+package com.dragontalker.java1;
+
+public class ToStringTest {
+    public static void main(String[] args) {
+        Customer c1 = new Customer("Tom", 22);
+        System.out.println(c1.toString());
+        System.out.println(c1);
+    }
+}
