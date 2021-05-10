be714ec1523df3a95d57ef78c711d0c3f3d27668
Richard <richard.yang.tong@gmail.com>
2021-05-10 11:49:11 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/java1/ObjectTest.java
+++ Day_13/Notes/src/com/dragontalker/java1/ObjectTest.java
@@ -4,6 +4,14 @@ package com.dragontalker.java1;
 java.lang.Object类
     1. Object类是所有Java类的根父类
     2. 如果在类的声明中未使用extends关键字指明其父类, 则默认父类为java.lang.Object类
+    3. Object类中的功能(属性、方法)就具有通用性
+
+    4. Object类只声明了一个空参的构造器
+
+面试题:
+final, finally, finalize的区别?
+    - final, finally是关键字
+    - finalize()是方法名
  */
 
 public class ObjectTest {
