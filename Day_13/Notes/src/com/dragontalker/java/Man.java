eb0172ee46ba8cde1407ef39de406993c6179524
Richard <richard.yang.tong@gmail.com>
2021-05-10 09:51:42 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/java/Man.java
@@ -0,0 +1,20 @@
+package com.dragontalker.java;
+
+public class Man {
+
+    boolean isSmoking;
+
+    int id = 1002;
+
+    public void earnMoney() {
+        System.out.println("男人负责挣钱养家");
+    }
+
+    public void eat() {
+        System.out.println("男人多吃肉, 长肌肉");
+    }
+
+    public void walk() {
+        System.out.println("男人霸气的走路");
+    }
+}
