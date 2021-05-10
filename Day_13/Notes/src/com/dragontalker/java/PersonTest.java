788678ed8421cc2663d1d906c6a324fd529cd14c
Richard <richard.yang.tong@gmail.com>
2021-05-10 09:54:46 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/java/PersonTest.java
+++ Day_13/Notes/src/com/dragontalker/java/PersonTest.java
@@ -3,6 +3,9 @@ package com.dragontalker.java;
 public class PersonTest {
     public static void main(String[] args) {
         Person p2 = new Man();
+        p2.eat();
+        p2.walk();
 
+        System.out.println(p2.id);
     }
 }
