01c35b3e1063fb6f2da140ac5764c91d7db6e4c8
Richard <richard.yang.tong@gmail.com>
2021-05-10 16:15:26 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/java2/JUnitTest.java
@@ -0,0 +1,10 @@
+package com.dragontalker.java2;
+
+/*
+Java中的JUnit单元测试
+
+步骤
+ */
+
+public class JUnitTest {
+}
---
+++ Day_13/Notes/src/com/dragontalker/java2/WrapperTest.java
@@ -0,0 +1,28 @@
+package com.dragontalker.java2;
+
+/*
+包装类的使用:
+
+1. Java提供了8中基本数据类型对应的包装类, 使得基本数据类型的变量具有类的特征
+
+2. 掌握的: 基本数据类型、 包装类、String三者之间的转换
+ */
+
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+public class WrapperTest {
+
+    // 基本数据类型 ---> 包装类: 调用包装类的构造器
+    @Test
+    public void test1() {
+         int num1 = 10;
+         Integer in1 = new Integer(num1);
+         System.out.println(in1.toString());
+
+         Integer in2 = new Integer("123");
+        System.out.println(in2.toString());
+    }
+
+}
