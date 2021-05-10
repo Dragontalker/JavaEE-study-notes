27027c2b1ea12d2e5f4f78d14003d0e7f82e8a64
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:02:56 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/exer2/OrderTest.java
@@ -0,0 +1,30 @@
+package com.dragontalker.exer2;
+
+public class OrderTest {
+}
+
+class Order {
+    private int orderId;
+    private String orderName;
+
+    public Order(int orderId, String orderName) {
+        this.orderId = orderId;
+        this.orderName = orderName;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+}
