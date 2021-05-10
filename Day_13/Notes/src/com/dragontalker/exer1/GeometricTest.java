7c8a06b68443b9c97df62d11dd092c28ae0b6a14
Richard <richard.yang.tong@gmail.com>
2021-05-10 11:17:45 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/exer1/GeometricTest.java
@@ -0,0 +1,11 @@
+package com.dragontalker.exer1;
+
+public class GeometricTest {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean equalsArea(GeometricObject o1, GeometricObject o2) {
+        return o1.findArea() == o2.findArea();
+    }
+}
