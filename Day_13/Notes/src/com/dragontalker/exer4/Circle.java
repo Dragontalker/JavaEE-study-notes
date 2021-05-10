c4bb1acd486654e6d3a6d43d6e4495238d496b5e
Richard <richard.yang.tong@gmail.com>
2021-05-10 15:35:35 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/exer4/Circle.java
@@ -0,0 +1,10 @@
+package com.dragontalker.exer4;
+
+public class Circle extends GeometricObject{
+    private double radius;
+
+    public Circle() {
+        super();
+        this.weight = 1.0;
+    }
+}
