9867c9da027d739b290f67ea67fa5cd4fb168f7a
Richard <richard.yang.tong@gmail.com>
2021-05-10 11:39:28 -0400

    Added notes.

---
+++ Day_13/Notes/src/com/dragontalker/java1/ObjectTest.java
@@ -0,0 +1,10 @@
+package com.dragontalker.java1;
+
+/*
+java.lang.Object类
+    1. Object类是所有Java类的根父类
+    2. 如果在类的声明中未使用extends关键字指明其父类, 则默认父类为java.lang.Object类
+ */
+
+public class ObjectTest {
+}
