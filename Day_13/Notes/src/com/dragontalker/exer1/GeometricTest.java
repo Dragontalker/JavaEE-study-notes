ab305e82e5cc0aa6e7b8630673cebb0f6139fd13
Richard <richard.yang.tong@gmail.com>
2021-05-10 11:18:51 -0400

    Added notes.

--- Day_13/Notes/src/com/dragontalker/exer1/GeometricTest.java
+++ Day_13/Notes/src/com/dragontalker/exer1/GeometricTest.java
@@ -5,6 +5,11 @@ public class GeometricTest {
 
     }
 
+    public void displayGeometricObject(GeometricObject o) {
+        System.out.println("面积为: " + o.findArea());
+    }
+
+    // 测试两个对象的面积是否相等
     public boolean equalsArea(GeometricObject o1, GeometricObject o2) {
         return o1.findArea() == o2.findArea();
     }
