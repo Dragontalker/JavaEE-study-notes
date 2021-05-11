44989cf157b4705b859fe79a478a1a78e9d2864c
Richard <richard.yang.tong@gmail.com>
2021-05-11 16:57:49 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java3/LeafTest.java
+++ Day_14/Notes/src/com/dragontalker/java3/LeafTest.java
@@ -32,5 +32,20 @@ class Mid extends Root {
     }
 }
 
+class Leaf extends Mid {
+    static {
+        System.out.println("Leaf的静态初始化块");
+    }
+    {
+        System.out.println("Leaf的普通初始化块");
+    }
+
+    public Leaf() {
+        //通过super调用父类中有一个字符串参数的构造器
+        super("尚硅谷");
+        System.out.println("Leaf的构造器");
+    }
+}
+
 public class LeafTest {
 }
