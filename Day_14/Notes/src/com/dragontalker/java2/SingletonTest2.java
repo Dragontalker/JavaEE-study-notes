c0b84f79a2dcd5d60948585f4c769365405e64e6
Richard <richard.yang.tong@gmail.com>
2021-05-11 11:08:34 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java2/SingletonTest2.java
+++ Day_14/Notes/src/com/dragontalker/java2/SingletonTest2.java
@@ -17,5 +17,14 @@ class Order {
     private Order() {}
 
     //2.声明当前类的对象, 没有初始化
-    private Order instance = null;
+    //4.此对象也必须声明为static的
+    private static Order instance = null;
+
+    //3.声明public、static的返回当前对象的方法
+    public static Order getInstance() {
+        if (instance == null) {
+            instance = new Order();
+        }
+        return instance;
+    }
 }
