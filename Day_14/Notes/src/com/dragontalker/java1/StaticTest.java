270f1dc6b00d576bbc34ae082168b437da8f877d
Richard <richard.yang.tong@gmail.com>
2021-05-11 09:14:27 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java1/StaticTest.java
+++ Day_14/Notes/src/com/dragontalker/java1/StaticTest.java
@@ -6,7 +6,7 @@ static关键字的使用
 1. static: 静态的
 2. static可以用来修饰: 属性、方法、代码块、内部类
 
-3. 使用static修饰属性: 静态变量
+3. 使用static修饰属性: 静态变量(类变量)
     3.1 属性：按是否使用static修饰， 又分为：静态属性 vs 非静态属性(实例变量)
 
     - 实例变量: 我们创建了类的多个对象, 每个对象都独立的拥有一套类中的非静态属性
@@ -15,12 +15,23 @@ static关键字的使用
     - 静态变量: 我们创造了类的多个对象, 多个对象共享同一个变量
         当通过某一个对象修改静态变量时, 会导致其他对象调用此静态变量时, 是修改过了的
 
+    3.2 static修饰属性的其他说明:
+        - 静态变量随着类的加载而加载, 可以通过"类.静态变量"的方式进行调用
+        - 静态变量的加载早于对象的创建
+        - 由于类只会加载一次, 则静态变量在内存中也只会存在一份, 存在方法区的静态域中
+
+        -       | 类变量(static) | 实例变量(non-static)
+        类             yes               no(编译不通过)
+        对象           yes               yes
 
 4. 使用static修饰方法:
  */
 
 public class StaticTest {
     public static void main(String[] args) {
+        Chinese.nation = "中国";
+        System.out.println(Chinese.nation);
+
         Chinese c1 = new Chinese();
         c1.name = "迪丽热巴";
         c1.age = 25;
