8eac91dbedd7d00a1fae008feb2178998079e3fd
Richard <richard.yang.tong@gmail.com>
2021-05-11 11:29:42 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java2/MainTest.java
+++ Day_14/Notes/src/com/dragontalker/java2/MainTest.java
@@ -8,16 +8,15 @@ main()方法的使用说明:
 
 public class MainTest {
     public static void main(String[] args) {
-
+        Main.main(new String[100]);
     }
 }
 
 class Main {
     public static void main(String[] args) {
-        args = new String[100];
         for (int i = 0; i < args.length; i++) {
             args[i] = "args_" + i;
-            System.out.println();
+            System.out.println(args[i]);
         }
     }
 }
