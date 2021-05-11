099d7c38d2282ee5e2410f78db3e095ee8f1efc8
Richard <richard.yang.tong@gmail.com>
2021-05-11 16:56:15 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java3/LeafTest.java
+++ Day_14/Notes/src/com/dragontalker/java3/LeafTest.java
@@ -13,5 +13,24 @@ class Root {
     }
 }
 
+class Mid extends Root {
+    static {
+        System.out.println("Mid的静态初始化块");
+    }
+    {
+        System.out.println("Mid的普通初始化快");
+    }
+
+    public Mid() {
+        System.out.println("Mid的无参数的构造器");
+    }
+
+    public Mid(String msg) {
+        //通过this调用同一类重载的构造器
+        this();
+        System.out.println("Mid的带参数的构造器, 其参数值" + msg);
+    }
+}
+
 public class LeafTest {
 }
