1029056f131927376375e6a6c0af8b7127c1ac39
Richard <richard.yang.tong@gmail.com>
2021-05-10 20:51:18 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java/ReviewTest.java
+++ Day_14/Notes/src/com/dragontalker/java/ReviewTest.java
@@ -8,6 +8,9 @@ public class ReviewTest {
     public void test1() {
         int[] arr = new int[]{1, 2, 3};
         print(arr);
+
+        // 数组也是Object的子类
+        System.out.println(arr.getClass().getSuperclass());
     }
 
     public void print(Object obj) {
