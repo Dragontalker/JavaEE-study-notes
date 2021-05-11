d3515e6293fb1b8c62aab9cc0e0a18f4079de1c7
Richard <richard.yang.tong@gmail.com>
2021-05-11 11:27:31 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java2/MainTest.java
+++ Day_14/Notes/src/com/dragontalker/java2/MainTest.java
@@ -3,10 +3,21 @@ package com.dragontalker.java2;
 /*
 main()方法的使用说明:
     1. 作为程序的入口出现
+    2. main()也是一个普通的静态方法
  */
 
 public class MainTest {
     public static void main(String[] args) {
 
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        args = new String[100];
+        for (int i = 0; i < args.length; i++) {
+            args[i] = "args_" + i;
+            System.out.println();
+        }
     }
 }
