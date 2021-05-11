4a56bbf80f9a35e0765d08335900b74d3324a0f2
Richard <richard.yang.tong@gmail.com>
2021-05-10 20:50:29 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java/ReviewTest.java
+++ Day_14/Notes/src/com/dragontalker/java/ReviewTest.java
@@ -6,6 +6,11 @@ public class ReviewTest {
 
     @Test
     public void test1() {
+        int[] arr = new int[]{1, 2, 3};
+        print(arr);
+    }
 
+    public void print(Object obj) {
+        System.out.println(obj);
     }
 }
