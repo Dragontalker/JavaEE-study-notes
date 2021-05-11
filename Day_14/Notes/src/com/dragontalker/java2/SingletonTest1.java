15429700ab4e81b69c9a7bb816b9d82b16cd25a6
Richard <richard.yang.tong@gmail.com>
2021-05-11 11:04:32 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java2/SingletonTest1.java
+++ Day_14/Notes/src/com/dragontalker/java2/SingletonTest1.java
@@ -10,19 +10,25 @@ package com.dragontalker.java2;
 public class SingletonTest1 {
     public static void main(String[] args) {
 
+        Bank bank1 = Bank.getInstance();
+        Bank bank2 = Bank.getInstance();
+
+        System.out.println(bank1 == bank2); //true 地址值一样
     }
 }
 
+//饿汉式
 class Bank {
 
     //1.私有化构造器
     private Bank() {}
 
     //2.内部创建类的对象
-    private Bank instance = new Bank();
+    //4.要求此对象也必须声明为静态的
+    private static Bank instance = new Bank();
 
     //3.提供公共的方法, 返回类的对象
-    public Bank getInstance() {
+    public static Bank getInstance() {
         return instance;
     }
 }
