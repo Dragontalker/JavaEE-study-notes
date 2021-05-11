4f7de464dc2687eef9cf2fda4380e529cea68b12
Richard <richard.yang.tong@gmail.com>
2021-05-11 10:37:02 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/exer/Account.java
+++ Day_14/Notes/src/com/dragontalker/exer/Account.java
@@ -3,9 +3,11 @@ package com.dragontalker.exer;
 public class Account {
 
     private int id;
-    private String pwd;
+    private String pwd = "000000";
     private double balance;
 
     private static double interestRate;
     private static double minMoney = 1.0;
+    private static int init = 1001; //用于自动生成id使用的
+
 }
