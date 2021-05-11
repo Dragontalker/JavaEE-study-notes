b78a294fb4d3d47b48454b39dd80349b7c18ba93
Richard <richard.yang.tong@gmail.com>
2021-05-11 11:01:37 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java2/SingletonTest1.java
+++ Day_14/Notes/src/com/dragontalker/java2/SingletonTest1.java
@@ -2,8 +2,22 @@ package com.dragontalker.java2;
 
 /*
 单例设计模式:
+    1. 所谓单例设计模式, 就是采取一定的方法保证在整个的软件系统中, 对某个类只能存在一个对象实例
 
+    2. 如何实现?
  */
 
 public class SingletonTest1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Bank {
+
+    //1.私有化构造器
+    private Bank() {}
+
+    //2.内部创建类的对象
+    private Bank instance = new Bank();
 }
