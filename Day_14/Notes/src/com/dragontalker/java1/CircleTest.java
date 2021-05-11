f9e42f05b3a1f0ffb2725dcaff6813e2de621662
Richard <richard.yang.tong@gmail.com>
2021-05-11 10:26:00 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java1/CircleTest.java
+++ Day_14/Notes/src/com/dragontalker/java1/CircleTest.java
@@ -6,4 +6,12 @@ public class CircleTest {
 class Circle {
     private double radius;
     private int id;
+
+    // 创造圆的个数
+    private static int total;
+    private static int init = 1001;
+
+    public double findArea() {
+        return Math.PI * radius * radius;
+    }
 }
