b35ad2e654e0b80dbbd7073f92411452af43b228
Richard <richard.yang.tong@gmail.com>
2021-05-11 17:17:44 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java3/FinalTest.java
+++ Day_14/Notes/src/com/dragontalker/java3/FinalTest.java
@@ -5,7 +5,8 @@ final: 最终的
 
 1. final可以用来修饰类、方法、变量
 
-2. final用来修饰一个类
+2. final用来修饰一个类: 此类不能被其他类所继承
+    - 比如: String类、System类、StringBuffer类
  */
 
 public class FinalTest {
@@ -14,3 +15,5 @@ public class FinalTest {
 final class FinalA {
 
 }
+
+//class B extends FinalA {}
