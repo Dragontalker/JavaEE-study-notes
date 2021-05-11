d1319f1f03e1157d7a74fcb650049011491ee127
Richard <richard.yang.tong@gmail.com>
2021-05-11 10:35:54 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/exer/Account.java
+++ Day_14/Notes/src/com/dragontalker/exer/Account.java
@@ -5,4 +5,7 @@ public class Account {
     private int id;
     private String pwd;
     private double balance;
+
+    private static double interestRate;
+    private static double minMoney = 1.0;
 }
