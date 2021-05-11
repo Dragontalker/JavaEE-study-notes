6fc1cbe64f6cffb238ee2b7dc66e6070e2b4efba
Richard <richard.yang.tong@gmail.com>
2021-05-11 12:07:47 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/java3/BlockTest.java
+++ Day_14/Notes/src/com/dragontalker/java3/BlockTest.java
@@ -9,18 +9,33 @@ package com.dragontalker.java3;
 
 4. 静态代码块
     >内部可以有输出语句
-    >随着类的加载而执行
+    >随着类的加载而执行, 而且只执行一次
 
 5. 非静态代码块
     >内部可以有输出语句
     >随着对象的创建而执行
+    >每创建一个对象, 就执行一次非静态代码块
+    >作用: 可以在创建对象时, 对对象的属性等进行初始化
+
+对属性可以赋值的位置:
+    1. 默认初始化
+    2. 显式初始化
+    3. 构造器中初始化
+    4. 有了对象以后, 可以通过"对象.属性"或"对象.方法"的方式, 进行赋值
+    5. 在代码块中赋值
  */
 
 public class BlockTest {
     public static void main(String[] args) {
         String desc = Person.decs;
+        System.out.println(desc);
+
         Person p1 = new Person();
         Person p2 = new Person();
+
+        System.out.println(p1.age);
+        System.out.println(p2.name);
+        Person.info();
     }
 }
 
@@ -41,11 +56,14 @@ class Person {
     //静态代码块
     static {
         System.out.println("Hello! Static block!");
+        decs = "我是一个爱学习的人";
     }
 
     //非静态代码块
     {
         System.out.println("Hello! Non-static block!");
+        this.age = 1;
+        this.name = "John Doe";
     }
 
     public static void info() {
