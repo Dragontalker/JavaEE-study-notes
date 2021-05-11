28c3a36e778ee0b04b7ace6e2da02aeef9435d9f
Richard <richard.yang.tong@gmail.com>
2021-05-11 10:40:56 -0400

    Added notes.

--- Day_14/Notes/src/com/dragontalker/exer/AccountTest.java
+++ Day_14/Notes/src/com/dragontalker/exer/AccountTest.java
@@ -1,4 +1,9 @@
 package com.dragontalker.exer;
 
 public class AccountTest {
+    public static void main(String[] args) {
+
+        Account acct1 = new Account();
+        Account acct2 = new Account("password", 2000);
+    }
 }
