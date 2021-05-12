d4e88b473740dfd2d69b2f0dd49af2f8fd24f219
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:02:47 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/exer1/Employee.java
@@ -0,0 +1,15 @@
+package com.dragontalker.exer1;
+
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee() {}
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+}
