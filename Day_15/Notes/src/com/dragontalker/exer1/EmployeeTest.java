94561f0145073ac7d2205a297d3336cfc5b01413
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:15:24 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/exer1/EmployeeTest.java
+++ Day_15/Notes/src/com/dragontalker/exer1/EmployeeTest.java
@@ -3,8 +3,15 @@ package com.dragontalker.exer1;
 public class EmployeeTest {
     public static void main(String[] args) {
 
+        //多态
+        Employee ceo = new Manager("乔布斯", 1000, 10000, 100000);
         Manager manager = new Manager("库克", 1001, 5000, 50000);
 
+        ceo.work();
         manager.work();
+
+        Employee worker1 = new CommonEmployee();
+
+        worker1.work();
     }
 }
