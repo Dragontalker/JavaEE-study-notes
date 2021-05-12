adadee836d9425e24e4217a873550ed1374fa2c2
Richard <richard.yang.tong@gmail.com>
2021-05-12 07:13:58 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java/AbstractTest.java
+++ Day_15/Notes/src/com/dragontalker/java/AbstractTest.java
@@ -4,17 +4,20 @@ package com.dragontalker.java;
 abstract关键字的使用
     1. abstract: 抽象的
     2. abstract可以用来修饰的结构、类、方法
+    3. abstract修饰类: 抽象类
+        > 此类不能实例化
  */
 
 public class AbstractTest {
     public static void main(String[] args) {
 
-        Person p1 = new Person();
-        p1.eat();
+        //一旦Person类抽象了, 就不可实例化
+        //Person p1 = new Person();
+        //p1.eat();
     }
 }
 
-class Person {
+abstract class Person {
     String name;
     int age;
 
