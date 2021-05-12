40c993636f9928549e3b61ed49ea4b36865d1c1c
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:13:47 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/exer1/EmployeeTest.java
@@ -0,0 +1,10 @@
+package com.dragontalker.exer1;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        Manager manager = new Manager("库克", 1001, 5000, 50000);
+
+        manager.work();
+    }
+}
