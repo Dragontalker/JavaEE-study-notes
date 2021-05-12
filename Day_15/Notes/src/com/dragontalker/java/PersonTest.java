bb307a2f4ca5858f29ca9c52966bd65b2379d874
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:22:41 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java/PersonTest.java
+++ Day_15/Notes/src/com/dragontalker/java/PersonTest.java
@@ -7,6 +7,23 @@ package com.dragontalker.java;
 public class PersonTest {
     public static void main(String[] args) {
         method(new Student());
+
+        Worker worker = new Worker();
+        method1(worker); //非匿名的类非匿名的对象
+
+        //创建了一个匿名类的对象: p, 右边可以看作一个匿名的子类
+        Person p = new Person(){
+            @Override
+            public void eat() {}
+
+            @Override
+            public void breath() {}
+        };
+    }
+
+    public static void method1(Person p) {
+        p.eat();
+        p.walk();
     }
 
     public static void method(Student s) {}
