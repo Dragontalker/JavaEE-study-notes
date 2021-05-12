fedc4c0f3b3b093ad67919c473e501f6a27c163f
Richard <richard.yang.tong@gmail.com>
2021-05-12 19:44:41 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/java8/SubClassTest.java
@@ -0,0 +1,28 @@
+package com.dragontalker.java8;
+
+public class SubClassTest {
+    public static void main(String[] args) {
+        SubClass s = new SubClass();
+
+        //知识点1: 接口中定义的静态方法, 只能通过接口调用
+        //s.method1();
+        CompareA.method1();
+
+        //知识点2: 通过实现类的对象, 可以调用接口中的默认方法
+        s.method2();
+        s.method3();
+    }
+}
+
+class SubClass implements CompareA {
+
+    @Override
+    public void method2() {
+        System.out.println("SubClass: 上海");
+    }
+
+    @Override
+    public void method3() {
+        System.out.println("SubClass: 深圳");
+    }
+}
---
+++ Day_15/Notes/src/com/dragontalker/java8/SuperClass.java
@@ -0,0 +1,7 @@
+package com.dragontalker.java8;
+
+public class SuperClass {
+    public void method3() {
+        System.out.println("SuperClass: 深圳");
+    }
+}
