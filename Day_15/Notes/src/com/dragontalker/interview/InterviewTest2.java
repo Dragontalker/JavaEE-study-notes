c1dd274cca467089b316516a368f31e059715590
Richard <richard.yang.tong@gmail.com>
2021-05-12 19:13:19 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/interview/InterviewTest2.java
@@ -0,0 +1,33 @@
+package com.dragontalker.interview;
+
+interface Playable {
+    void play();
+}
+
+interface Bounceable {
+    void play();
+}
+
+interface Rollable extends Playable, Bounceable {
+    Ball ball = new Ball("PingPong");
+}
+
+class Ball implements Rollable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Ball(String name) {
+        this.name = name;
+    }
+
+    public void play() {
+        ball = new Ball("Football"); //ball是常量, 所以不能调用
+        System.out.println(ball.getName());
+    }
+}
+
+public class InterviewTest2 {
+}
