0bc8ae13ef94e79a45acffd0e62c4bcaa94c5249
Richard <richard.yang.tong@gmail.com>
2021-05-12 11:18:28 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/exer2/MyDate.java
+++ Day_15/Notes/src/com/dragontalker/exer2/MyDate.java
@@ -1,9 +1,15 @@
 package com.dragontalker.exer2;
 
 public class MyDate {
-    private int year;
-    private int month;
-    private int day;
+    private final int year;
+    private final int month;
+    private final int day;
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 
     public String toDateString() {
         return year + "年" + month + "月" + day + "日";
--- Day_15/Notes/src/com/dragontalker/exer2/PayrollProblem.java
+++ Day_15/Notes/src/com/dragontalker/exer2/PayrollProblem.java
@@ -2,6 +2,9 @@ package com.dragontalker.exer2;
 
 public class PayrollProblem {
     public static void main(String[] args) {
+        Employee[] emps = new Employee[2];
 
+        emps[0] = new SalariedEmployee("马森", 1002, new MyDate(1992, 2, 28));
+        emps[1] = new HourlyEmployee("潘雨生", 2001, new MyDate(1991, 5, 6));
     }
 }
