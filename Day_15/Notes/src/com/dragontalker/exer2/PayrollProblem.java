6afc756b25703540b5d7fe64a14b6db1f83f5deb
Richard <richard.yang.tong@gmail.com>
2021-05-12 11:22:00 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/exer2/PayrollProblem.java
+++ Day_15/Notes/src/com/dragontalker/exer2/PayrollProblem.java
@@ -4,7 +4,7 @@ public class PayrollProblem {
     public static void main(String[] args) {
         Employee[] emps = new Employee[2];
 
-        emps[0] = new SalariedEmployee("马森", 1002, new MyDate(1992, 2, 28));
-        emps[1] = new HourlyEmployee("潘雨生", 2001, new MyDate(1991, 5, 6));
+        emps[0] = new SalariedEmployee("马森", 1002, new MyDate(1992, 2, 28), 10000);
+        emps[1] = new HourlyEmployee("潘雨生", 2001, new MyDate(1991, 5, 6), 60, 240);
     }
 }
