e0df603fa4cb589aa24320b62eabd3f1e1b8dc39
Richard <richard.yang.tong@gmail.com>
2021-05-12 11:46:42 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java1/InterfaceTest.java
+++ Day_15/Notes/src/com/dragontalker/java1/InterfaceTest.java
@@ -4,6 +4,15 @@ package com.dragontalker.java1;
 接口的使用:
     1. 接口使用interface来定义
     2. Java中, 接口和类是并列的两个结构
+    3. 如何定义接口, 定义接口中的成员
+
+        3.1 JDK7及以前:
+            > 只能定义全局常量和抽象方法
+            > 全局常量: public static final
+            > 抽象方法: public abstract
+
+        3.2 JDK8:
+            > 除了定义全局常量和抽象方法外, 还可以定义静态方法, 默认方法(略)
  */
 
 public class InterfaceTest {
