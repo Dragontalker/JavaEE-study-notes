9814750d14d91e67042706328b6194e4c3e33865
Richard <richard.yang.tong@gmail.com>
2021-05-12 06:36:38 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java/SingletonTest.java
+++ Day_15/Notes/src/com/dragontalker/java/SingletonTest.java
@@ -2,7 +2,13 @@ package com.dragontalker.java;
 
 public class SingletonTest {
     public static void main(String[] args) {
+        Bank bank1 = Bank.instance;
+        Bank bank2 = Bank.instance;
+        System.out.println(bank1 == bank2);
 
+        Bank.instance = null;
+
+        System.out.println(bank1 == bank2);
     }
 }
 
