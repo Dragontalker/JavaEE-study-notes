e671aedb25edd5fe3ade634aa9b303a18fc174fd
Richard <richard.yang.tong@gmail.com>
2021-05-12 19:10:17 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/interview/InterviewTest1.java
+++ Day_15/Notes/src/com/dragontalker/interview/InterviewTest1.java
@@ -12,7 +12,7 @@ class C extends B implements A {
     public void pX() {
         //System.out.println(x); Error: ambiguous definition of x
         System.out.println(super.x); //1
-        System.out.println(A.x); //0 记住接口中的是全局常量
+        System.out.println(A.x); //0 记住接口中的是全局常量 public static final
     }
     public static void main(String[] args) {
         new C().pX();
