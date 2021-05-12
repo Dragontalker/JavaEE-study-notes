4541917a5ac7a82d4ad27182460b10c66a6fa208
Richard <richard.yang.tong@gmail.com>
2021-05-12 19:20:36 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/exer3/ComparableCircle.java
@@ -0,0 +1,24 @@
+package com.dragontalker.exer3;
+
+public class ComparableCircle extends Circle implements CompareObject{
+
+
+    @Override
+    public int compareTo(Object o) {
+        if (this == o) {
+            return 0;
+        }
+        if (o instanceof Circle) {
+            Circle c = (Circle)o;
+            if (this.getRadius() > c.getRadius()) {
+                return 1;
+            } else if (this.getRadius() < c.getRadius()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } else {
+            throw new RuntimeException("传入的数据类型不匹配");
+        }
+    }
+}
