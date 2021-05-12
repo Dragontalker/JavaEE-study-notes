1b81595c06b6c59c64c24ef64889bc3c1b7448a9
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:53:19 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java/TemplateTest.java
+++ Day_15/Notes/src/com/dragontalker/java/TemplateTest.java
@@ -22,3 +22,21 @@ abstract class Template {
 
     public abstract void code();
 }
+
+class SubTemplate extends Template {
+     @Override
+    public void code() {
+         for (int i = 0; i <=1000; i++) {
+             boolean isFlag = true;
+             for (int j = 2; j <= Math.sqrt(i); j++) {
+                 if (i % j == 0) {
+                     isFlag = false;
+                     break;
+                 }
+             }
+             if (isFlag) {
+                 System.out.println(i);
+             }
+         }
+     }
+}
