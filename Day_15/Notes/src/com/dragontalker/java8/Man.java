1d3e81728bf8a1c7b5e01fe8db82f1a37876e34d
Richard <richard.yang.tong@gmail.com>
2021-05-12 19:59:14 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java8/Man.java
+++ Day_15/Notes/src/com/dragontalker/java8/Man.java
@@ -12,5 +12,12 @@ interface Spoony {
     }
 }
 
-public class Man {
+public class Man implements Filial, Spoony{
+
+    @Override
+    public void help() {
+        System.out.println("我该怎么办呢?");
+        Filial.super.help();
+        Spoony.super.help();
+    }
 }
