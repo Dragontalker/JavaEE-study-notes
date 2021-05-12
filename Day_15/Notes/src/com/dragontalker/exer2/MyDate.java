2dd2b916395ea1c8d7b001483d1e2951af51f9a4
Richard <richard.yang.tong@gmail.com>
2021-05-12 10:03:19 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/exer2/MyDate.java
+++ Day_15/Notes/src/com/dragontalker/exer2/MyDate.java
@@ -1,4 +1,11 @@
 package com.dragontalker.exer2;
 
 public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    public String toDateString() {
+        return year + "年" + month + "月" + day + "日";
+    }
 }
