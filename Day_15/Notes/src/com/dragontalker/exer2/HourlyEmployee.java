d4207dbbc49d3480e31ee7628cb4fd206d5a4814
Richard <richard.yang.tong@gmail.com>
2021-05-12 11:19:12 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/exer2/HourlyEmployee.java
+++ Day_15/Notes/src/com/dragontalker/exer2/HourlyEmployee.java
@@ -8,6 +8,12 @@ public class HourlyEmployee extends Employee{
         super(name, number, birthday);
     }
 
+    public HourlyEmployee(String name, int number, MyDate birthday, int wage, int hour) {
+        super(name, number, birthday);
+        this.wage = wage;
+        this.hour = hour;
+    }
+
     @Override
     public double earnings() {
         return wage * hour;
