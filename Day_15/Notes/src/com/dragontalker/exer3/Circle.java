479b97556fd1db297881a08e73a74a8ac55258dd
Richard <richard.yang.tong@gmail.com>
2021-05-12 19:16:00 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/exer3/Circle.java
@@ -0,0 +1,20 @@
+package com.dragontalker.exer3;
+
+public class Circle {
+
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {}
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
