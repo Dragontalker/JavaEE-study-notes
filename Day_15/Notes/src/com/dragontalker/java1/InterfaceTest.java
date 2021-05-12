9ee8b49f8768ef18f2eb1be2fa2fad4db3440efd
Richard <richard.yang.tong@gmail.com>
2021-05-12 11:53:31 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java1/InterfaceTest.java
+++ Day_15/Notes/src/com/dragontalker/java1/InterfaceTest.java
@@ -8,19 +8,46 @@ package com.dragontalker.java1;
 
         3.1 JDK7及以前:
             > 只能定义全局常量和抽象方法
-            > 全局常量: public static final
-            > 抽象方法: public abstract
+            > 全局常量: public static final的, 但是可以忽略不写
+            > 抽象方法: public abstract的, 但是可以忽略不写
 
         3.2 JDK8:
             > 除了定义全局常量和抽象方法外, 还可以定义静态方法, 默认方法(略)
+
+    4. 接口中不能定义构造器的! 意味着接口不能实例化!
+
+    5. Java开发中, 接口通过让类去实现(implements)的
  */
 
 public class InterfaceTest {
+    public static void main(String[] args) {
+        System.out.println(Flyable.MAX_SPEED);
+        System.out.println(Flyable.MIN_SPEED);
+    }
 }
 
 interface Flyable {
 
     //全局常量
     int MAX_SPEED = 7900; //第一宇宙速度
-    int MIN_SPEED = 1;
+    int MIN_SPEED = 1; //省略了public static final
+
+    //抽象方法
+    public abstract void fly();
+
+    //省略了public abstract
+    void stop();
+}
+
+class Plane implements Flyable {
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
