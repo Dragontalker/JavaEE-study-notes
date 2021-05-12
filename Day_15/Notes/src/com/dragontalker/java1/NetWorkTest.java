8becab770ab4f3d4cf36db073369670de76c046d
Richard <richard.yang.tong@gmail.com>
2021-05-12 14:10:21 -0400

    Added notes.

---
+++ Day_15/Notes/src/com/dragontalker/java1/NetWorkTest.java
@@ -0,0 +1,41 @@
+package com.dragontalker.java1;
+
+/*
+接口的应用: 代理模式
+ */
+
+public class NetWorkTest {
+}
+
+interface NetWork {
+    void browse();
+}
+
+//被代理类
+class Server implements NetWork {
+
+    @Override
+    public void browse() {
+        System.out.println("真实的服务器访问网络");
+    }
+}
+
+
+//被代理类
+class ProxyServer implements NetWork {
+    private final NetWork work;
+
+    public ProxyServer(NetWork work) {
+        this.work = work;
+    }
+
+    public void check() {
+        System.out.println("联网之前的检查工作");
+    }
+
+    @Override
+    public void browse() {
+        check();
+        work.browse();
+    }
+}
