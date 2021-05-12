30b5fa35c96a5f93f53a53d2238655d4a3cd1f8a
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:51:18 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/java/TemplateTest.java
+++ Day_15/Notes/src/com/dragontalker/java/TemplateTest.java
@@ -6,3 +6,19 @@ package com.dragontalker.java;
 
 public class TemplateTest {
 }
+
+abstract class Template {
+
+    // 计算某段代码执行所需要花费的时间
+    public void spendTime() {
+        long start = System.currentTimeMillis();
+
+        code(); // 不确定的部分, 易变的部分
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("花费的时间为: " + (end - start));
+    }
+
+    public abstract void code();
+}
