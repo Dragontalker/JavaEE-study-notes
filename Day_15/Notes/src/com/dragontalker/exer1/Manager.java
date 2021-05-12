63b0e7574b73bfd62c3fef6ea5511a3d3d18670d
Richard <richard.yang.tong@gmail.com>
2021-05-12 09:05:13 -0400

    Added notes.

--- Day_15/Notes/src/com/dragontalker/exer1/Manager.java
+++ Day_15/Notes/src/com/dragontalker/exer1/Manager.java
@@ -4,6 +4,15 @@ public class Manager extends Employee{
 
     private double bonus;
 
+    public Manager(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Manager(String name, int id, double salary, double bonus) {
+        super(name, id, salary);
+        this.bonus = bonus;
+    }
+
     @Override
     public void work() {
         System.out.println("管理员工, 提高公司运行的效率");
