2d914475065c4a35263ddd57752b4114d7e2ab8e
Richard <richard.yang.tong@gmail.com>
2021-05-13 13:28:36 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/ExceptionTest.java
+++ Day_16/Notes/src/com/dragontalker/java/ExceptionTest.java
@@ -7,6 +7,9 @@ java.lang.Throwable
     |---> java.lang.Error: 一般不编写针对性的代码处理
     |---> java.lang.Exception: 可以进行异常的处理
         |---> 编译时异常(checked)
+            |---> IOException
+                |---> FileNotFoundException
+            |---> ClassNotFoundException
         |---> 运行时异常(unchecked)
  */
 
