13aaac688a83c6f6f8a351845a0642d19dc7a1de
Richard <richard.yang.tong@gmail.com>
2021-05-13 17:32:31 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java2/MyException.java
+++ Day_16/Notes/src/com/dragontalker/java2/MyException.java
@@ -1,4 +1,18 @@
 package com.dragontalker.java2;
 
+/*
+如何自定义异常类?
+    1. 继承于现有的异常结构: RuntimeException, Exception
+    2.
+ */
+
 public class MyException extends RuntimeException{
+
+    static final long serialVersionUID = -3387516993124229948L;
+
+    public MyException() {}
+
+    public MyException(String msg) {
+        super(msg);
+    }
 }
