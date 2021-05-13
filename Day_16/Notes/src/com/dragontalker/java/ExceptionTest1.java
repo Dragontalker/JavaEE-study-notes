789f49949c9cc394414a4081699f2af5aecd27ff
Richard <richard.yang.tong@gmail.com>
2021-05-13 14:13:03 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/ExceptionTest1.java
+++ Day_16/Notes/src/com/dragontalker/java/ExceptionTest1.java
@@ -1,7 +1,7 @@
 package com.dragontalker.java;
 
 /*
-异常的处理: 抓抛模型
+一、异常的处理: 抓抛模型
 
 过程一: "抛": 程序在正常执行的过程中, 一旦出现异常, 就会在异常代码处生成一个对应异常类的对象
     并将此对象抛出. 一旦抛出对象以后, 其后的代码就不再执行.
@@ -9,6 +9,18 @@ package com.dragontalker.java;
 过程二: "抓": 可以理解为异常的处理方式:
     - 1. try-catch-finally
     - 2. throws
+
+二、try-catch-finally的使用
+
+try {
+    //可能出现异常的代码
+} catch(异常类型1 变量名1) {
+    //处理异常的方式1
+} catch(异常类型2 变量名2) {
+    //处理异常的方式2
+} catch(异常类型3 变量名3) {
+    //处理异常的方式3
+}
  */
 
 public class ExceptionTest1 {
