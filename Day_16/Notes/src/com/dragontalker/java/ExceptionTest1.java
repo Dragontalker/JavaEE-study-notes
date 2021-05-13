9aa709848b231baacc91c6c86a0f42780531b9db
Richard <richard.yang.tong@gmail.com>
2021-05-13 14:10:47 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/ExceptionTest1.java
+++ Day_16/Notes/src/com/dragontalker/java/ExceptionTest1.java
@@ -6,7 +6,9 @@ package com.dragontalker.java;
 过程一: "抛": 程序在正常执行的过程中, 一旦出现异常, 就会在异常代码处生成一个对应异常类的对象
     并将此对象抛出. 一旦抛出对象以后, 其后的代码就不再执行.
 
-
+过程二: "抓": 可以理解为异常的处理方式:
+    - 1. try-catch-finally
+    - 2. throws
  */
 
 public class ExceptionTest1 {
