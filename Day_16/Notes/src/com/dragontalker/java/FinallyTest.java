b25e0aaa37b080b98e6e1926212092eb9e9b2b3c
Richard <richard.yang.tong@gmail.com>
2021-05-13 14:55:18 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/FinallyTest.java
+++ Day_16/Notes/src/com/dragontalker/java/FinallyTest.java
@@ -7,6 +7,11 @@ try-catch-finally当中finally的使用
 2. finally中声明的是一定会被执行的代码. 即使catch中又出现异常了,
     - try中有return语句,
     - catch中有return语句.
+
+3. 像数据库连接, 输入输出流, 网络编程Socket等资源
+    - JVM是不能自动回收的
+    - 我们需要手动的进行资源释放
+    - 此时的资源释放, 就需要声明在finally中
  */
 
 import org.junit.Test;
