edaa0965d6d7fcfd7caf6bd29cbab4117678d7cf
Richard <richard.yang.tong@gmail.com>
2021-05-13 12:06:51 -0400

    Added notes.

---
+++ Day_16/Notes/src/com/dragontalker/java/ErrorTest.java
@@ -0,0 +1,13 @@
+package com.dragontalker.java;
+
+/*
+Error:
+    - Java虚拟机都无法解决的严重问题, 如:
+        > JVM系统内部错误
+        > 资源耗尽等严重情况, 比如StackOverflow
+
+一般不编写针对性的代码处理
+ */
+
+public class ErrorTest {
+}
