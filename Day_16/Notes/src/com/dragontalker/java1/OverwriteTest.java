9ef0de0e3c037af64bb2a9dcc60c82d7a85e06b2
Richard <richard.yang.tong@gmail.com>
2021-05-13 16:53:58 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java1/OverwriteTest.java
+++ Day_16/Notes/src/com/dragontalker/java1/OverwriteTest.java
@@ -1,4 +1,23 @@
 package com.dragontalker.java1;
 
+/*
+方法重写的规则之一:
+    - 子类重写的方法抛出的异常类型不大于父类被重写的方法抛出的异常类型
+ */
+
+import java.io.IOException;
+
 public class OverwriteTest {
+}
+
+class SuperClass {
+    public void method() throws IOException {
+
+    }
+}
+
+class SubClass extends SuperClass {
+    public void method() {
+
+    }
 }
