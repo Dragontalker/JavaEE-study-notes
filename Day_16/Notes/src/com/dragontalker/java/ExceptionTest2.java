84387cfaa7a694b9a6446af789b4d7af151ec7ab
Richard <richard.yang.tong@gmail.com>
2021-05-13 16:39:37 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/ExceptionTest2.java
+++ Day_16/Notes/src/com/dragontalker/java/ExceptionTest2.java
@@ -4,5 +4,21 @@ package com.dragontalker.java;
 异常方处理的方式二: throws + 异常类型
  */
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ExceptionTest2 {
+    public void method1() throws FileNotFoundException, IOException {
+        File file = new File("hello.txt");
+        FileInputStream fis = new FileInputStream(file);
+
+        int data = fis.read();
+        while(data != -1) {
+            System.out.println((char)data);
+            data = fis.read();
+        }
+        fis.close();
+    }
 }
