38a0e173044ac88ed7922d339e48b89adf3e958d
Richard <richard.yang.tong@gmail.com>
2021-05-13 17:55:15 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/exer1/EcmDef.java
+++ Day_16/Notes/src/com/dragontalker/exer1/EcmDef.java
@@ -3,12 +3,25 @@ package com.dragontalker.exer1;
 public class EcmDef {
     public static void main(String[] args) {
 
+        try {
+            int i = Integer.parseInt(args[0]);
+            int j = Integer.parseInt(args[1]);
+            int result = ecm(i, j);
+        } catch (NumberFormatException e) {
+            System.out.println("数据类型不一致");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("缺少命令行参数");
+        } catch (ArithmeticException e) {
+            System.out.println("除0");
+        } catch (EcDef e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public int divide(int i, int j) throws EcDef{
+    public static int ecm(int i, int j) throws EcDef{
         if (i < 0 || j < 0) {
             throw new EcDef("分子或分母为负数啦!");
         }
-        return 1;
+        return i / j;
     }
 }
