5c535cc33d36a54ad30cb907bf3c91e1144cfeed
Richard <richard.yang.tong@gmail.com>
2021-05-13 17:15:00 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java2/StudentTest.java
+++ Day_16/Notes/src/com/dragontalker/java2/StudentTest.java
@@ -1,6 +1,11 @@
 package com.dragontalker.java2;
 
 public class StudentTest {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.register(-1001);
+        System.out.println(s);
+    }
 }
 
 class Student {
