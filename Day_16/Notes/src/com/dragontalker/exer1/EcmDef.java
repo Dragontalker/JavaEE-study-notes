7d0c3d3883a5ef889d9e4f6279788b975007295e
Richard <richard.yang.tong@gmail.com>
2021-05-13 17:47:56 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/exer1/EcmDef.java
+++ Day_16/Notes/src/com/dragontalker/exer1/EcmDef.java
@@ -4,4 +4,10 @@ public class EcmDef {
     public static void main(String[] args) {
 
     }
+
+    public int divide(int i, int j) {
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+    }
 }
