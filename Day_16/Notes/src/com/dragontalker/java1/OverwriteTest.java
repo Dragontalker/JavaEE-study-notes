5ffa1e2d00330531653501cdec37469972478492
Richard <richard.yang.tong@gmail.com>
2021-05-13 16:54:26 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java1/OverwriteTest.java
+++ Day_16/Notes/src/com/dragontalker/java1/OverwriteTest.java
@@ -5,19 +5,16 @@ package com.dragontalker.java1;
     - 子类重写的方法抛出的异常类型不大于父类被重写的方法抛出的异常类型
  */
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class OverwriteTest {
 }
 
 class SuperClass {
-    public void method() throws IOException {
-
-    }
+    public void method() throws IOException {}
 }
 
 class SubClass extends SuperClass {
-    public void method() {
-
-    }
+    public void method() throws FileNotFoundException {}
 }
