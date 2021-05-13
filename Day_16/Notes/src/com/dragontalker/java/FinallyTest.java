5055049df401ca670e236a1441db37e079e2a6b2
Richard <richard.yang.tong@gmail.com>
2021-05-13 14:43:34 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/FinallyTest.java
+++ Day_16/Notes/src/com/dragontalker/java/FinallyTest.java
@@ -2,7 +2,25 @@ package com.dragontalker.java;
 
 /*
 try-catch-finally当中finally的使用
+
+1. finally是可选的
+2.
  */
 
+import org.junit.Test;
+
 public class FinallyTest {
+
+    @Test
+    public void test1() {
+        try {
+            int a = 10;
+            int b = 0;
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
