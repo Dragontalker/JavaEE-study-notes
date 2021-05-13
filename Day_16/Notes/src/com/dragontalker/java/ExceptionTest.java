cabc87220782ab62b7acc6949e9566eabea98051
Richard <richard.yang.tong@gmail.com>
2021-05-13 13:41:00 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/ExceptionTest.java
+++ Day_16/Notes/src/com/dragontalker/java/ExceptionTest.java
@@ -21,5 +21,29 @@ java.lang.Throwable
 面试题: 常见的异常都有哪些？举例说明: 空指针、角标越界
  */
 
+import org.junit.Test;
+
 public class ExceptionTest {
+
+    //NullPointerException
+    @Test
+    public void test1() {
+        int[] arr = new int[]{1,2, 3};
+        arr = null;
+        System.out.println(arr[3]);
+    }
+
+    //ArrayIndexOutOfBoundException
+    @Test
+    public void test2() {
+        int[] arr = new int[10];
+        System.out.println(arr[10]);
+    }
+
+    //StringIndexOutOfBoundException
+    @Test
+    public void test3() {
+        String str = "abc";
+        System.out.println(str.charAt(3));
+    }
 }
