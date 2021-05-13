7c9dbcd3e988e9620524932dfc8fc53fb6232c39
Richard <richard.yang.tong@gmail.com>
2021-05-13 11:50:46 -0400

    Added notes.

--- Day_16/Notes/src/com/dragontalker/java/InnerClassTest.java
+++ Day_16/Notes/src/com/dragontalker/java/InnerClassTest.java
@@ -4,6 +4,9 @@ public class InnerClassTest {
     /*
     在局部内部类的方法中(比如: show) 如果调用局部内部类所声明的方法(比如:method)中的局部变量
     (num)要求此局部变量声明为final的
+
+    JDK7及之前的版本: 要求此局部变量显式的声明为final的
+    JDK8及之后的版本: 可以省略final的声明
      */
 
     public void method() {
@@ -16,6 +19,7 @@ public class InnerClassTest {
                 //final int num = 10;
                 //所以, num = 20;就会报错
                 //理解: 子结构不能更改父结构的参数
+                //final之后, 传入的实际上是一个num的副本, 而不是num本身
                 System.out.println(num);
             }
         }
