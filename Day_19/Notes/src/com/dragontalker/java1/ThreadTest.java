5dfa35196350f30a915c7c96543800e3eb0f5c5c
Richard <richard.yang.tong@gmail.com>
2021-05-14 18:37:27 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java1/ThreadTest.java
+++ Day_19/Notes/src/com/dragontalker/java1/ThreadTest.java
@@ -8,9 +8,26 @@ public class ThreadTest {
 
     public static void main(String[] args) {
 
+        StringBuffer s1 = new StringBuffer();
+        StringBuffer s2 = new StringBuffer();
+
         new Thread(){
             @Override
-            public void run() {}
+            public void run() {
+
+                synchronized (s1) {
+                    s1.append("a");
+                    s2.append("1");
+
+                    synchronized (s2) {
+                        s1.append("b");
+                        s2.append("2");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
         }.start();
     }
 }
