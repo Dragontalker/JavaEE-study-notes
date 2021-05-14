3b103bd9d739b6adb8f6f02001c7f69e2137e6a2
Richard <richard.yang.tong@gmail.com>
2021-05-14 10:05:59 -0400

    Added notes.

--- Day_18/Notes/src/com/dragontalker/java/ThreadTest1.java
+++ Day_18/Notes/src/com/dragontalker/java/ThreadTest1.java
@@ -23,4 +23,11 @@ class MThread implements Runnable {
 }
 
 public class ThreadTest1 {
+    public static void main(String[] args) {
+        //3. 创建实现类的对象
+        MThread mThread = new MThread();
+
+        //4. 将此对象作为参数传递到Thread类的构造器中, 创建Thread类的对象
+        Thread t1 = new Thread(mThread);
+    }
 }
