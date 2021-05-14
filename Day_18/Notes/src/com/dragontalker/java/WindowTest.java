1dd5ab8f64508b1d5361785ded7338ed315525b3
Richard <richard.yang.tong@gmail.com>
2021-05-14 09:48:07 -0400

    Added notes.

--- Day_18/Notes/src/com/dragontalker/java/WindowTest.java
+++ Day_18/Notes/src/com/dragontalker/java/WindowTest.java
@@ -5,9 +5,19 @@ package com.dragontalker.java;
  */
 
 class Window extends Thread {
+    private int ticket = 100;
+
     @Override
     public void run() {
-        super.run();
+
+        while(true) {
+            if (ticket > 0) {
+                System.out.println(getName() + ": 卖票, 票号为: " + ticket);
+                ticket--;
+            } else {
+                break;
+            }
+        }
     }
 }
 
