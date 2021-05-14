77bbcd75320fbc939e5fdbaa8ffc0209680e2639
Richard <richard.yang.tong@gmail.com>
2021-05-14 06:04:42 -0400

    Added notes.

---
+++ Day_18/Notes/src/com/dragontalker/java/ThreadTest.java
@@ -0,0 +1,12 @@
+package com.dragontalker.java;
+
+/*
+多线程的创建, 方式一: 继承于Thread类
+    1. 创建一个继承于Thread类的子类
+    2. 重写Thread类的run()方法
+    3. 创建Thread类的子类的对象
+    4. 通过此对调用start()方法
+ */
+
+public class ThreadTest {
+}
