b3d897cca6667c74c4e11a4cc44c67f7bedade2e
Richard <richard.yang.tong@gmail.com>
2021-05-14 15:41:33 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java/WindowTest1.java
+++ Day_19/Notes/src/com/dragontalker/java/WindowTest1.java
@@ -4,7 +4,10 @@ package com.dragontalker.java;
 例子: 创建三个窗口卖票, 总票数为100张, 使用实现Runnable接口的方式
 
 1. 问题: 卖票过程中, 出现了重票、错票 ---> 出现了线程安全问题
-2. 问题出现的原因:
+2. 问题出现的原因: 当某个线程操作车票的过程中, 尚未完成操作时, 其他线程参与进来, 也操作车票
+3. 如何解决:
+    - 当一个线程在操作ticket的时候, 其他线程不能参与进来.
+    - 直到线程a操作完ticket的时候, 其他线程才可以开始操作ticket.
  */
 
 class Window1 implements Runnable {
@@ -15,11 +18,11 @@ class Window1 implements Runnable {
         while(true) {
             if (ticket > 0) {
 
-                try {
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    Thread.sleep(200);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
 
                 System.out.println(Thread.currentThread().getName() + ": 卖票, 票号为: " + ticket);
                 ticket--;
