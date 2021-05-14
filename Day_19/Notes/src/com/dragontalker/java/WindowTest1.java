ab9cc727056ef52b2c72cc90d233bb744ed09df7
Richard <richard.yang.tong@gmail.com>
2021-05-14 15:03:31 -0400

    Added notes.

---
+++ Day_19/Notes/src/com/dragontalker/java/WindowTest1.java
@@ -0,0 +1,35 @@
+package com.dragontalker.java;
+
+/*
+例子: 创建三个窗口卖票, 总票数为100张, 使用实现Runnable接口的方式
+ */
+
+class Window1 implements Runnable {
+    private int ticket = 100;
+
+    @Override
+    public void run() {
+        while(true) {
+            if (ticket > 0) {
+                System.out.println(Thread.currentThread().getName() + ": 卖票, 票号为: " + ticket);
+                ticket--;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+public class WindowTest1 {
+    public static void main(String[] args) {
+        Window1 w = new Window1();
+
+        Thread t1 = new Thread(w);
+        Thread t2 = new Thread(w);
+        Thread t3 = new Thread(w);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
