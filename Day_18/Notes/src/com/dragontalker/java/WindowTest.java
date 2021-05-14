e57b0c50bebc6330dd6d207c72e009ad7523efef
Richard <richard.yang.tong@gmail.com>
2021-05-14 09:46:10 -0400

    Added notes.

--- Day_18/Notes/src/com/dragontalker/java/WindowTest.java
+++ Day_18/Notes/src/com/dragontalker/java/WindowTest.java
@@ -4,5 +4,12 @@ package com.dragontalker.java;
 例子: 创建三个窗口卖票, 总票数为100张
  */
 
+class Window extends Thread {
+    @Override
+    public void run() {
+        super.run();
+    }
+}
+
 public class WindowTest {
 }
