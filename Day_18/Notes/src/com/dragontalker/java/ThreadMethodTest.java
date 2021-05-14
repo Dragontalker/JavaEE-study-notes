2158489409e8f45b1a37a38cb50c81fcadcf599a
Richard <richard.yang.tong@gmail.com>
2021-05-14 06:43:12 -0400

    Added notes.

---
+++ Day_18/Notes/src/com/dragontalker/java/ThreadMethodTest.java
@@ -0,0 +1,13 @@
+package com.dragontalker.java;
+
+/*
+测试Thread中的常用方法:
+    1. start(): 启动当前线程: 调用当前线程的run()
+    2. run(): 通常需要重写Thread类中的此方法, 将创建的线程要执行的操作声明在此方法中
+    3. currentThread(): 静态方法, 返回执行当前代码的线程
+    4. getName(): 获取当前线程的名字
+    5. setName(): 设置当前线程的名字
+ */
+
+public class ThreadMethodTest {
+}
