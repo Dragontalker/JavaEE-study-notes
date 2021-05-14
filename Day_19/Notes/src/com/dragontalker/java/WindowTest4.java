c5067ea7d44eea877bd1cd5d866b6751c9172002
Richard <richard.yang.tong@gmail.com>
2021-05-14 18:07:10 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java/WindowTest4.java
+++ Day_19/Notes/src/com/dragontalker/java/WindowTest4.java
@@ -1,16 +1,17 @@
 package com.dragontalker.java;
 
-class Window4 implements Runnable {
+class Window4 extends Thread {
     private static int ticket = 100;
 
     @Override
     public void run() {
-        while(true) {
-
+        while(ticket > 0) {
+            show();
         }
     }
 
-    private void show() {
+    private static synchronized void show() {//同步监视器: Window4.class
+        //private static synchronized void show() {//同步监视器: w1, w2, w3, 错误的方法
         if (ticket > 0) {
 
             try {
@@ -27,14 +28,12 @@ class Window4 implements Runnable {
 
 public class WindowTest4 {
     public static void main(String[] args) {
-        Window4 w = new Window4();
+        Window4 w1 = new Window4();
+        Window4 w2 = new Window4();
+        Window4 w3 = new Window4();
 
-        Thread t1 = new Thread(w);
-        Thread t2 = new Thread(w);
-        Thread t3 = new Thread(w);
-
-        t1.start();
-        t2.start();
-        t3.start();
+        w1.start();
+        w2.start();
+        w3.start();
     }
 }
