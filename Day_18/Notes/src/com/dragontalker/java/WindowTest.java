91e5221201960961f697175013f2b82a20231768
Richard <richard.yang.tong@gmail.com>
2021-05-14 09:49:47 -0400

    Added notes.

--- Day_18/Notes/src/com/dragontalker/java/WindowTest.java
+++ Day_18/Notes/src/com/dragontalker/java/WindowTest.java
@@ -30,5 +30,9 @@ public class WindowTest {
         w1.setName("窗口1");
         w2.setName("窗口2");
         w3.setName("窗口3");
+
+        w1.start();
+        w2.start();
+        w3.start();
     }
 }
