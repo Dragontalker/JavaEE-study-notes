154a854860a8385c4b5618236f6772e95162d888
Richard <richard.yang.tong@gmail.com>
2021-05-14 10:06:26 -0400

    Added notes.

--- Day_18/Notes/src/com/dragontalker/java/ThreadTest1.java
+++ Day_18/Notes/src/com/dragontalker/java/ThreadTest1.java
@@ -29,5 +29,8 @@ public class ThreadTest1 {
 
         //4. 将此对象作为参数传递到Thread类的构造器中, 创建Thread类的对象
         Thread t1 = new Thread(mThread);
+
+        //5. 通过Thread类的对象调用start()
+        t1.start();
     }
 }
