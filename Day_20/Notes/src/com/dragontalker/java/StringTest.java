fad9f4638248ba5daff8b8574086cf4bbd383798
Richard <richard.yang.tong@gmail.com>
2021-05-15 15:22:39 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/java/StringTest.java
+++ Day_20/Notes/src/com/dragontalker/java/StringTest.java
@@ -8,6 +8,12 @@ import org.junit.Test;
 
 public class StringTest {
 
+    /*
+    String: 字符串, 使用一对""引起来表示
+        1. String声明为final的, 不可被继承
+        2. String实现了Serializable接口: 表示字符串是支持序列化的
+     */
+
     @Test
     public void test1() {
 
