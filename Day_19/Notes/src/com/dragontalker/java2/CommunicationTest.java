13c4bf0acf7250b60151cce3150b2b76469e8627
Richard <richard.yang.tong@gmail.com>
2021-05-15 09:52:40 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
+++ Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
@@ -4,7 +4,9 @@ package com.dragontalker.java2;
 线程通信的例子: 使用两个线程打印 1-100. 线程1, 线程2, 交替打印
 
 涉及到的三个方法:
-wait(): 一旦执行此方法,
+wait(): 一旦执行此方法, 当前线程就进入阻塞状态, 并释放同步监视器
+notify(): 一旦执行此方法, 就会唤醒被wait的一个线程. 如果有多个线程被wait, 就唤醒优先级高的那个
+notifyAll(): 一旦执行此方法, 就唤醒所有被wait的线程
  */
 
 class Number implements Runnable {
