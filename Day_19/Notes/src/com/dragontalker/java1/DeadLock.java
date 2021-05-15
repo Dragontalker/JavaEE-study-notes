5b8341c74ac214c56f2a76a6077b5fcef0228c24
Richard <richard.yang.tong@gmail.com>
2021-05-15 06:51:57 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java1/DeadLock.java
+++ Day_19/Notes/src/com/dragontalker/java1/DeadLock.java
@@ -20,6 +20,23 @@ class A {
     }
 }
 
+class B {
+    public synchronized void bar(A a) {
+        System.out.println("当前线程名: " + Thread.currentThread().getName() +
+                " 进入了B实例的bar方法");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("当前线程名: " + Thread.currentThread().getName() +
+                " 企图调用A实例的last方法");
+    }
+    public synchronized void last() {
+        System.out.println("进入了B类的last方法内部");
+    }
+}
+
 
 public class DeadLock {
 }
