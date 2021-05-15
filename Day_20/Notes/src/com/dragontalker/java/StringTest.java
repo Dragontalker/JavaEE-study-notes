b97b2b1c289322e8c6d51132d1f28a106cc38f4a
Richard <richard.yang.tong@gmail.com>
2021-05-15 15:25:30 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/java/StringTest.java
+++ Day_20/Notes/src/com/dragontalker/java/StringTest.java
@@ -12,6 +12,9 @@ public class StringTest {
     String: 字符串, 使用一对""引起来表示
         1. String声明为final的, 不可被继承
         2. String实现了Serializable接口: 表示字符串是支持序列化的
+                 实现了Comparable接口: 表示String可以比较大小
+        3. String内部定义了final char[] value用于存储字符串数据
+        4. String: 代表不可变的字符序列, 简称: 不可变性
      */
 
     @Test
