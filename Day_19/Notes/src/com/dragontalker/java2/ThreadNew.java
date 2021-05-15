62f512d6f7e1e2a63ecafe5703fbfb041df1f2c5
Richard <richard.yang.tong@gmail.com>
2021-05-15 10:55:38 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
@@ -10,7 +10,13 @@ class NumThread implements Callable {
 
     @Override
     public Object call() throws Exception {
-        return null;
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
     }
 }
 
