bb9d38b0450e4de48e8c90e74b9948f92b5da9bc
Richard <richard.yang.tong@gmail.com>
2021-05-15 09:57:23 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
+++ Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
@@ -10,7 +10,9 @@ notifyAll(): 一旦执行此方法, 就唤醒所有被wait的线程
 
 说明:
 1. wait(), notify(), notifyAll()这三个方法必须使用在同步代码块或同步方法中
-2.
+2. wait(), notify(), notifyAll()这三个方法的调用者必须是同步代码块或同步方法中的同步监视器
+    否则, 会出现IllegalMonitorStateException异常
+
  */
 
 class Number implements Runnable {
