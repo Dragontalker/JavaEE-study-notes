eb9e63d1278a2feff57c157584e37b7c587de08b
Richard <richard.yang.tong@gmail.com>
2021-05-15 11:08:11 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
@@ -2,6 +2,11 @@ package com.dragontalker.java2;
 
 /*
 创建线程的方式三: 实现Callable接口 --- JDK5.0新增
+
+如何理解实现Callable接口的方式创建多线程比实现Runnable接口创建多线程要强大?
+    1. call()可以有返回值的
+    2. call()可以抛出异常, 被外面的操作捕获, 获取异常的信息
+    3. Callable是支持泛型的
  */
 
 import java.util.concurrent.Callable;
