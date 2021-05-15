cc027e72e2b7aa9ff62fb1f0f518e3b8efcc4cf0
Richard <richard.yang.tong@gmail.com>
2021-05-15 09:43:12 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
+++ Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
@@ -10,15 +10,36 @@ class Number implements Runnable {
     @Override
     public void run() {
         while(true) {
-            if (number <= 100) {
-                System.out.println(Thread.currentThread().getName() +
-                        ": " + number++);
-            } else {
-                break;
+            synchronized (this) {
+                if (number <= 100) {
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(Thread.currentThread().getName() +
+                            ": " + number++);
+                } else {
+                    break;
+                }
             }
         }
     }
 }
 
 public class CommunicationTest {
+    public static void main(String[] args) {
+        Number number = new Number();
+
+        Thread t1 = new Thread(number);
+        Thread t2 = new Thread(number);
+
+        t1.setName("线程1");
+        t2.setName("线程2");
+
+        t1.start();
+        t2.start();
+    }
 }
