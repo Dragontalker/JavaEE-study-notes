ff1a4e74e93b802b1ecda5a69fe87dfd8f66fde5
Richard <richard.yang.tong@gmail.com>
2021-05-15 09:50:56 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
+++ Day_19/Notes/src/com/dragontalker/java2/CommunicationTest.java
@@ -2,6 +2,9 @@ package com.dragontalker.java2;
 
 /*
 线程通信的例子: 使用两个线程打印 1-100. 线程1, 线程2, 交替打印
+
+涉及到的三个方法:
+wait(): 一旦执行此方法,
  */
 
 class Number implements Runnable {
@@ -11,6 +14,9 @@ class Number implements Runnable {
     public void run() {
         while(true) {
             synchronized (this) {
+
+                notifyAll();
+
                 if (number <= 100) {
 
                     try {
@@ -43,11 +49,22 @@ public class CommunicationTest {
 
         Thread t1 = new Thread(number);
         Thread t2 = new Thread(number);
+        Thread t3 = new Thread(number);
+        Thread t4 = new Thread(number);
+        Thread t5 = new Thread(number);
 
         t1.setName("线程1");
         t2.setName("线程2");
+        t3.setName("线程3");
+        t4.setName("线程4");
+        t5.setName("线程5");
+
 
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
     }
 }
