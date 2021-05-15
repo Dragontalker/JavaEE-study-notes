4be674bbb0ff8dd4494699178e6434cade433078
Richard <richard.yang.tong@gmail.com>
2021-05-15 10:24:12 -0400

    Added notes.

---
+++ Day_19/Notes/src/com/dragontalker/java2/ProductTest.java
@@ -0,0 +1,14 @@
+package com.dragontalker.java2;
+
+/*
+线程通信的应用: 经典例题: Producer/Consumer问题
+
+分析:
+1. 是否是多线程问题? 是, Producer线程、Consumer线程
+2. 是否有共享数据? 是, Clerk(Or Product)
+3. 如何解决线程的安全问题? 同步机制, 有三种方法
+4. 是否涉及到线程的通信? 是
+ */
+
+public class ProductTest {
+}
