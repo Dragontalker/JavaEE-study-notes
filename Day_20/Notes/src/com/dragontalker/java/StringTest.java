da8696495f8c8b3c844321940c03e1ffcb201eba
Richard <richard.yang.tong@gmail.com>
2021-05-15 15:26:32 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/java/StringTest.java
+++ Day_20/Notes/src/com/dragontalker/java/StringTest.java
@@ -19,6 +19,11 @@ public class StringTest {
 
     @Test
     public void test1() {
+        String s1 = "abc";
+        String s2 = "abc";
+        s1 = "hello";
 
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
