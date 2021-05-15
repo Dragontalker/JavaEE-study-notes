b9f359236f7cd0f0e52086402a3daac55719570f
Richard <richard.yang.tong@gmail.com>
2021-05-15 11:36:54 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadPool.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadPool.java
@@ -4,14 +4,41 @@ package com.dragontalker.java2;
 创建线程的方式四: 使用线程池
  */
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+class EvenThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 ==0) {
+                System.out.println(Thread.currentThread().getName() + "---" + i);
+            }
+        }
+    }
+}
+
+class OddThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 !=0) {
+                System.out.println(Thread.currentThread().getName() + "---" + i);
+            }
+        }
+    }
+}
 
 public class ThreadPool {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(10);
 
-        service.execute(); //适合使用于Runnable
-        service.submit(); //适合使用于Callable
+        service.execute(new OddThread()); //适合使用于Runnable
+        service.execute(new EvenThread()); //适合使用于Callable
+
+        service.shutdown();
     }
 }
