a716355df25251dc820cd122f1cb157fce0c88d0
Richard <richard.yang.tong@gmail.com>
2021-05-15 06:54:14 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java1/DeadLock.java
+++ Day_19/Notes/src/com/dragontalker/java1/DeadLock.java
@@ -38,5 +38,21 @@ class B {
 }
 
 
-public class DeadLock {
+public class DeadLock implements Runnable{
+    A a = new A();
+    B b = new B();
+
+    public void init() {
+        Thread.currentThread().setName("主线程");
+        //调用a对象的foo方法
+        a.foo(b);
+        System.out.println("进入了主线程之后");
+    }
+
+    public void run() {
+        Thread.currentThread().setName("副线程");
+        //调用b对象的bar方法
+        b.bar(a);
+        System.out.println("进入了副线程之后");
+    }
 }
