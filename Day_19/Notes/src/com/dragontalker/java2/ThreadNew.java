508ef48de161ef0793d07564bedd8832ba48216d
Richard <richard.yang.tong@gmail.com>
2021-05-15 10:54:02 -0400

    Added notes.

---
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
@@ -0,0 +1,18 @@
+package com.dragontalker.java2;
+
+/*
+创建线程的方式三: 实现Callable接口 --- JDK5.0新增
+ */
+
+import java.util.concurrent.Callable;
+
+class NumThread implements Callable {
+
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+}
+
+public class ThreadNew {
+}
