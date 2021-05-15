cb51f1ee6cd0e0df11bfcfc4ead274ce53859cf5
Richard <richard.yang.tong@gmail.com>
2021-05-15 11:05:12 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
@@ -8,8 +8,10 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
+//1. 创建一个实现Callable的实现类
 class NumThread implements Callable {
 
+    //2. 实现call方法, 将此线程需要执行的操作声明在call()方法中
     @Override
     public Object call() throws Exception {
         int sum = 0;
@@ -25,13 +27,17 @@ class NumThread implements Callable {
 
 public class ThreadNew {
     public static void main(String[] args) {
+        //3. 创建Callable接口实现类的对象
         NumThread numThread = new NumThread();
+        //4. 将此Callable接口实现类的对象作为传递到FutureTask构造器中, 创建FutureTask的对象
         FutureTask futureTask = new FutureTask(numThread);
 
+        //5. 将FutureTask的对象作为参数传递到Thread类的构造器中, 创建Thread对象, 并调用start()
         Thread thread = new Thread(futureTask);
         thread.start();
 
         try {
+            //6. 获取Callable中call方法的返回值
             //get()返回值即为FutureTask的构造器参数Callable实现类重写的call()的返回值
             Object sum = futureTask.get();
             System.out.println("总和为:" + sum);
