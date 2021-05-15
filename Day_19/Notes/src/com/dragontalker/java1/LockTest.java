e4017a4cbbb5be8608c4059f6c3a7bf233c1eeae
Richard <richard.yang.tong@gmail.com>
2021-05-15 07:07:56 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java1/LockTest.java
+++ Day_19/Notes/src/com/dragontalker/java1/LockTest.java
@@ -23,4 +23,19 @@ class Window implements Runnable {
 }
 
 public class LockTest {
+    public static void main(String[] args) {
+        Window w = new Window();
+
+        Thread t1 = new Thread(w);
+        Thread t2 = new Thread(w);
+        Thread t3 = new Thread(w);
+
+        t1.setName("窗口1");
+        t2.setName("窗口2");
+        t3.setName("窗口3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
