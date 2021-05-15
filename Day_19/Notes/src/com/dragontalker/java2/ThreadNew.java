cfadf5b25b71cd44cec049ecf8a49edcfc888dc8
Richard <richard.yang.tong@gmail.com>
2021-05-15 11:09:36 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadNew.java
@@ -14,11 +14,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 //1. 创建一个实现Callable的实现类
-class NumThread implements Callable {
+class NumThread implements Callable<Integer> {
 
     //2. 实现call方法, 将此线程需要执行的操作声明在call()方法中
     @Override
-    public Object call() throws Exception {
+    public Integer call() throws Exception {
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
@@ -35,7 +35,7 @@ public class ThreadNew {
         //3. 创建Callable接口实现类的对象
         NumThread numThread = new NumThread();
         //4. 将此Callable接口实现类的对象作为传递到FutureTask构造器中, 创建FutureTask的对象
-        FutureTask futureTask = new FutureTask(numThread);
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(numThread);
 
         //5. 将FutureTask的对象作为参数传递到Thread类的构造器中, 创建Thread对象, 并调用start()
         Thread thread = new Thread(futureTask);
@@ -44,7 +44,7 @@ public class ThreadNew {
         try {
             //6. 获取Callable中call方法的返回值
             //get()返回值即为FutureTask的构造器参数Callable实现类重写的call()的返回值
-            Object sum = futureTask.get();
+            Integer sum = futureTask.get();
             System.out.println("总和为:" + sum);
         } catch (InterruptedException e) {
             e.printStackTrace();
