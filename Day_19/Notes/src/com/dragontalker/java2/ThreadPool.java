94e6b5e76d3912e12d5d42157bd37790c484af8c
Richard <richard.yang.tong@gmail.com>
2021-05-15 11:38:25 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadPool.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadPool.java
@@ -34,10 +34,13 @@ class OddThread implements Runnable {
 
 public class ThreadPool {
     public static void main(String[] args) {
+        //1. 提供指定线程数量的线程池
         ExecutorService service = Executors.newFixedThreadPool(10);
 
+        //2. 执行指定的线程操作, 需要提供实现Runnable接口或Callable接口实现类的对象
         service.execute(new OddThread()); //适合使用于Runnable
         service.execute(new EvenThread()); //适合使用于Callable
+        //service.submit(Callable callable);
 
         service.shutdown();
     }
