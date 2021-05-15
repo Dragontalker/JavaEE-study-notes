455ea077174389ba5785c68f60970a77d288e4f8
Richard <richard.yang.tong@gmail.com>
2021-05-15 06:58:51 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java1/DeadLock.java
+++ Day_19/Notes/src/com/dragontalker/java1/DeadLock.java
@@ -3,7 +3,7 @@ package com.dragontalker.java1;
 //死锁的演示
 
 class A {
-    public synchronized void foo(B b) { //同步锁: A类的对象 ---> a
+    public synchronized void foo(B b) { //同步监视器: A类的对象 ---> a
         System.out.println("当前线程名: " + Thread.currentThread().getName() +
                 " 进入了A实例的foo方法");
         try {
@@ -16,13 +16,13 @@ class A {
         b.last();
     }
 
-    public synchronized void last() {
+    public synchronized void last() { //同步监视器: b
         System.out.println("进入了A类的last方法内部");
     }
 }
 
 class B {
-    public synchronized void bar(A a) {
+    public synchronized void bar(A a) { //同步监视器: b
         System.out.println("当前线程名: " + Thread.currentThread().getName() +
                 " 进入了B实例的bar方法");
         try {
@@ -34,7 +34,7 @@ class B {
                 " 企图调用A实例的last方法");
         a.last();
     }
-    public synchronized void last() {
+    public synchronized void last() { //同步监视器: a
         System.out.println("进入了B类的last方法内部");
     }
 }
