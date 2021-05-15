5a6a5e6a90f56403c16cbed876d322ad6da46a08
Richard <richard.yang.tong@gmail.com>
2021-05-15 11:48:03 -0400

    Added notes.

--- Day_19/Notes/src/com/dragontalker/java2/ThreadPool.java
+++ Day_19/Notes/src/com/dragontalker/java2/ThreadPool.java
@@ -15,6 +15,7 @@ package com.dragontalker.java2;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 class EvenThread implements Runnable {
 
@@ -44,6 +45,12 @@ public class ThreadPool {
     public static void main(String[] args) {
         //1. 提供指定线程数量的线程池
         ExecutorService service = Executors.newFixedThreadPool(10);
+        ThreadPoolExecutor service1 = (ThreadPoolExecutor) service;
+
+        //设置线程池的属性
+        System.out.println(service.getClass());
+        //service1.setCorePoolSize(15);
+        //service1.getKeepAliveTime();
 
         //2. 执行指定的线程操作, 需要提供实现Runnable接口或Callable接口实现类的对象
         service.execute(new OddThread()); //适合使用于Runnable
