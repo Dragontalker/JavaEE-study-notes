0f4d0824f52d98800078dce625e07d9a8b8ffde2
Richard <richard.yang.tong@gmail.com>
2021-05-16 09:59:02 -0400

    Added notes.

---
+++ Day_20/Notes/src/com/dragontalker/exer/StringTest.java
@@ -0,0 +1,23 @@
+package com.dragontalker.exer;
+
+/*
+一道面试题
+
+ */
+
+public class StringTest {
+    String str = new String("good");
+    char[] ch = {'t', 'e', 's', 't'};
+
+    public void change(String str, char ch[]) {
+        str = "test ok";
+        ch[0] = 'b';
+    }
+
+    public static void main(String[] args) {
+        StringTest ex = new StringTest();
+        ex.change(ex.str, ex.ch);
+        System.out.println(ex.str + " and ");
+        System.out.println(ex.ch);
+    }
+}
