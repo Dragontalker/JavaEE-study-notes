6e347ab203901074703240fae32eda3aad1fee53
Richard <richard.yang.tong@gmail.com>
2021-05-16 18:01:19 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/java/StringBufferBuilderTest.java
+++ Day_20/Notes/src/com/dragontalker/java/StringBufferBuilderTest.java
@@ -5,4 +5,10 @@ package com.dragontalker.java;
  */
 
 public class StringBufferBuilderTest {
+    /*
+    String, StringBuffer, StringBuilder三者的异同?
+    String: 不可变的字符序列: 底层使用char[]存储
+    StringBuffer: 可变的字符序列: 线程安全的, 效率偏低, 底层使用char[]存储
+    StringBuilder: jdk5.0新增, 可变的字符序列: 线程不安全, 效率高, 底层使用char[]存储
+     */
 }
