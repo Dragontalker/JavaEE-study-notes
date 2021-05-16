5d76e00393103fad8b747463fc120379a89b3734
Richard <richard.yang.tong@gmail.com>
2021-05-16 10:07:33 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/exer/StringTest.java
+++ Day_20/Notes/src/com/dragontalker/exer/StringTest.java
@@ -10,8 +10,8 @@ public class StringTest {
     char[] ch = {'t', 'e', 's', 't'};
 
     public void change(String str, char[] ch) {
-        str = "test ok";
-        ch[0] = 'b';
+        str = "test ok"; //String有不可变性, 和一般传递的形参不一样
+        ch[0] = 'b'; //数组是可变的
     }
 
     public static void main(String[] args) {
