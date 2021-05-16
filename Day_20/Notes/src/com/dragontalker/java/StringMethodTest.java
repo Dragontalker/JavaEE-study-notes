b1562f6bdf11f838dc7c27946fed53bc4f817215
Richard <richard.yang.tong@gmail.com>
2021-05-16 10:25:26 -0400

    Added notes.

---
+++ Day_20/Notes/src/com/dragontalker/java/StringMethodTest.java
@@ -0,0 +1,18 @@
+package com.dragontalker.java;
+
+import org.junit.Test;
+
+public class StringMethodTest {
+
+    /*
+
+     */
+    @Test
+    public void test1() {
+        String s1 = "Hello world!";
+
+        //int length()
+        System.out.println(s1.length());
+
+    }
+}
