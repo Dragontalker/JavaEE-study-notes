8a8c84245eea60344b258bf0857c1855bbb22bb9
Richard <richard.yang.tong@gmail.com>
2021-05-16 10:02:04 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/exer/StringTest.java
+++ Day_20/Notes/src/com/dragontalker/exer/StringTest.java
@@ -9,7 +9,7 @@ public class StringTest {
     String str = new String("good");
     char[] ch = {'t', 'e', 's', 't'};
 
-    public void change(String str, char ch[]) {
+    public void change(String str, char[] ch) {
         str = "test ok";
         ch[0] = 'b';
     }
@@ -17,7 +17,7 @@ public class StringTest {
     public static void main(String[] args) {
         StringTest ex = new StringTest();
         ex.change(ex.str, ex.ch);
-        System.out.println(ex.str + " and ");
-        System.out.println(ex.ch);
+        System.out.println(ex.str); //good
+        System.out.println(ex.ch); //best
     }
 }
