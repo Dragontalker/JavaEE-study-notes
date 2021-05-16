ee7d80c16fbbb5c8e2f3e7c1ea7c3b419b2c6539
Richard <richard.yang.tong@gmail.com>
2021-05-16 10:29:28 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/java/StringMethodTest.java
+++ Day_20/Notes/src/com/dragontalker/java/StringMethodTest.java
@@ -2,6 +2,8 @@ package com.dragontalker.java;
 
 import org.junit.Test;
 
+import java.util.Locale;
+
 public class StringMethodTest {
 
     /*
@@ -19,5 +21,15 @@ public class StringMethodTest {
 
         //3. boolean isEmpty()
         System.out.println(s1.isEmpty());
+
+        //4. String toLowerCase()
+        String s2 = s1.toLowerCase();
+        System.out.println(s2);
+        System.out.println(s1); //s1是不可变的
+
+        //4. String toUpperCase()
+        String s3 = s1.toUpperCase();
+        System.out.println(s3);
+        System.out.println(s1);
     }
 }
