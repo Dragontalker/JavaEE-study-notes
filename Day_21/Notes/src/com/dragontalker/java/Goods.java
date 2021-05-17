432c4fea9ad1b56769c800bc1e6e0d776effe337
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:19:45 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/Goods.java
+++ Day_21/Notes/src/com/dragontalker/java/Goods.java
@@ -4,4 +4,13 @@ public class Goods {
 
     private String name;
     private double price;
+
+    public Goods() {}
+
+    public Goods(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
 }
