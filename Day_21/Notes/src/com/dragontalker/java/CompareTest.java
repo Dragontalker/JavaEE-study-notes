6bfe189286703fe188740af4ee8b596ac58a3883
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:12:21 -0400

    Added notes.

---
+++ Day_21/Notes/src/com/dragontalker/java/CompareTest.java
@@ -0,0 +1,11 @@
+package com.dragontalker.java;
+
+/**
+ * 一、说明: Java中的对象, 正常情况下, 只能进行比较: == 或 !=.
+ *  - 不能使用 > 或 <, 但是在开发的场景中, 我们需要对多个对象进行排序
+ *  - 言外之意, 就需要比较对象的大小
+ *  - 如何实现？使用两个接口中的任何一个: Comparable 或 Comparator
+ */
+
+public class CompareTest {
+}
