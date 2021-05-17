74479c4c7935cddaab3180b6497d066d96e3e390
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:50:40 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/CompareTest.java
+++ Day_21/Notes/src/com/dragontalker/java/CompareTest.java
@@ -76,4 +76,31 @@ public class CompareTest {
         });
         System.out.println(Arrays.toString(arr));
     }
+
+    @Test
+    public void test4() {
+        Goods[] arr = new Goods[4];
+        arr[0] = new Goods("LenovoMouse", 34);
+        arr[1] = new Goods("DellMouse", 43);
+        arr[2] = new Goods("XiaoMiMouse", 12);
+        arr[3] = new Goods("HuaWeiMouse", 65);
+
+        Arrays.sort(arr, new Comparator() {
+
+            //指明商品比较大小的方式: 按照商品名称从低到高排序, 再按照加个从高到低排序
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof Goods && o2 instanceof Goods) {
+                    Goods g1 = (Goods) o1;
+                    Goods g2 = (Goods) o2;
+                    if (g1.getName().equals(g2.getName())) {
+                        return Double.compare(g1.getPrice(), g2.getPrice());
+                    } else {
+                        return g1.getName().compareTo(g2.getName());
+                    }
+                }
+                throw new RuntimeException("输入的数据类型不一致");
+            }
+        });
+    }
 }
