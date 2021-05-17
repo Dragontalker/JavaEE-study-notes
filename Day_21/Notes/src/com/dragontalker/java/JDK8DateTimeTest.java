7e7fa402a8111c976b3008fe1672d3333b2dcf7b
Richard <richard.yang.tong@gmail.com>
2021-05-17 14:32:25 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/JDK8DateTimeTest.java
+++ Day_21/Notes/src/com/dragontalker/java/JDK8DateTimeTest.java
@@ -4,5 +4,15 @@ package com.dragontalker.java;
 jdk 8中日期时间API的测试
  */
 
+import org.junit.Test;
+
+import java.util.Date;
+
 public class JDK8DateTimeTest {
+
+    @Test
+    public void testDate() {
+        Date date1 = new Date(2020, 9, 8);
+        System.out.println(date1);
+    }
 }
