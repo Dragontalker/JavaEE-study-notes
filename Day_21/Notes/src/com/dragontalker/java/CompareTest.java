9d7e390e10c27eb6af6b02f302b39cba077689fa
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:45:27 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/CompareTest.java
+++ Day_21/Notes/src/com/dragontalker/java/CompareTest.java
@@ -64,6 +64,10 @@ public class CompareTest {
         Arrays.sort(arr, new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
+                if (o1 instanceof String && o2 instanceof String) {
+                    String s1 = (String) o1;
+                    String s2 = (String) o2;
+                }
                 return 0;
             }
         });
