5e16c00dbf3c3c4c428ad8e24a0caa13e13efd8a
Richard <richard.yang.tong@gmail.com>
2021-05-17 11:26:35 -0400

    Added notes.

---
+++ Day_21/Notes/src/com/dragontalker/java/DateTimeTest.java
@@ -0,0 +1,11 @@
+package com.dragontalker.java;
+/*
+jdk 8之前的日期时间的API测试
+1. System类中currentTimeMillis();
+2. java.util.Date和子类java.sql.Date();
+3. SimpleDateFormat
+4. Calender
+ */
+
+public class DateTimeTest {
+}
