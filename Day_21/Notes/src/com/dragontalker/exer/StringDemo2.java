1e0c95fe226a49a4618081314dee7a1cd6743943
Richard <richard.yang.tong@gmail.com>
2021-05-17 10:50:27 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
@@ -6,6 +6,19 @@ public class StringDemo2 {
     //获取两个字符串中最大相同的字串
 
     public String getMaxSameString(String str1, String str2) {
+        String maxStr = (str1.length() >= str2.length()) ? str1 : str2;
+        String minStr = (str1.length() < str2.length()) ? str1 : str2;
+        int length = minStr.length();
+
+        for (int i = 0; i < length; i++) {
+
+            for (int x = 0, y = length - i; y <= length; x++, y++) {
+                String subStr = minStr.substring(x, y);
+                if (maxStr.contains(subStr)) {
+                    return subStr;
+                }
+            }
+        }
         return null;
     }
 
