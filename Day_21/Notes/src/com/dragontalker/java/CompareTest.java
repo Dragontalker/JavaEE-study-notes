d8cca3aa38fa96ead6d95f84e3addf9251ddb21c
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:46:04 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/CompareTest.java
+++ Day_21/Notes/src/com/dragontalker/java/CompareTest.java
@@ -67,8 +67,9 @@ public class CompareTest {
                 if (o1 instanceof String && o2 instanceof String) {
                     String s1 = (String) o1;
                     String s2 = (String) o2;
+                    return -s1.compareTo(s2);
                 }
-                return 0;
+                throw new RuntimeException("输入的数据类型不一致");
             }
         });
     }
