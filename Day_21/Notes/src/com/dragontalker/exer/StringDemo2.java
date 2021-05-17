ae08f7d0dbb8be5e8916b2db78d3fe4d9440a7f3
Richard <richard.yang.tong@gmail.com>
2021-05-17 10:52:19 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
@@ -6,16 +6,18 @@ public class StringDemo2 {
     //获取两个字符串中最大相同的字串
 
     public String getMaxSameString(String str1, String str2) {
-        String maxStr = (str1.length() >= str2.length()) ? str1 : str2;
-        String minStr = (str1.length() < str2.length()) ? str1 : str2;
-        int length = minStr.length();
+        if (str1 != null && str2 != null) {
+            String maxStr = (str1.length() >= str2.length()) ? str1 : str2;
+            String minStr = (str1.length() < str2.length()) ? str1 : str2;
+            int length = minStr.length();
 
-        for (int i = 0; i < length; i++) {
+            for (int i = 0; i < length; i++) {
 
-            for (int x = 0, y = length - i; y <= length; x++, y++) {
-                String subStr = minStr.substring(x, y);
-                if (maxStr.contains(subStr)) {
-                    return subStr;
+                for (int x = 0, y = length - i; y <= length; x++, y++) {
+                    String subStr = minStr.substring(x, y);
+                    if (maxStr.contains(subStr)) {
+                        return subStr;
+                    }
                 }
             }
         }
@@ -26,5 +28,7 @@ public class StringDemo2 {
     public void test1() {
         String str1 = "abcwerthelloyuiodef";
         String str2 = "cvhellobnm";
+        String result = getMaxSameString(str1, str2);
+        System.out.println(result);
     }
 }
