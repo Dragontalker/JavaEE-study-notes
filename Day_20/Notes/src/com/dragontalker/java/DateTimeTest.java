dd3382d7f901c6131140405f14a42da2dab66d23
Richard <richard.yang.tong@gmail.com>
2021-05-16 20:07:39 -0400

    Added notes.

--- Day_20/Notes/src/com/dragontalker/java/DateTimeTest.java
+++ Day_20/Notes/src/com/dragontalker/java/DateTimeTest.java
@@ -26,12 +26,14 @@ public class DateTimeTest {
     1. 两个构造器的使用
 
     2. 两个方法的使用
-        > toString()
+        > toString(): 显示当前的年、月、日、时、分、秒
+        > getTime(): 获取当前Date对象的毫秒数(时间戳)
      */
     @Test
     public void test2() {
         //构造器一: Date()
         Date date1 = new Date();
-        System.out.println(date1.toString());
+        System.out.println(date1); //Sun May 16 20:06:52 EDT 2021
+        System.out.println(date1.getTime()); //1621210012282
     }
 }
