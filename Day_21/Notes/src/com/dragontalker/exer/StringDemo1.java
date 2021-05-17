e98b7f78be36bf2f529308b92529d713d52ed2ba
Richard <richard.yang.tong@gmail.com>
2021-05-17 10:05:23 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/exer/StringDemo1.java
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo1.java
@@ -1,5 +1,7 @@
 package com.dragontalker.exer;
 
+import org.junit.Test;
+
 public class StringDemo1 {
     /*
     获取一个字符串在另一个字符串中出现的次数
@@ -9,12 +11,20 @@ public class StringDemo1 {
     public int getCount(String mainStr, String subStr) {
         int mainLength = mainStr.length();
         int subLength = subStr.length();
+        int count = 0;
 
         if (mainLength >= subLength) {
 
         } else {
-            return 0;
+            return count;
         }
-        return 0;
+        return count;
+    }
+
+    @Test
+    public void testGetCount() {
+        String mainStr = "abkkcadkabkebfkabkskab";
+        String subStr = "ab";
+
     }
 }
