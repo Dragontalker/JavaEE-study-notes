5ee94481d2d98e23c92756211ed992d61a215fe9
Richard <richard.yang.tong@gmail.com>
2021-05-17 10:58:51 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
@@ -24,6 +24,32 @@ public class StringDemo2 {
         return null;
     }
 
+    //如果存在多个长度相同的最大相同字串
+    //此时先返回String[], 后面可以用集合中的ArrayList替换, 较方便
+    public String getMaxSameString1(String str1, String str2) {
+        if (str1 != null && str2 != null) {
+            StringBuffer sBuffer = new StringBuffer();
+            String maxStr = (str1.length() >= str2.length()) ? str1 : str2;
+            String minStr = (str1.length() < str2.length()) ? str1 : str2;
+            int length = minStr.length();
+
+            for (int i = 0; i < length; i++) {
+
+                for (int x = 0, y = length - i; y <= length; x++, y++) {
+                    String subStr = minStr.substring(x, y);
+                    if (maxStr.contains(subStr)) {
+                        sBuffer.append(subStr + ",");
+                    }
+                }
+                System.out.println(sBuffer);
+                if (sBuffer.length() != 0) {
+                    break;
+                };
+            }
+        }
+        return null;
+    }
+
     @Test
     public void test1() {
         String str1 = "abcwerthello12345yuiodef";
