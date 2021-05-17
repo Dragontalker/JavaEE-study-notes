25e9b05a05a14595c05af10e5b8154e4703616b3
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:19:54 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/Goods.java
+++ Day_21/Notes/src/com/dragontalker/java/Goods.java
@@ -12,5 +12,19 @@ public class Goods {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
