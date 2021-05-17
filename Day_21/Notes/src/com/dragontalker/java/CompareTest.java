adccc1ecd0382f9c06c4a3ddda21322ee0516e84
Richard <richard.yang.tong@gmail.com>
2021-05-17 16:21:40 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/CompareTest.java
+++ Day_21/Notes/src/com/dragontalker/java/CompareTest.java
@@ -31,4 +31,16 @@ public class CompareTest {
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
+
+    @Test
+    public void test2() {
+        Goods[] arr = new Goods[4];
+        arr[0] = new Goods("LenovoMouse", 34);
+        arr[1] = new Goods("DellMouse", 43);
+        arr[2] = new Goods("XiaoMiMouse", 12);
+        arr[3] = new Goods("HuaWeiMouse", 65);
+
+        Arrays.sort(arr);
+        System.out.println(arr.toString());
+    }
 }
