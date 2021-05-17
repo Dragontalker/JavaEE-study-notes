07cd7e49cb59e3ab368ca5998d57cf83d42f2990
Richard <richard.yang.tong@gmail.com>
2021-05-17 10:20:44 -0400

    Added notes.

---
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
@@ -0,0 +1,13 @@
+package com.dragontalker.exer;
+
+import org.junit.Test;
+
+public class StringDemo2 {
+    //获取两个字符串中最大相同的字串
+
+    @Test
+    public void test1() {
+        String str1 = "abcwerthelloyuiodef";
+        String str2 = "cvhellobnm";
+    }
+}
