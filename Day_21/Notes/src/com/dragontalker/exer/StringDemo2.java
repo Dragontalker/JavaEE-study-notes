5ccebebadc2c01a6f1ec589be296075c35d5b645
Richard <richard.yang.tong@gmail.com>
2021-05-17 10:22:30 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo2.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 public class StringDemo2 {
     //获取两个字符串中最大相同的字串
 
+    public String getMaxSameString(String str1, String str2) {
+        return null;
+    }
+
     @Test
     public void test1() {
         String str1 = "abcwerthelloyuiodef";
