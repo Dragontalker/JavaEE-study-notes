a171e5bfb631e41b4b61db65fd785ea2df23ef49
Richard <richard.yang.tong@gmail.com>
2021-05-17 09:36:37 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/exer/StringDemo.java
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo.java
@@ -1,8 +1,13 @@
 package com.dragontalker.exer;
 
 public class StringDemo {
-    //将一个字符串进行反装, 将字符串中指定部分进行反转.
+    /*
+    将一个字符串进行反装, 将字符串中指定部分进行反转.
+
+    方式一: 转换为char[]
+     */
     public  String reverse(String str, int startIndex, int endIndex) {
+        char[] arr = str.toCharArray();
         return null;
     }
 }
