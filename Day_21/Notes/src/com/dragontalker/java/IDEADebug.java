81981329736beae54983d78a99dc376560761c9f
Richard <richard.yang.tong@gmail.com>
2021-05-17 11:06:38 -0400

    Added notes.

--- Day_21/Notes/src/com/dragontalker/java/IDEADebug.java
+++ Day_21/Notes/src/com/dragontalker/java/IDEADebug.java
@@ -1,4 +1,19 @@
 package com.dragontalker.java;
 
+import org.junit.Test;
+
 public class IDEADebug {
+    @Test
+    public void testStringBuffer() {
+        String str = null;
+        StringBuffer sb = new StringBuffer();
+        sb.append(str);
+
+        System.out.println(sb.length());
+
+        System.out.println(sb);
+
+        StringBuffer sb1 = new StringBuffer(str);
+        System.out.println(sb1);
+    }
 }
