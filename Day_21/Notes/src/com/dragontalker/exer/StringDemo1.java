cee4a3cb7283dc5c85c411f9aa37af4186f4ab43
Richard <richard.yang.tong@gmail.com>
2021-05-17 09:53:53 -0400

    Added notes.

---
+++ Day_21/Notes/src/com/dragontalker/exer/StringDemo1.java
@@ -0,0 +1,11 @@
+package com.dragontalker.exer;
+
+public class StringDemo1 {
+    /*
+    获取一个字符串在另一个字符串中出现的次数
+     */
+
+    public int getCount(String mainStr, String subStr) {
+        return 0;
+    }
+}
