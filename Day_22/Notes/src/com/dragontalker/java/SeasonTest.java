aae3236458a8aad2a20042adeedc3d8d2eab6c2f
Richard <richard.yang.tong@gmail.com>
2021-05-18 07:13:52 -0400

    Added notes.

--- Day_22/Notes/src/com/dragontalker/java/SeasonTest.java
+++ Day_22/Notes/src/com/dragontalker/java/SeasonTest.java
@@ -21,9 +21,15 @@ class Season {
     private final String seasonName;
     private final String seasonDesc;
 
-    //2. 私有化类的构造器
+    //2. 私有化类的构造器, 并给对象属性初始化
     private Season(String seasonName, String seasonDesc) {
         this.seasonName = seasonName;
         this.seasonDesc = seasonDesc;
     }
+
+    //3. 提供当前枚举类的多个对象
+    public static final Season SPRING = new Season("春天", "春暖花开");
+    public static final Season SUMMER = new Season("夏天", "夏日炎炎");
+    public static final Season AUTUMN = new Season("秋天", "秋高气爽");
+    public static final Season WINTER = new Season("冬天", "冰天雪地");
 }
