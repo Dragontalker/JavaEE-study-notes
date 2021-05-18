9189943e19793327eb56171548c7af070413b4ef
Richard <richard.yang.tong@gmail.com>
2021-05-18 10:18:44 -0400

    Added notes.

--- Day_22/Notes/src/com/dragontalker/java1/AnnotationTest.java
+++ Day_22/Notes/src/com/dragontalker/java1/AnnotationTest.java
@@ -20,6 +20,8 @@ class Person {
     private String name;
     private int age;
 
+    public Person() {}
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
