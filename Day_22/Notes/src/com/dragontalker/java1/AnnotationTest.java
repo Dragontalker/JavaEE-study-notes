cefc27db44d79b81eb749d69de3750f89346938e
Richard <richard.yang.tong@gmail.com>
2021-05-18 10:17:57 -0400

    Added notes.

--- Day_22/Notes/src/com/dragontalker/java1/AnnotationTest.java
+++ Day_22/Notes/src/com/dragontalker/java1/AnnotationTest.java
@@ -19,4 +19,9 @@ public class AnnotationTest {
 class Person {
     private String name;
     private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
