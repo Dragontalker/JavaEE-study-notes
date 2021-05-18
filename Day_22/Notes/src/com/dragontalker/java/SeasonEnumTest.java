18f55a3d97239e1b1a5085353f919f29d78f306a
Richard <richard.yang.tong@gmail.com>
2021-05-18 09:26:51 -0400

    Added notes.

--- Day_22/Notes/src/com/dragontalker/java/SeasonEnumTest.java
+++ Day_22/Notes/src/com/dragontalker/java/SeasonEnumTest.java
@@ -7,7 +7,9 @@ package com.dragontalker.java;
 public class SeasonEnumTest {
 }
 
+//使用enum关键字枚举类
 enum EnumSeason {
+    //1. 提供当前枚举类的对象, 多个对象之间用","隔开, 末尾的对象用";"结束
     SPRING ("春天", "春暖花开"),
     SUMMER ("夏天", "夏日炎炎"),
     AUTUMN ("秋天", "秋高气爽"),
