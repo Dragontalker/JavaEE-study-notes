6cfc94ec584ed410248d47aa4169811d5f48441e
Richard <richard.yang.tong@gmail.com>
2021-05-18 11:41:23 -0400

    Added notes.

--- Day_22/Notes/src/com/dragontalker/java2/CollectionTest.java
+++ Day_22/Notes/src/com/dragontalker/java2/CollectionTest.java
@@ -4,6 +4,9 @@ package com.dragontalker.java2;
  * 一、集合框架的概述
  *
  * 1. 集合、数组都是对多个数据进行存储操作的结构, 简称Java容器
+ *  - 说明: 此时的存储, 主要指的是内存层面的存储, 不涉及到持久化的存储(.txt, .jpg, .avi, 数据库中)
+ *
+ * 2. 数组在存储多个数据方面的特点:
  */
 
 public class CollectionTest {
