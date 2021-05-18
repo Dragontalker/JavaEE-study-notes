c0426bb90e644bab42fa015d4c9f180a3dfdad9b
Richard <richard.yang.tong@gmail.com>
2021-05-18 11:22:25 -0400

    Added notes.

--- Day_22/Notes/src/com/dragontalker/java1/MyAnnotation.java
+++ Day_22/Notes/src/com/dragontalker/java1/MyAnnotation.java
@@ -3,9 +3,12 @@ package com.dragontalker.java1;
 import java.lang.annotation.*;
 import java.lang.reflect.Method;
 
+import static java.lang.annotation.ElementType.*;
+
 @Inherited
+@Repeatable(MyAnnotations.class)
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.PARAMETER})
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
 public @interface MyAnnotation {
     String value() default "hello";
 }
--- Day_22/Notes/src/com/dragontalker/java1/MyAnnotations.java
+++ Day_22/Notes/src/com/dragontalker/java1/MyAnnotations.java
@@ -1,5 +1,9 @@
 package com.dragontalker.java1;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotations {
     MyAnnotation[] value();
 }
