88be801e1df4ba3a34906c71a2f9973a047a8933
Richard <richard.yang.tong@gmail.com>
2021-05-19 14:21:40 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/src7/ArrayListTest.java
+++ Day_23/Notes/src/com/dragontalker/src7/ArrayListTest.java
@@ -4,6 +4,8 @@ import com.dragontalker.java.Person;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 1. 数组存储在 private transient Object[] elementData;
@@ -32,5 +34,11 @@ public class ArrayListTest {
         //void add(int index, Object ele): 在index位置插入ele元素
         list.add(1, "BB");
         System.out.println(list);
+
+        //boolean addAll(int index, Collection eles): 从index位置开始将eles中所有的元素添加进来
+        List list1 = Arrays.asList(1, 2, 3);
+        list.addAll(list1);
+        System.out.println(list);
+        System.out.println(list.size());
     }
 }
