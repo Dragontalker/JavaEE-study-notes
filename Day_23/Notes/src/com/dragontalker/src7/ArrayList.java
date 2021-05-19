01a492c5f318bde4ff5b64fedc196d353da409f5
Richard <richard.yang.tong@gmail.com>
2021-05-19 11:41:42 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/src7/ArrayList.java
+++ Day_23/Notes/src/com/dragontalker/src7/ArrayList.java
@@ -5,6 +5,8 @@ package com.dragontalker.src7;
  * 2. 构造器里: this.elementDate = new Object[initialCapacity];
  * 3. 默认值: int initialCapacity = 10;
  * 4. 所以, ArrayList list = new ArrayList(); 创建了长度是10的数组elementData
+ * 5. 如果添加导致底层的elementData数组容量不够, 则扩容
+ * 6. 默认情况下, 扩容为原来容量的1.5倍, 同时需要将原有数组中的数据复制到新的数组中
  */
 
 public class ArrayList {
