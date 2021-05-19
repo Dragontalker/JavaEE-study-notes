6efe04d657d2acb987cfe4769d6f6fbcb4563e61
Richard <richard.yang.tong@gmail.com>
2021-05-19 14:44:23 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/exer/ListExercise.java
+++ Day_23/Notes/src/com/dragontalker/exer/ListExercise.java
@@ -2,9 +2,15 @@ package com.dragontalker.exer;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class ListExercise {
     @Test
     public void testListRemove() {
 
+    }
+
+    public static void updateList(List list) {
+        list.remove(2);
     }
 }
