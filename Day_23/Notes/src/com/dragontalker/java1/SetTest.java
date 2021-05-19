50abc8beb1ef13066f9aa048d3eda9f8c719ec93
Richard <richard.yang.tong@gmail.com>
2021-05-19 17:00:42 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java1/SetTest.java
+++ Day_23/Notes/src/com/dragontalker/java1/SetTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -80,6 +81,18 @@ public class SetTest {
     //LinkedHashSet的使用
     @Test
     public void test2() {
+        Set set = new LinkedHashSet();
+        set.add(456);
+        set.add(123);
+        set.add("AA");
+        set.add("CC");
+        set.add(new User("Tom", 12));
+        set.add(new User("Tom", 12));
+        set.add(129);
 
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        };
     }
 }
