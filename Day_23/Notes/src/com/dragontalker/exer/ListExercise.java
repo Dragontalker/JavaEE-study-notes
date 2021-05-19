6b6fe60f8d1566ceba23ed9cbc96a743568dd47b
Richard <richard.yang.tong@gmail.com>
2021-05-19 14:48:07 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/exer/ListExercise.java
+++ Day_23/Notes/src/com/dragontalker/exer/ListExercise.java
@@ -13,10 +13,12 @@ public class ListExercise {
         list.add(2);
         list.add(3);
         updateList(list);
-        System.out.println(list); //[1, 2]
+        //System.out.println(list); //[1, 2]
+        System.out.println(list); //[1, 3]
     }
 
     public static void updateList(List list) {
-        list.remove(2); //注意这里的int是index而不是Integer类
+        //list.remove(2); //注意这里的int是index而不是Integer类
+        list.remove(new Integer(2));
     }
 }
