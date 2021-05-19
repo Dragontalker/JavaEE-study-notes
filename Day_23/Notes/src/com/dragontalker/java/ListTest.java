609a2e00f428600a219b4c25756664397cceb311
Richard <richard.yang.tong@gmail.com>
2021-05-19 10:12:21 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java/ListTest.java
+++ Day_23/Notes/src/com/dragontalker/java/ListTest.java
@@ -1,7 +1,11 @@
 package com.dragontalker.java;
 
 /**
- * List接口
+ * List接口: ArrayList, LinkedList, Vector
+ *
+ * 面试题: ArrayList, LinkedList, Vector三者的异同？
+ * 同: 三个类都是实现了List接口, 存储数据的特点相同: 存储有序的, 可重复的数据
+ * 不同:  - ArrayList: 作为List接口的主要实现类
  */
 
 public class ListTest {
