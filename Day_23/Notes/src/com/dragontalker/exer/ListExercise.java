c59cd206486e3a0c1ec93fb5834057d618ccda46
Richard <richard.yang.tong@gmail.com>
2021-05-19 14:44:57 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/exer/ListExercise.java
+++ Day_23/Notes/src/com/dragontalker/exer/ListExercise.java
@@ -2,12 +2,18 @@ package com.dragontalker.exer;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercise {
     @Test
     public void testListRemove() {
-
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        updateList(list);
+        System.out.println(list);
     }
 
     public static void updateList(List list) {
