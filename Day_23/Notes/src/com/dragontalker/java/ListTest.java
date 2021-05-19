d2018d402e5a0c1e7364d94951cd35fbe2ec7dac
Richard <richard.yang.tong@gmail.com>
2021-05-19 10:17:55 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java/ListTest.java
+++ Day_23/Notes/src/com/dragontalker/java/ListTest.java
@@ -5,7 +5,11 @@ package com.dragontalker.java;
  *
  * 面试题: ArrayList, LinkedList, Vector三者的异同？
  * 同: 三个类都是实现了List接口, 存储数据的特点相同: 存储有序的, 可重复的数据
- * 不同:  - ArrayList: 作为List接口的主要实现类
+ * 不同:
+ *  - ArrayList: 作为List接口的主要实现类, 执行效率高(线程不安全的),
+ *      - 底层结构: 使用Object[]存储, 用elementData变量存储
+ *  - LinkedList:
+ *  - Vector: 作为List接口的古老实现类, 执行效率低(线程安全的)
  */
 
 public class ListTest {
