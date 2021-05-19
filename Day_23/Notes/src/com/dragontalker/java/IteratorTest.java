9eb1c648e55878e6b6c134a1ca88a784b9de0417
Richard <richard.yang.tong@gmail.com>
2021-05-19 08:47:28 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java/IteratorTest.java
+++ Day_23/Notes/src/com/dragontalker/java/IteratorTest.java
@@ -1,8 +1,24 @@
 package com.dragontalker.java;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * 集合元素的遍历操作, 使用Iterator接口
  */
 
 public class IteratorTest {
+    @Test
+    public void test1() {
+        Collection collection = new ArrayList();
+        collection.add(new Person("Jerry", 20));
+        collection.add(123);
+        collection.add(456);
+        collection.add(new String("Tom"));
+        collection.add(false);
+
+
+    }
 }
