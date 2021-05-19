d216711d6140aab8d3a3710005f791456d0030b4
Richard <richard.yang.tong@gmail.com>
2021-05-19 10:33:40 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/src7/ArrayList.java
+++ Day_23/Notes/src/com/dragontalker/src7/ArrayList.java
@@ -2,6 +2,9 @@ package com.dragontalker.src7;
 
 /**
  * 1. 数组存储在 private transient Object[] elementData;
+ * 2. 构造器里: this.elementDate = new Object[initialCapacity];
+ * 3. 默认值: int initialCapacity = 10;
+ * 4. 所以, ArrayList list = new ArrayList(); 创建了长度是10的数组elementData
  */
 
 public class ArrayList {
