ef9e05e9eb535439f34839483598a566118e2238
Richard <richard.yang.tong@gmail.com>
2021-05-19 14:55:18 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java1/SetTest.java
+++ Day_23/Notes/src/com/dragontalker/java1/SetTest.java
@@ -3,11 +3,11 @@ package com.dragontalker.java1;
 /**
  * Set接口的框架:
  *
- * 1. HashSet
+ * 1. HashSet: 作为Set接口的主要实现类, 线程不安全的, 可以储存null值
  *
- * 2. LinkedHashSet
+ * 2. LinkedHashSet: 作为HashSet的子类, 遍历其内部数据时, 可以按照添加的顺序去遍历
  *
- * 3. TreeSet
+ * 3. TreeSet: 可以按照添加对象的指定属性, 进行排序
  */
 
 public class SetTest {
