f8d667ac09458c5ec4d039c5d3db290b71877400
Richard <richard.yang.tong@gmail.com>
2021-05-19 16:04:19 -0400

    Added notes.

---
+++ Day_23/Notes/src/com/dragontalker/java1/User.java
@@ -0,0 +1,15 @@
+package com.dragontalker.java1;
+
+public class User {
+    private String name;
+    private int age;
+
+    public void User() {}
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
