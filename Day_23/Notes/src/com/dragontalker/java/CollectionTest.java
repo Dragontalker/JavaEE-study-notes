72105a95e3e6531c1f46761bd65a7659f7f64222
Richard <richard.yang.tong@gmail.com>
2021-05-18 20:04:29 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java/CollectionTest.java
+++ Day_23/Notes/src/com/dragontalker/java/CollectionTest.java
@@ -2,6 +2,9 @@ package com.dragontalker.java;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Collection接口中声明的方法的测试
  */
@@ -9,6 +12,14 @@ import org.junit.Test;
 public class CollectionTest {
     @Test
     public void test1() {
+        Collection collection = new ArrayList();
+        collection.add(123);
+        collection.add(456);
+        collection.add(new String("Tom"));
+        collection.add(false);
+        collection.add(123);
+
+        //contains()
 
     }
 }
