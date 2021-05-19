a47ab96141c1ebb25c439cba2a33e5302a1381c0
Richard <richard.yang.tong@gmail.com>
2021-05-19 15:55:33 -0400

    Added notes.

--- Day_23/Notes/src/com/dragontalker/java1/SetTest.java
+++ Day_23/Notes/src/com/dragontalker/java1/SetTest.java
@@ -8,7 +8,17 @@ package com.dragontalker.java1;
  * 2. LinkedHashSet: 作为HashSet的子类, 遍历其内部数据时, 可以按照添加的顺序去遍历
  *
  * 3. TreeSet: 可以按照添加对象的指定属性, 进行排序
+ *
+ * *******
+ *
+ * 1. Set接口中没有额外定义的方法, 都是Collection中的方法
  */
 
 public class SetTest {
+    /*
+    Set: 存储无序的, 不可重复的数据
+    1. 无序性
+
+    2. 不可重复性
+     */
 }
