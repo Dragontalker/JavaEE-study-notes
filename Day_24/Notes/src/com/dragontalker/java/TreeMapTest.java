2ddb66a96e54877ed34273cce14ac49b69f16f2f
Richard <richard.yang.tong@gmail.com>
2021-05-20 15:15:11 -0400

    Added notes.

---
+++ Day_24/Notes/src/com/dragontalker/java/TreeMapTest.java
@@ -0,0 +1,12 @@
+package com.dragontalker.java;
+
+import org.junit.Test;
+
+public class TreeMapTest {
+    //向TreeMap中添加key-value, 要求key必须是由同一个类创建的对象
+    //因为要按照key进行排序: 自然排序、定制排序
+    @Test
+    public void test1() {
+
+    }
+}
