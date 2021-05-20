3f58b677f347b42a47dd47333d7f0fb2b8e822f9
Richard <richard.yang.tong@gmail.com>
2021-05-20 09:26:26 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/exer/EmployeeTest.java
+++ Day_24/Notes/src/com/dragontalker/exer/EmployeeTest.java
@@ -44,22 +44,23 @@ public class EmployeeTest {
                     MyDate b1 = e1.getBirthday();
                     MyDate b2 = e2.getBirthday();
 
-                    //比较年
-                    int yearDiff = b1.getYear() - b2.getYear();
-
-                    if (yearDiff != 0) {
-                        return yearDiff;
-                    }
-
-                    //比较月
-                    int monthDiff = b1.getMonth() - b2.getMonth();
-
-                    if (monthDiff != 0) {
-                        return monthDiff;
-                    }
-
-                    //比较日
-                    return b1.getDay() - b2.getDay();
+//                    //比较年
+//                    int yearDiff = b1.getYear() - b2.getYear();
+//
+//                    if (yearDiff != 0) {
+//                        return yearDiff;
+//                    }
+//
+//                    //比较月
+//                    int monthDiff = b1.getMonth() - b2.getMonth();
+//
+//                    if (monthDiff != 0) {
+//                        return monthDiff;
+//                    }
+//
+//                    //比较日
+//                    return b1.getDay() - b2.getDay();
+                    return b1.compareTo(b2);
                 }
                 throw new RuntimeException("传入的数据类型不一致");
             }
