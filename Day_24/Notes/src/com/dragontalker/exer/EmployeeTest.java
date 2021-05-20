8d9ec35b223009d27b0dc5c7df2d35a031b61df0
Richard <richard.yang.tong@gmail.com>
2021-05-20 09:19:53 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/exer/EmployeeTest.java
+++ Day_24/Notes/src/com/dragontalker/exer/EmployeeTest.java
@@ -2,6 +2,7 @@ package com.dragontalker.exer;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
 
@@ -28,5 +29,37 @@ public class EmployeeTest {
         while(iterator.hasNext()) {
             System.out.println(iterator.next());
         }
+    }
+
+    //问题二: 按生日日期的先后排序
+    @Test
+    public void test2() {
+        TreeSet set = new TreeSet(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof Employee && o2 instanceof Employee) {
+                    Employee e1 = (Employee) o1;
+                    Employee e2 = (Employee) o2;
+
+                    MyDate b1 = e1.getBirthday();
+                    MyDate b2 = e2.getBirthday();
+
+                    int diffYear = b1.getYear() - b2.getYear();
+                }
+                throw new RuntimeException("传入的数据类型不一致");
+            }
+        });
+
+        Employee e1 = new Employee("liudehua", 55, new MyDate(1965, 5, 4));
+        Employee e2 = new Employee("zhangxueyou", 43, new MyDate(1987, 5, 4));
+        Employee e3 = new Employee("guofucheng", 44, new MyDate(1987, 5, 9));
+        Employee e4 = new Employee("liming", 51, new MyDate(1954, 8, 12));
+        Employee e5 = new Employee("liangchaowei", 21, new MyDate(1978, 12, 4));
+
+        set.add(e1);
+        set.add(e2);
+        set.add(e3);
+        set.add(e4);
+        set.add(e5);
     }
 }
