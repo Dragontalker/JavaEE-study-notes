211f575fada66fa20b03e159ecba7c4a6d6fa6f6
Richard <richard.yang.tong@gmail.com>
2021-05-20 10:50:12 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/java/MapTest.java
+++ Day_24/Notes/src/com/dragontalker/java/MapTest.java
@@ -2,10 +2,10 @@ package com.dragontalker.java;
 
 /**
  * Map: 双列数据, 存储key-value对的数据 --- 类似数学中的函数: y = f(X)
- *  |----HashMap: 作为Map的主要实现类
- *          |----LinkedHashMap:
+ *  |----HashMap: 作为Map的主要实现类(1.2之后才出现), 线程不安全的, 效率高
+ *          |----LinkedHashMap: (1.4之后才出现)
 *   |----TreeMap:
- *  |----Hashtable:
+ *  |----Hashtable: 作为Map的古老实现类, 线程安全, 效率低
  *          |----Properties:
  */
 
