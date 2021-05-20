b6c2e7d730488e14cf49dd4611596251f6d084b7
Richard <richard.yang.tong@gmail.com>
2021-05-20 15:33:23 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/java/PropertiesTest.java
+++ Day_24/Notes/src/com/dragontalker/java/PropertiesTest.java
@@ -9,13 +9,28 @@ public class PropertiesTest {
 
     //Properties:  常用来处理配置文件, key和value都是String类型
     public static void main(String[] args) throws IOException {
-        Properties props = new Properties();
-        FileInputStream fis = new FileInputStream("jdbc.properties");
-        props.load(fis); //加载流对应的文件
+        FileInputStream fis = null;
+        try {
+            Properties props = new Properties();
+            fis = new FileInputStream("jdbc.properties");
+            props.load(fis); //加载流对应的文件
 
-        String name = props.getProperty("name");
-        String password = props.getProperty("password");
+            String name = props.getProperty("name");
+            String password = props.getProperty("password");
 
-        System.out.println("name = " + name + ", password = " + password);
+            System.out.println("name = " + name + ", password = " + password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
     }
 }
