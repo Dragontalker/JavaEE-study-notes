b850c070cb14b16151087607ec2e13aaba632101
Richard <richard.yang.tong@gmail.com>
2021-05-20 09:00:27 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/exer/MyDate.java
+++ Day_24/Notes/src/com/dragontalker/exer/MyDate.java
@@ -4,4 +4,12 @@ public class MyDate {
     private int year;
     private int month;
     private int day;
+
+    public MyDate() {}
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 }
