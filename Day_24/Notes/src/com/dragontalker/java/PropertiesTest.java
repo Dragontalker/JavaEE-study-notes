d34291d2081b54a2e175d6e2f4f6dad59ad15c5f
Richard <richard.yang.tong@gmail.com>
2021-05-20 15:28:29 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/java/PropertiesTest.java
+++ Day_24/Notes/src/com/dragontalker/java/PropertiesTest.java
@@ -2,14 +2,20 @@ package com.dragontalker.java;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesTest {
 
     //Properties:  常用来处理配置文件, key和value都是String类型
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Properties props = new Properties();
         FileInputStream fis = new FileInputStream("jdbc.properties");
-        props.load();
+        props.load(fis); //加载流对应的文件
+
+        String name = props.getProperty("name");
+        String password = props.getProperty("password");
+
+        System.out.println("name = " + name + ", password = " + password);
     }
 }
