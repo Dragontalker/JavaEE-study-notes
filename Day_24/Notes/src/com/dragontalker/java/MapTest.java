79de61b6a0fb469f8687cbd83550f02cd23ec896
Richard <richard.yang.tong@gmail.com>
2021-05-20 10:47:53 -0400

    Added notes.

---
+++ Day_24/Notes/src/com/dragontalker/java/MapTest.java
@@ -0,0 +1,13 @@
+package com.dragontalker.java;
+
+/**
+ * Map: 双列数据, 存储key-value对的数据 --- 类似数学中的函数: y = f(X)
+ *  |----HashMap: 作为Map的主要实现类
+ *          |----LinkedHashMap:
+*   |----TreeMap:
+ *  |----Hashtable:
+ *          |----Properties:
+ */
+
+public class MapTest {
+}
