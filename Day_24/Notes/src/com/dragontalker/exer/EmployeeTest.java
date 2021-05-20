ecfddf2982ed6d7f010e2b552da571aaf4cb32a3
Richard <richard.yang.tong@gmail.com>
2021-05-20 09:09:01 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/exer/EmployeeTest.java
+++ Day_24/Notes/src/com/dragontalker/exer/EmployeeTest.java
@@ -2,9 +2,20 @@ package com.dragontalker.exer;
 
 import org.junit.Test;
 
+import java.util.TreeSet;
+
 public class EmployeeTest {
+
+    //问题一: 使用自然排序
     @Test
     public void test1() {
+        TreeSet set = new TreeSet();
+
+        Employee e1 = new Employee("liudehua", 55, new MyDate(1965, 5, 4));
+        Employee e2 = new Employee("zhangxueyou", 43, new MyDate(1987, 5, 4));
+        Employee e3 = new Employee("guofucheng", 44, new MyDate(1987, 5, 9));
+        Employee e4 = new Employee("liming", 51, new MyDate(1954, 8, 12));
+        Employee e5 = new Employee("liangchaowei", 21, new MyDate(1978, 12, 4));
 
     }
 }
