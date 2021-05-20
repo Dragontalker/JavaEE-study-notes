eed1f0d082c8001bd8642b2cda15b501c206ae15
Richard <richard.yang.tong@gmail.com>
2021-05-20 15:36:09 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/java/CollectionsTest.java
+++ Day_24/Notes/src/com/dragontalker/java/CollectionsTest.java
@@ -2,6 +2,10 @@ package com.dragontalker.java;
 
 /**
  * Collections: 操作Collection、Map的工具类
+ *
+ * 面试题: Collection 和 Collections的区别?
+ *  - Collection是接口
+ *  - Collections是工具类
  */
 
 public class CollectionsTest {
