2727d90091dd0d50ec6c54376aa36d6a3ba9dec7
Richard <richard.yang.tong@gmail.com>
2021-05-20 09:01:48 -0400

    Added notes.

--- Day_24/Notes/src/com/dragontalker/exer/Employee.java
+++ Day_24/Notes/src/com/dragontalker/exer/Employee.java
@@ -4,4 +4,12 @@ public class Employee {
     private String name;
     private int age;
     private MyDate birthday;
+
+    public Employee() {}
+
+    public Employee(String name, int age, MyDate birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
 }
