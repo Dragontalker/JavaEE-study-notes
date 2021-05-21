9c1900a5f4a6f0df6a3805838fac8ddec7b048f2
Richard <richard.yang.tong@gmail.com>
2021-05-21 10:35:14 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/exer1/DAOTest.java
+++ Day_25/Notes/src/com/dragontalker/exer1/DAOTest.java
@@ -1,9 +1,16 @@
 package com.dragontalker.exer1;
 
+import java.util.List;
+
 public class DAOTest {
     public static void main(String[] args) {
         DAO<User> dao = new DAO<>();
 
         dao.save("1001", new User(1001, 34, "周杰伦"));
+        dao.save("1002", new User(1002, 20, "昆凌"));
+        dao.save("1003", new User(1003, 25, "蔡依林"));
+
+        List<User> list = dao.list();
+        list.forEach(System.out::println);
     }
 }
