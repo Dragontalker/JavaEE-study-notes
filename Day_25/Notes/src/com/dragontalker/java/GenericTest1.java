6bd1eefeaa057d7136c1ccfdac6f17aea06fdf2d
Richard <richard.yang.tong@gmail.com>
2021-05-20 21:49:49 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/java/GenericTest1.java
+++ Day_25/Notes/src/com/dragontalker/java/GenericTest1.java
@@ -11,7 +11,8 @@ public class GenericTest1 {
     public void test1() {
         //如果定义了泛型类, 实例化没有指明类的泛型, 则认为此泛型类为Object类
         //要求: 如果大家定义了类是带泛型的, 建议在实例化时要指明类的泛型
-        Order<Integer> order = new Order<>();
-        order.setOrderT(123);
+        //建议: 实例化时指明类的泛型
+        Order<String> order = new Order<String>("AA", 1001, "orderAA");
+        order.setOrderT("Order: AA");
     }
 }
