ab9f7d9a494f2394b04e68a9ebed013b329dfe74
Richard <richard.yang.tong@gmail.com>
2021-05-20 21:42:37 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/java/Order.java
+++ Day_25/Notes/src/com/dragontalker/java/Order.java
@@ -6,4 +6,12 @@ public class Order<T> {
 
     //类的内部结构就可以使用类的泛型
     T orderT;
+
+    public Order() {}
+
+    public Order(String orderName, int orderId, T orderT) {
+        this.orderName = orderName;
+        this.orderId = orderId;
+        this.orderT = orderT;
+    }
 }
