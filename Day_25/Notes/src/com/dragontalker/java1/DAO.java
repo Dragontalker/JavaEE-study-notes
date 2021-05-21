71b1c2df96baf3da6cf1094c4517db11aa81ec36
Richard <richard.yang.tong@gmail.com>
2021-05-21 08:50:27 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/java1/DAO.java
+++ Day_25/Notes/src/com/dragontalker/java1/DAO.java
@@ -1,5 +1,7 @@
 package com.dragontalker.java1;
 
+import java.util.List;
+
 /**
  * DAO: Data(base) Access Object
  */
@@ -7,12 +9,24 @@ package com.dragontalker.java1;
 public class DAO<T> {
 
     //添加一条记录
+    public void add(T t) {}
 
     //删除一条记录
+    public boolean remove(int index) {
+        return false;
+    }
 
     //修改一条记录
+    public void update(int index, T t) {}
 
     //查询一条记录
+    public T getIndex(int index) {
+        return null;
+    }
 
     //查询多条记录
+    public List<T> getForList(int index) {
+        return null;
+    }
+
 }
