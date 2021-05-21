7f0152a8c93055dea5192ac1449a1d01e55706c1
Richard <richard.yang.tong@gmail.com>
2021-05-21 10:30:55 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/exer1/User.java
+++ Day_25/Notes/src/com/dragontalker/exer1/User.java
@@ -12,4 +12,28 @@ public class User {
         this.age = age;
         this.name = name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
