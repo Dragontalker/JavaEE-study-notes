f62794482e9f888f26753ddb6c7187039d9dc74d
Richard <richard.yang.tong@gmail.com>
2021-05-20 20:45:25 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/java/GenericTest.java
+++ Day_25/Notes/src/com/dragontalker/java/GenericTest.java
@@ -22,5 +22,11 @@ public class GenericTest {
         list.add(96);
         //问题一: 类型不安全
         list.add("Tom");
+
+        for (Object score : list) {
+            //问题二: 强转时, 有可能出现类型转换异常
+            int studentScore = (int) score; //ClassCastException
+            System.out.println(studentScore);
+        }
     }
 }
