355ff3557d859648c015848e59481ccfb3ee9004
Richard <richard.yang.tong@gmail.com>
2021-05-21 08:51:39 -0400

    Added notes.

---
+++ Day_25/Notes/src/com/dragontalker/java1/DAOTest.java
@@ -0,0 +1,16 @@
+package com.dragontalker.java1;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class DAOTest {
+
+    @Test
+    public void test1() {
+        CustomerDAO dao1 = new CustomerDAO();
+
+        dao1.add(new Customer());
+        List<Customer> list = dao1.getForList(10);
+    }
+}
