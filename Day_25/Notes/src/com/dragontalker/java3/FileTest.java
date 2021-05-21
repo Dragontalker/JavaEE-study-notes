eb6d0ed1000c849001eaa3bea5a8ebaa1b3e4e6e
Richard <richard.yang.tong@gmail.com>
2021-05-21 10:51:22 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/java3/FileTest.java
+++ Day_25/Notes/src/com/dragontalker/java3/FileTest.java
@@ -1,5 +1,9 @@
 package com.dragontalker.java3;
 
+import org.junit.Test;
+
+import java.io.File;
+
 /**
  * File类的使用
  *
@@ -8,4 +12,15 @@ package com.dragontalker.java3;
  */
 
 public class FileTest {
+    /*
+    1. 如何创建File类的实例
+
+    2.
+        - 相对路径:
+        - 绝对路径:
+     */
+    @Test
+    public void test1() {
+        File file = new File("hello.txt");
+    }
 }
