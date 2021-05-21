2453065a681c19401424a2f88df7eb3966580d76
Richard <richard.yang.tong@gmail.com>
2021-05-20 20:42:06 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/java/GenericTest.java
+++ Day_25/Notes/src/com/dragontalker/java/GenericTest.java
@@ -1,9 +1,23 @@
 package com.dragontalker.java;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * 泛型的使用
  * 1. jdk5.0新增的特性
  */
 
 public class GenericTest {
+
+    //在集合中使用泛型之前的情况:
+    @Test
+    public void test1() {
+        ArrayList list = new ArrayList();
+        list.add(78);
+        list.add(76);
+        list.add(89);
+        list.add(96);
+    }
 }
