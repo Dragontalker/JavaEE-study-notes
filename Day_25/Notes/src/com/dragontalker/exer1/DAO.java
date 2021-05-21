8bafc7bc5e6226cc30b65c0272c15b0f020d6c91
Richard <richard.yang.tong@gmail.com>
2021-05-21 10:22:04 -0400

    Added notes.

--- Day_25/Notes/src/com/dragontalker/exer1/DAO.java
+++ Day_25/Notes/src/com/dragontalker/exer1/DAO.java
@@ -7,11 +7,13 @@ public class DAO<T> {
     private Map<String, T> map;
 
     //保存T类型的对象到Map的成员变量中
-    public void save(String id, T entity) {}
+    public void save(String id, T entity) {
+        map.put(id, entity);
+    }
 
     //从map中获取id对应的对象
     public T get(String id) {
-        return null;
+        return map.get(id);
     }
 
     //替换map中key为id的内容, 改为entity对象
