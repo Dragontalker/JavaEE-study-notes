6c098b61a2b9905cf60d1cecde5dff9dbb2d05bd
Richard <richard.yang.tong@gmail.com>
2021-05-22 13:46:21 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/FIleInputOutputStreamTest.java
+++ Day_26/Notes/src/com/dragontalker/java/FIleInputOutputStreamTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -14,24 +13,34 @@ import java.io.IOException;
 public class FIleInputOutputStreamTest {
 
     @Test
-    public void testFIleInputStream() throws IOException {
-        //1. 造文件
-        File file = new File("hello.txt");
+    public void testFIleInputStream() {
+        FileInputStream fis = null;
+        try {
+            //1. 造文件
+            File file = new File("hello.txt");
 
-        //2. 造流
-        FileInputStream fis = new FileInputStream(file);
+            //2. 造流
+            fis = new FileInputStream(file);
 
-        //3. 读数据
-        byte[] buffer = new byte[5];
-        int len; //记录每次读取的字节的个数
-        while((len = fis.read(buffer)) != -1) {
+            //3. 读数据
+            byte[] buffer = new byte[5];
+            int len; //记录每次读取的字节的个数
+            while((len = fis.read(buffer)) != -1) {
 
-            String str = new String(buffer, 0, len);
-            System.out.print(str);
+                String str = new String(buffer, 0, len);
+                System.out.print(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4. 关闭资源
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
-
-
-
     }
 }
