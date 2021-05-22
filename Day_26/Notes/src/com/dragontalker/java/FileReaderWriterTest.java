5362b938cc95e4e348238b70769cbb418568b963
Richard <richard.yang.tong@gmail.com>
2021-05-21 21:19:26 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/FileReaderWriterTest.java
+++ Day_26/Notes/src/com/dragontalker/java/FileReaderWriterTest.java
@@ -22,16 +22,13 @@ import java.io.IOException;
  */
 
 public class FileReaderWriterTest {
-//    public static void main(String[] args) {
-//        File file = new File("hello.txt"); // 相较于当前工程
-//        System.out.println(file.getAbsoluteFile());
-//
-//        File file1 = new File("Notes\\hello.txt");
-//        System.out.println(file1.getAbsolutePath());
-//    }
-
     /*
     将Notes下的hello.txt文件内容读入程序中, 并输出到控制台
+
+    说明点:
+    1. read()的理解: 返回读入的一个字符, 如果达到文件末尾, 返回-1
+    2. 异常的处理: 为了保证六资源一定可以执行关闭操作, 需要使用try-catch-finally处理
+    3. 读入的文件一定要存在, 否则就会报FileNotFoundException
      */
     @Test
     public void testFileReader() {
