ed7ccfed3dc8cef15e5221df527e732893d9e644
Richard <richard.yang.tong@gmail.com>
2021-05-22 16:23:43 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/OtherStreamTest.java
+++ Day_26/Notes/src/com/dragontalker/java/OtherStreamTest.java
@@ -1,5 +1,7 @@
 package com.dragontalker.java;
 
+import org.junit.Test;
+
 /**
  * 其他流的使用
  * 1. 标准的输入、输出流
@@ -8,4 +10,14 @@ package com.dragontalker.java;
  */
 
 public class OtherStreamTest {
+    /*
+    1. 标准的输入、输出流
+        - System.in: 标准的输入流, 默认从键盘输入
+        - System.out: 标准的输出流, 默认从控制台输出
+     */
+    @Test
+    public void test1() {
+
+    }
+
 }
