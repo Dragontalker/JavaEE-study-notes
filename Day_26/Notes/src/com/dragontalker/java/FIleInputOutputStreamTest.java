fbf087d1c8a7306e13929a43ad7342c050617485
Richard <richard.yang.tong@gmail.com>
2021-05-22 13:57:09 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/FIleInputOutputStreamTest.java
+++ Day_26/Notes/src/com/dragontalker/java/FIleInputOutputStreamTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -54,9 +55,41 @@ public class FIleInputOutputStreamTest {
      */
     @Test
     public void testFileInputOutputStream() {
-        //
-        File srcFile = new File("myPicture.jpg");
-        File destFile= new File("copiedPicture.jpg");
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
 
+        try {
+            //
+            File srcFile = new File("myPicture.jpg");
+            File destFile= new File("copiedPicture.jpg");
+
+            //
+            fis = new FileInputStream(srcFile);
+            fos = new FileOutputStream(destFile);
+
+            //
+            byte[] buffer = new byte[5];
+            int len;
+            while((len = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0 , len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
