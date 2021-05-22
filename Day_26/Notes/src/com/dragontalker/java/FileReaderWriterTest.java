a97d7ebee48018ee239c7fd233c51c242abbece3
Richard <richard.yang.tong@gmail.com>
2021-05-22 09:15:09 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/FileReaderWriterTest.java
+++ Day_26/Notes/src/com/dragontalker/java/FileReaderWriterTest.java
@@ -132,25 +132,44 @@ public class FileReaderWriterTest {
     }
 
     @Test
-    public void testFileReaderFileWriter() throws IOException {
-        //1. 创建File类的对象, 指明读入和写出的文件
-        File srcFile = new File("hello.txt");
-        File destFile = new File("hello2.txt");
+    public void testFileReaderFileWriter() {
+        FileReader fr = null;
+        FileWriter fw = null;
 
-        //2. 创建输入流和输出流的对象
-        FileReader fr = new FileReader(srcFile);
-        FileWriter fw = new FileWriter(destFile);
+        try {
+            //1. 创建File类的对象, 指明读入和写出的文件
+            File srcFile = new File("hello.txt");
+            File destFile = new File("hello2.txt");
 
-        //3. 数据的读入和写出操作
-        char[] cbuf = new char[5];
-        int len; //记录每次读入到cbuf数组中的数据的字符的个数
-        while((len = fr.read(cbuf)) != -1) {
-            //每次写出len个字符
-            fw.write(cbuf, 0, len);
+            //2. 创建输入流和输出流的对象
+            fr = new FileReader(srcFile);
+            fw = new FileWriter(destFile);
+
+            //3. 数据的读入和写出操作
+            char[] cbuf = new char[5];
+            int len; //记录每次读入到cbuf数组中的数据的字符的个数
+            while((len = fr.read(cbuf)) != -1) {
+                //每次写出len个字符
+                fw.write(cbuf, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4. 关闭流资源
+            if (fr != null) {
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
-        //4. 关闭流资源
-        fr.close();
-        fw.close();
     }
 }
