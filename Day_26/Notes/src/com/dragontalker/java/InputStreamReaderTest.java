69667f786b1b02c9d16012ce50588533e4ba1a87
Richard <richard.yang.tong@gmail.com>
2021-05-22 15:27:13 -0400

    Added notes.

---
+++ Day_26/Notes/src/com/dragontalker/java/InputStreamReaderTest.java
@@ -0,0 +1,15 @@
+package com.dragontalker.java;
+
+/**
+ * 处理流之二: 转换流的使用
+ *
+ * 1. 转换流: 属于字符流
+ *  - InputStreamReader: 将一个字节的输入流转换为字符的输入流
+ *  - OutputStreamWriter: 将一个字符的输出流转换为字节的输出流
+ *
+ * 2. 作用: 提供字节流和字符流之间的转换
+ *
+ */
+
+public class InputStreamReaderTest {
+}
