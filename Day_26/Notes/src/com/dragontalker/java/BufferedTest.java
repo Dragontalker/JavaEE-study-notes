49c11c4c216994a057fd5e49c243a9e4a1655d5b
Richard <richard.yang.tong@gmail.com>
2021-05-22 14:28:51 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/BufferedTest.java
+++ Day_26/Notes/src/com/dragontalker/java/BufferedTest.java
@@ -2,6 +2,8 @@ package com.dragontalker.java;
 
 import org.junit.Test;
 
+import java.io.*;
+
 /**
  * 处理流之一: 缓冲流的使用
  *
@@ -16,8 +18,48 @@ import org.junit.Test;
 
 public class BufferedTest {
 
+    /*
+    实现非文本文件的复制
+     */
     @Test
     public void BufferedStreamTest() {
 
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try {
+            File srcFile = new File("myPicture.jpg");
+            File destFile = new File("newPicture.jpg");
+
+            fis = new FileInputStream(srcFile);
+            fos = new FileOutputStream(destFile);
+
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            byte[] buffer = new byte[10];
+            int len;
+            while((len = bis.read(buffer)) != -1) {
+                bos.write(buffer, 0, len);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
