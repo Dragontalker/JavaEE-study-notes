f2b0f471058ed5dd6552631ed8e2c7abac8b7029
Richard <richard.yang.tong@gmail.com>
2021-05-22 13:51:01 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/FIleInputOutputStreamTest.java
+++ Day_26/Notes/src/com/dragontalker/java/FIleInputOutputStreamTest.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 
 /**
  * 测试FileInputStream和 FileOutputStream的使用
+ *
+ * 结论:
+ * 1. 对于文本文件(.txt, .java, .c, .cpp), 使用字符流处理
+ * 2. 对于非文本文件(.jpg, .mp3, .mp4, .avi, .doc, .ppt), 使用字节流处理
  */
 
 public class FIleInputOutputStreamTest {
