e8f6983fc7d2ae21dd143b222cc9bb3c5ffae816
Richard <richard.yang.tong@gmail.com>
2021-05-22 14:22:42 -0400

    Added notes.

---
+++ Day_26/Notes/src/com/dragontalker/java/BufferedTest.java
@@ -0,0 +1,14 @@
+package com.dragontalker.java;
+
+/**
+ * 处理流之一: 缓冲流的使用
+ *
+ * 1. 缓冲流:
+ * BufferedInputStream
+ * BufferedOutputStream
+ * BufferedReader
+ * BufferedWriter
+ */
+
+public class BufferedTest {
+}
