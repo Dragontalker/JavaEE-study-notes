0a52d60592db32e7a85e03d9345513dd1942aab8
Richard <richard.yang.tong@gmail.com>
2021-05-21 21:04:57 -0400

    Added notes.

--- Day_26/Notes/src/com/dragontalker/java/FileReaderWriterTest.java
+++ Day_26/Notes/src/com/dragontalker/java/FileReaderWriterTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * 一、流的分类:
@@ -33,11 +34,22 @@ public class FileReaderWriterTest {
     将Notes下的hello.txt文件内容读入程序中, 并输出到控制台
      */
     @Test
-    public void testFileReader() throws FileNotFoundException {
+    public void testFileReader() throws IOException {
         //1. 实例化File类的对象, 指明要操作的文件
         File file = new File("hello.txt"); //相较于当前Module下
 
         //2. 提供具体的流
         FileReader fr = new FileReader(file);
+
+        //3. 数据的读入
+        //read(): 返回读入的一个字符, 如果达到文件末尾, 返回-1
+        int data = fr.read();
+        while(data != -1) {
+            System.out.println((char) data);
+            data = fr.read();
+        }
+
+        //4. 流的关闭操作
+        fr.close();
     }
 }
