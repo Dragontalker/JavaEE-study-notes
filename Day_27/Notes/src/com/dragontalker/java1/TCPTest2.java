4e08d5e4e5823b99a21b2245f42e2359cb7a091b
Richard <richard.yang.tong@gmail.com>
2021-05-23 12:01:19 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/TCPTest2.java
+++ Day_27/Notes/src/com/dragontalker/java1/TCPTest2.java
@@ -1,8 +1,26 @@
 package com.dragontalker.java1;
 
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * 实现TCP的网络编程
  * 例题2: 客户端发送文件给服务端, 服务端将文件保存在本地
  */
 public class TCPTest2 {
+
+    @Test
+    public void client() throws IOException {
+        Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 9090);
+
+    }
+
+    @Test
+    public void server() {
+
+    }
 }
