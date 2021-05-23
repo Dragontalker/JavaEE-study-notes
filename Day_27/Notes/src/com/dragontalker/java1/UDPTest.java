fc6550206179e9dc25f889ba364debcb04ce6bc6
Richard <richard.yang.tong@gmail.com>
2021-05-23 14:25:35 -0400

    Added notes.

---
+++ Day_27/Notes/src/com/dragontalker/java1/UDPTest.java
@@ -0,0 +1,22 @@
+package com.dragontalker.java1;
+
+import org.junit.Test;
+
+/**
+ * UDP协议的网络编程
+ */
+
+public class UDPTest {
+    //发送端
+    @Test
+    public void sender() {
+
+    }
+
+    //接收端
+    @Test
+    public void receiver() {
+
+    }
+
+}
