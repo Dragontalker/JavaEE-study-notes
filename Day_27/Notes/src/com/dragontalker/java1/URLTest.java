c0e734e8cddba4a58d8a2bd4fca0582b5578ed26
Richard <richard.yang.tong@gmail.com>
2021-05-23 14:40:12 -0400

    Added notes.

---
+++ Day_27/Notes/src/com/dragontalker/java1/URLTest.java
@@ -0,0 +1,12 @@
+package com.dragontalker.java1;
+
+/**
+ * URL网络编程
+ * 1. URL: 统一资源定位符, 对应着互联网的某一资源地址
+ * 2. 格式:
+ *  http(协议)://localhost(主机名):8080(端口号)/
+ *      examples/beauty.jpg(资源地址)?username=Xxx(参数列表)
+ */
+
+public class URLTest {
+}
