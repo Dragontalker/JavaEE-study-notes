c96a37a455233b69a24983ef5116a42f04d18caf
Richard <richard.yang.tong@gmail.com>
2021-05-23 13:54:47 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/TCPTest2.java
+++ Day_27/Notes/src/com/dragontalker/java1/TCPTest2.java
@@ -2,7 +2,10 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -16,7 +19,19 @@ public class TCPTest2 {
     @Test
     public void client() throws IOException {
         Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 9090);
+        OutputStream os = socket.getOutputStream();
 
+        FileInputStream fis = new FileInputStream(new File("myPicture.jpg"));
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len = fis.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+
+        fis.close();
+        os.close();
+        socket.close();
     }
 
     @Test
