30e9b4f2722d541bd3f26a2c1508668eda096aed
Richard <richard.yang.tong@gmail.com>
2021-05-23 06:42:03 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java/Person.java
+++ Day_27/Notes/src/com/dragontalker/java/Person.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
  * Person需要满足如下的要求, 方可序列化
  * 1. 需要实现接口: Serializable
  * 2. 当前类提供一个全局常量: serialVersionUID
+ * 3. 除了当前Person类许需要实现Serializable接口之外, 还需要保证其内部所有属性
+ *  也必须是可序列化的(默认情况下, 基础数据类型都是可序列化的)
  */
 
 public class Person implements Serializable {
@@ -13,6 +15,7 @@ public class Person implements Serializable {
 
     private String name;
     private int age;
+    private Account account;
 
     public Person() {}
 
@@ -43,5 +46,23 @@ public class Person implements Serializable {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+}
+
+class Account implements Serializable{
+    public static final long serialVersionUID = 475463534542L;
+
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 }
