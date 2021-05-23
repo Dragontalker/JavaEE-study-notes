bc23db2a61079808b52832357cb43016ca3bb086
Richard <richard.yang.tong@gmail.com>
2021-05-23 14:29:33 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/UDPTest.java
+++ Day_27/Notes/src/com/dragontalker/java1/UDPTest.java
@@ -2,8 +2,10 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * UDP协议的网络编程
@@ -13,11 +15,23 @@ public class UDPTest {
     //发送端
     @Test
     public void sender() {
-        DatagramSocket socket = new DatagramSocket();
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket();
 
-        String str = "我是UDP方式发送的导弹!";
-        byte[] data = str.getBytes();
-        DatagramPacket packet = new DatagramPacket(data, 0, data.length);
+            String str = "我是UDP方式发送的导弹!";
+            byte[] data = str.getBytes();
+            InetAddress inet = InetAddress.getLocalHost();
+            DatagramPacket packet = new DatagramPacket(data, 0, data.length, inet, 9090);
+
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
     }
 
     //接收端
