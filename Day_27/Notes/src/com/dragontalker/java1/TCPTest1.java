8a7d0f1df286de6eeb73a1e350f8c95f0e15f734
Richard <richard.yang.tong@gmail.com>
2021-05-23 10:00:41 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/TCPTest1.java
+++ Day_27/Notes/src/com/dragontalker/java1/TCPTest1.java
@@ -1,10 +1,22 @@
 package com.dragontalker.java1;
 
+import org.junit.Test;
+
 /**
  * 实现TCP的网络编程
  *
- * 例子1:
+ * 例子1: 客户端发送信息给服务端, 服务端将信息展现在控制台上
  */
 
 public class TCPTest1 {
+    //客户端
+    @Test
+    public void client() {
+
+    }
+    //服务端
+    @Test
+    public void server() {
+
+    }
 }
