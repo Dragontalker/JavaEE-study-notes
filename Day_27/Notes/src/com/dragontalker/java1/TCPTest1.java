ab6a23783fe09e93176b44988963af69908f3f44
Richard <richard.yang.tong@gmail.com>
2021-05-23 11:42:21 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/TCPTest1.java
+++ Day_27/Notes/src/com/dragontalker/java1/TCPTest1.java
@@ -2,6 +2,12 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * 实现TCP的网络编程
  *
@@ -12,7 +18,32 @@ public class TCPTest1 {
     //客户端
     @Test
     public void client() {
+        Socket socket = null;
+        OutputStream os = null;
+        try {
+            InetAddress inet = InetAddress.getByName("127.0.0.1");
+            socket = new Socket(inet, 8899);
 
+            os = socket.getOutputStream();
+            os.write("您好, 我是客户端MM".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
     //服务端
     @Test
