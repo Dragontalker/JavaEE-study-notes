d6e4b67e39e878f004efd075ccd33572601c83f0
Richard <richard.yang.tong@gmail.com>
2021-05-23 06:54:46 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java/RandomAccessFileTest.java
+++ Day_27/Notes/src/com/dragontalker/java/RandomAccessFileTest.java
@@ -1,5 +1,11 @@
 package com.dragontalker.java;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  * RandomAccessFile的使用
  * 1. RandomAccessFile直接继承于java.lang.Object类
@@ -9,4 +15,37 @@ package com.dragontalker.java;
  */
 
 public class RandomAccessFileTest {
+
+    @Test
+    public void test1() {
+        RandomAccessFile raf1 = null;
+        RandomAccessFile raf2 = null;
+        try {
+            raf1 = new RandomAccessFile(new File("myPicture.jpg"), "r");
+            raf2 = new RandomAccessFile(new File("newPicture.jpg"), "rw");
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = raf1.read(buffer)) != -1) {
+                raf2.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (raf1 != null) {
+                try {
+                    raf1.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (raf2 != null) {
+                try {
+                    raf2.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
