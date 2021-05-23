6d1e7726d255ca924e71066a1771eefe29b8328c
Richard <richard.yang.tong@gmail.com>
2021-05-23 14:27:49 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/UDPTest.java
+++ Day_27/Notes/src/com/dragontalker/java1/UDPTest.java
@@ -2,6 +2,9 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 /**
  * UDP协议的网络编程
  */
@@ -10,7 +13,11 @@ public class UDPTest {
     //发送端
     @Test
     public void sender() {
+        DatagramSocket socket = new DatagramSocket();
 
+        String str = "我是UDP方式发送的导弹!";
+        byte[] data = str.getBytes();
+        DatagramPacket packet = new DatagramPacket(data, 0, data.length);
     }
 
     //接收端
