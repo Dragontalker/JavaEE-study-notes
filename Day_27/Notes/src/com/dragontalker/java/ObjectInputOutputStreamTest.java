d3af1fed9fd4006bb4ab10aa536f4683a4cd7d44
Richard <richard.yang.tong@gmail.com>
2021-05-23 06:11:46 -0400

    Added notes.

---
+++ Day_27/Notes/src/com/dragontalker/java/ObjectInputOutputStreamTest.java
@@ -0,0 +1,11 @@
+package com.dragontalker.java;
+
+/**
+ * 对象流的使用
+ * 1. ObjectInputStream和 ObjectOutputStream
+ *
+ * 2. 作用: 用于存储和读取基本数据类型数据或对象的处理流
+ */
+
+public class ObjectInputOutputStreamTest {
+}
