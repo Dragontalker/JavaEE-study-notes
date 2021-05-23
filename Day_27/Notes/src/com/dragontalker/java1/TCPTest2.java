1f30c648397c3bef410d5f9c62ef65a72612329b
Richard <richard.yang.tong@gmail.com>
2021-05-23 13:55:40 -0400

    Added notes.

--- Day_27/Notes/src/com/dragontalker/java1/TCPTest2.java
+++ Day_27/Notes/src/com/dragontalker/java1/TCPTest2.java
@@ -17,21 +17,46 @@ import java.net.UnknownHostException;
 public class TCPTest2 {
 
     @Test
-    public void client() throws IOException {
-        Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 9090);
-        OutputStream os = socket.getOutputStream();
+    public void client() {
+        Socket socket = null;
+        OutputStream os = null;
+        FileInputStream fis = null;
+        try {
+            socket = new Socket(InetAddress.getByName("127.0.0.1"), 9090);
+            os = socket.getOutputStream();
 
-        FileInputStream fis = new FileInputStream(new File("myPicture.jpg"));
+            fis = new FileInputStream(new File("myPicture.jpg"));
 
-        byte[] buffer = new byte[1024];
-        int len;
-        while((len = fis.read(buffer)) != -1) {
-            os.write(buffer, 0, len);
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
-        fis.close();
-        os.close();
-        socket.close();
     }
 
     @Test
