352af7e2dc1b37256d84b78fbc5f620957fc917d
Richard <richard.yang.tong@gmail.com>
2021-05-23 20:34:05 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java/ReflectionTest.java
+++ Day_28/Notes/src/com/dragontalker/java/ReflectionTest.java
@@ -19,4 +19,19 @@ public class ReflectionTest {
         //比如: name属性, showNation()方法, 已经私有的Person(String name)构造器
     }
 
+    //反射之后, 对于Person类的操作
+    @Test
+    public void test2() {
+        //1. 创建Person类的对象
+        Person p1 = new Person("Tom", 12);
+
+        //2. 通过对象调用其内部的属性和方法
+        p1.age = 10;
+        System.out.println(p1);
+
+        p1.show();
+
+        //Person类外部, 不可以通过Person类的对象调用其私有的结构的
+        //比如: name属性, showNation()方法, 已经私有的Person(String name)构造器
+    }
 }
