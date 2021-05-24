26036bcdb9f8a9add7d864961d61eadd83c265d3
Richard <richard.yang.tong@gmail.com>
2021-05-24 11:30:51 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java2/OtherTest.java
+++ Day_28/Notes/src/com/dragontalker/java2/OtherTest.java
@@ -1,4 +1,20 @@
 package com.dragontalker.java2;
 
+import com.dragontalker.java1.Person;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
 public class OtherTest {
+    /*
+    获取构造器结构
+     */
+    @Test
+    public void test1() {
+        Class<Person> clazz = Person.class;
+        Constructor[] constructors = clazz.getConstructors();
+        for (Constructor c : constructors) {
+            System.out.println(c);
+        }
+    }
 }
