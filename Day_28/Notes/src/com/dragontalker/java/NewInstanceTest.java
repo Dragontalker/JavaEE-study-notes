120df232995ea6409d4eb9a676db32debd4c8dce
Richard <richard.yang.tong@gmail.com>
2021-05-24 08:53:52 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java/NewInstanceTest.java
+++ Day_28/Notes/src/com/dragontalker/java/NewInstanceTest.java
@@ -14,6 +14,11 @@ public class NewInstanceTest {
         Class<Person> clazz = Person.class;
         /*
         newInstance(): 调用此方法, 创建对应的运行时类的对象
+            内部调用了运行时类的空参构造器
+
+        要想此方法正常的创建运行时类的对现象, 要求:
+            1. 运行时类必须提供空参的构造器
+            2. 空参的构造器的访问权限符合访问需求, 通常: 设置为public
          */
         Person obj = clazz.newInstance();
         System.out.println(obj);
