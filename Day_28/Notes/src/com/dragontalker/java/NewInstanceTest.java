d11448122cf5204bd071627d63be9a460348e722
Richard <richard.yang.tong@gmail.com>
2021-05-24 08:56:27 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java/NewInstanceTest.java
+++ Day_28/Notes/src/com/dragontalker/java/NewInstanceTest.java
@@ -19,6 +19,11 @@ public class NewInstanceTest {
         要想此方法正常的创建运行时类的对现象, 要求:
             1. 运行时类必须提供空参的构造器
             2. 空参的构造器的访问权限符合访问需求, 通常: 设置为public
+
+        通常在JavaBean中要求提供一个public的空参构造器,
+            - 原因:
+                1. 便于通过反射, 创建运行时类的对象
+                2. 便于子类继承此运行类时, 默认调用super()时, 保证父类有此构造器
          */
         Person obj = clazz.newInstance();
         System.out.println(obj);
