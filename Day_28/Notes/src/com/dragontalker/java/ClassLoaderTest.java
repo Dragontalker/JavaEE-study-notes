842f922c5843da324f48603c07b0de1d3c5b8d75
Richard <richard.yang.tong@gmail.com>
2021-05-24 06:58:35 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java/ClassLoaderTest.java
+++ Day_28/Notes/src/com/dragontalker/java/ClassLoaderTest.java
@@ -11,5 +11,11 @@ public class ClassLoaderTest {
     public void test1() {
         ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
         System.out.println(classLoader);
+
+        ClassLoader classLoader1 = classLoader.getParent();
+        System.out.println(classLoader1);
+
+        ClassLoader classLoader2 = classLoader1.getParent();
+        System.out.println(classLoader2); //Bootstrap引导器不能被直接获取到
     }
 }
