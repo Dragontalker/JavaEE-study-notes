b6977124af82005473cfd9b670c690dd3029eabc
Richard <richard.yang.tong@gmail.com>
2021-05-23 20:30:32 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java/ReflectionTest.java
+++ Day_28/Notes/src/com/dragontalker/java/ReflectionTest.java
@@ -1,4 +1,13 @@
 package com.dragontalker.java;
 
+import org.junit.Test;
+
 public class ReflectionTest {
+    //反射之前, 对于Person类的操作
+    @Test
+    public void test1() {
+        //1. 创建Person类的对象
+        Person p1 = new Person();
+    }
+
 }
