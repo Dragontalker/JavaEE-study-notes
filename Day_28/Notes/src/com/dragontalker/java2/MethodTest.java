577696534701427a250bc654d23d462d9ec6a2fe
Richard <richard.yang.tong@gmail.com>
2021-05-24 10:59:01 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java2/MethodTest.java
+++ Day_28/Notes/src/com/dragontalker/java2/MethodTest.java
@@ -1,6 +1,9 @@
 package com.dragontalker.java2;
 
+import com.dragontalker.java1.Person;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 /**
  * 获取运行时类的方法结构
@@ -9,6 +12,12 @@ import org.junit.Test;
 public class MethodTest {
     @Test
     public void test1() {
+        Class<Person> clazz = Person.class;
 
+        //
+        Method[] methods = clazz.getMethods();
+        for (Method m : methods) {
+            System.out.println(m);
+        }
     }
 }
