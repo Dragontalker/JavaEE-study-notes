72bcd8f8f6d7c587a19756bca2ce02f1a0b33311
Richard <richard.yang.tong@gmail.com>
2021-05-24 11:32:39 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java2/OtherTest.java
+++ Day_28/Notes/src/com/dragontalker/java2/OtherTest.java
@@ -12,8 +12,16 @@ public class OtherTest {
     @Test
     public void test1() {
         Class<Person> clazz = Person.class;
+        //getConstructor(): 获取当前运行时中声明为public的构造器
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor c : constructors) {
+            System.out.println(c);
+        }
+        System.out.println();
+
+        //getDeclaredConstructors(): 获取当前运行时类中声明的所有的构造器
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        for (Constructor c : declaredConstructors) {
             System.out.println(c);
         }
     }
