c1f19ea6b04c42f70079cf6e52f0f05c04b8f526
Richard <richard.yang.tong@gmail.com>
2021-05-23 20:31:45 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java/Person.java
+++ Day_28/Notes/src/com/dragontalker/java/Person.java
@@ -31,6 +31,14 @@ public class Person {
         this.age = age;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public void show() {
         System.out.println("你好! 我是一个人~~");
     }
--- Day_28/Notes/src/com/dragontalker/java/ReflectionTest.java
+++ Day_28/Notes/src/com/dragontalker/java/ReflectionTest.java
@@ -7,7 +7,11 @@ public class ReflectionTest {
     @Test
     public void test1() {
         //1. 创建Person类的对象
-        Person p1 = new Person();
+        Person p1 = new Person("Tom", 12);
+
+        //2. 通过对象调用其内部的属性和方法
+        p1.age = 10;
+        System.out.println(p1);
     }
 
 }
