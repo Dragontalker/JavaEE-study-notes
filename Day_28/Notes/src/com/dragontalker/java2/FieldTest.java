7c23ab1effd18103248d44d2e95ac3b07ec9f933
Richard <richard.yang.tong@gmail.com>
2021-05-24 09:58:11 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java2/FieldTest.java
+++ Day_28/Notes/src/com/dragontalker/java2/FieldTest.java
@@ -1,6 +1,9 @@
 package com.dragontalker.java2;
 
+import com.dragontalker.java1.Person;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
 
 /**
  * 获取当前运行时类的属性结构
@@ -9,6 +12,13 @@ import org.junit.Test;
 public class FieldTest {
     @Test
     public void test1() {
+        Class clazz = Person.class;
+
+        //获取属性结构
+        Field[] fields = clazz.getFields();
+        for (Field f : fields) {
+            System.out.println(f);
+        }
 
     }
 }
