427045586af749261fdd16ecb7810f73fb9a3f0b
Richard <richard.yang.tong@gmail.com>
2021-05-24 09:46:14 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java1/Person.java
+++ Day_28/Notes/src/com/dragontalker/java1/Person.java
@@ -1,6 +1,11 @@
 package com.dragontalker.java1;
 
-public class Person {
+public class Person extends Creature<String> implements Comparable<String>{
     private String name;
     private int age;
+
+    @Override
+    public int compareTo(String o) {
+        return 0;
+    }
 }
