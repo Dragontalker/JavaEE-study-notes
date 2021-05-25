1e7b58a0c95a8908f2cf8eac18e74381a1d5827a
Richard <richard.yang.tong@gmail.com>
2021-05-25 09:05:53 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java/StaticProxyTest.java
+++ Day_29/Notes/src/com/dragontalker/java/StaticProxyTest.java
@@ -3,5 +3,19 @@ package com.dragontalker.java;
 /**
  * 静态代理举例
  */
+
+interface ClothFactory {
+    void produceCloth();
+}
+
+//代理类
+class ProxyClothFactory implements ClothFactory {
+
+    @Override
+    public void produceCloth() {
+
+    }
+}
+
 public class StaticProxyTest {
 }
