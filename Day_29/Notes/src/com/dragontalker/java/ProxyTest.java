a4ccfd1096259985eee48d0358c0f32ee17326c0
Richard <richard.yang.tong@gmail.com>
2021-05-25 09:16:20 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java/ProxyTest.java
+++ Day_29/Notes/src/com/dragontalker/java/ProxyTest.java
@@ -15,12 +15,12 @@ class SuperMan implements Human{
 
     @Override
     public String getBelief() {
-        return null;
+        return "I believe I can fly!";
     }
 
     @Override
     public void eat(String food) {
-
+        System.out.println("我喜欢吃" + food);
     }
 }
 
