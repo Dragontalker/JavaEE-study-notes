8b54e08e8299882169d451f7269b28986fd469c6
Richard <richard.yang.tong@gmail.com>
2021-05-25 14:04:15 -0400

    Added notes.

---
+++ Day_29/Notes/src/com/dragontalker/java1/LambdaTest2.java
@@ -0,0 +1,16 @@
+package com.dragontalker.java1;
+
+/**
+ * Java内置的4大核心函数式接口
+ *
+ * 1. 消费型接口 Consumer<T> void accept(T t)
+ *
+ * 2. 供给型接口 Supplier<T> T get()
+ *
+ * 3. 函数型接口 Function<T, R> R apply(T t)
+ *
+ * 4. 断定型接口 Predicate<T> boolean test(T t)
+ */
+
+public class LambdaTest2 {
+}
