19a99486ec3f7bcf7d07d96cb80d40707efe83d0
Richard <richard.yang.tong@gmail.com>
2021-05-24 20:37:35 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java2/ReflectionTest.java
+++ Day_28/Notes/src/com/dragontalker/java2/ReflectionTest.java
@@ -3,6 +3,8 @@ package com.dragontalker.java2;
 import com.dragontalker.java1.Person;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+
 /**
  * 调用运行时类中指定的结构: 属性、方法、构造器
  */
@@ -13,7 +15,10 @@ public class ReflectionTest {
 
      */
     @Test
-    public void testFiled() {
+    public void testFiled() throws Exception{
         Class<Person> clazz = Person.class;
+
+        //获取指定的属性
+        Field ids = clazz.getField("id");
     }
 }
