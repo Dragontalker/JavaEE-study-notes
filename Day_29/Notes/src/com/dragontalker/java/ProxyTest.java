4dd1f89b88a05404d9654a7226e236baf61fec24
Richard <richard.yang.tong@gmail.com>
2021-05-25 09:31:28 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java/ProxyTest.java
+++ Day_29/Notes/src/com/dragontalker/java/ProxyTest.java
@@ -1,5 +1,9 @@
 package com.dragontalker.java;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * 动态代理的举例
  */
@@ -29,5 +33,29 @@ class SuperMan implements Human{
  - 问题1: 如何根据加载到内存中的被代理类, 动态的创建一个代理类及其对象?
  - 问题2: 当通过代理类的对象调用方法时, 如何动态的去调用被代理类中的同名方法?
  */
+
+class ProxyFactory {
+    //obj: 被代理类的对象
+    //调用此方法, 返回一个代理类的对象, 解决问题1
+    public static Object getProxyInstance(Object obj) {
+
+        MyInvocationHandler handler = new MyInvocationHandler();
+        return Proxy.newProxyInstance(
+                obj.getClass().getClassLoader(),
+                obj.getClass().getInterfaces(),
+                handler
+        );
+    }
+}
+
+class MyInvocationHandler implements InvocationHandler {
+
+    //当我们通过代理类的对象, 调用方法a时, 就会自动调用如下的方法: invoke()
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
+
 public class ProxyTest {
 }
