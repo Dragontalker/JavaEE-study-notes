340c590d0050f7af6222ee98265c489b1a46df0e
Richard <richard.yang.tong@gmail.com>
2021-05-24 20:35:34 -0400

    Added notes.

---
+++ Day_28/Notes/src/com/dragontalker/java2/ReflectionTest.java
@@ -0,0 +1,19 @@
+package com.dragontalker.java2;
+
+import com.dragontalker.java1.Person;
+import org.junit.Test;
+
+/**
+ * 调用运行时类中指定的结构: 属性、方法、构造器
+ */
+
+public class ReflectionTest {
+
+    /*
+
+     */
+    @Test
+    public void testFiled() {
+        Class<Person> clazz = Person.class;
+    }
+}
