e0f139194ad9d393036d27fb3939812f09509833
Richard <richard.yang.tong@gmail.com>
2021-05-25 15:08:38 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java2/Employee.java
+++ Day_29/Notes/src/com/dragontalker/java2/Employee.java
@@ -36,4 +36,13 @@ public class Employee {
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
