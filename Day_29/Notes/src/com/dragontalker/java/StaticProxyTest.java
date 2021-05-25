314e23e1f211cd5404bb1d7edb12d197f249b453
Richard <richard.yang.tong@gmail.com>
2021-05-25 09:08:29 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java/StaticProxyTest.java
+++ Day_29/Notes/src/com/dragontalker/java/StaticProxyTest.java
@@ -25,5 +25,14 @@ class ProxyClothFactory implements ClothFactory {
     }
 }
 
+//被代理类
+class NikeClothFactory implements ClothFactory {
+
+    @Override
+    public void produceCloth() {
+        System.out.println("Nike工厂在生产一批运动服");
+    }
+}
+
 public class StaticProxyTest {
 }
