d69fba646dc3851d23432d50aaf1fe42bc668b04
Richard <richard.yang.tong@gmail.com>
2021-05-25 15:08:15 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java2/ConstructorRefTest.java
+++ Day_29/Notes/src/com/dragontalker/java2/ConstructorRefTest.java
@@ -16,9 +16,11 @@ public class ConstructorRefTest {
     @Test
     public void test1() {
         Supplier<Employee> sup1 = () -> new Employee();
+        System.out.println(sup1.get());
 
         System.out.println("************");
 
         Supplier<Employee> sup2 = Employee::new;
+        System.out.println(sup2.get());
     }
 }
