638d46755f8b2b22845e03a7fe1f868812281b3d
Richard <richard.yang.tong@gmail.com>
2021-05-25 13:38:42 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java1/LambdaTest1.java
+++ Day_29/Notes/src/com/dragontalker/java1/LambdaTest1.java
@@ -2,6 +2,7 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.function.Consumer;
 
 /**
@@ -79,7 +80,6 @@ public class LambdaTest1 {
     }
 
     //语法格式4: Lambda或需要一个参数时, 参数的小括号也可以省略
-    //称为"类型推断"
     @Test
     public void test4() {
         Consumer<String> con = new Consumer<String>() {
@@ -97,5 +97,51 @@ public class LambdaTest1 {
             System.out.println(s);
         };
         con1.accept("一个是听的人当真了, 一个是说的人当真了");
+    }
+
+    //语法格式5:
+    //1. Lambda需要两个或以上的参数
+    //2. 多条执行语句
+    //3. 并且有返回值
+    @Test
+    public void test5() {
+        Comparator<Integer> com1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                System.out.println(o1);
+                System.out.println(o2);
+                return o1.compareTo(o2);
+            }
+        };
+
+        com1.compare(12, 21);
+
+        System.out.println("*****************");
+
+        Comparator<Integer> com2 = (o1, o2) -> {
+                System.out.println(o1);
+                System.out.println(o2);
+                return o1.compareTo(o2);
+        };
+
+        System.out.println(com2.compare(12, 6));
+    }
+
+    //语法格式六: 当Lambda体只有一条语句时, return与大括号可以省略
+    @Test
+    public void test6() {
+        Comparator<Integer> com1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        com1.compare(12, 21);
+
+        System.out.println("*****************");
+
+        Comparator<Integer> com2 = (o1, o2) -> o1.compareTo(o2);
+        System.out.println(com2.compare(12, 6));
     }
 }
