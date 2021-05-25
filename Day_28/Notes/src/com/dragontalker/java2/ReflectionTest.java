a6c21ab95ae7ecc8a0136fc443a1eb7b36a9f3b6
Richard <richard.yang.tong@gmail.com>
2021-05-24 20:40:29 -0400

    Added notes.

--- Day_28/Notes/src/com/dragontalker/java2/ReflectionTest.java
+++ Day_28/Notes/src/com/dragontalker/java2/ReflectionTest.java
@@ -18,7 +18,27 @@ public class ReflectionTest {
     public void testFiled() throws Exception{
         Class<Person> clazz = Person.class;
 
+        //创建运行时类的对象
+        Person p = clazz.newInstance();
+
         //获取指定的属性
-        Field ids = clazz.getField("id");
+        Field id = clazz.getField("id");
+
+        /*
+        设置当前属性的值
+
+        set(): 参数1: 指明设置哪个对象的属性
+               参数2: 将此属性值设置为多少
+         */
+
+        id.set(p, 1001);
+
+        /*
+        获取当前属性的值
+
+        get(): 参数1: 获取哪个对象的当前属性值
+         */
+        int pId = (int) id.get(p);
+        System.out.println(pId);
     }
 }
