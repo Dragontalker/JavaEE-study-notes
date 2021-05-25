981a97d1716d809c22bd7e6c09b9b153a8dc1a26
Richard <richard.yang.tong@gmail.com>
2021-05-25 11:48:39 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java1/LambdaTest.java
+++ Day_29/Notes/src/com/dragontalker/java1/LambdaTest.java
@@ -2,6 +2,8 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.util.Comparator;
+
 /**
  * Lambda表达式的使用举例
  */
@@ -9,7 +11,7 @@ import org.junit.Test;
 public class LambdaTest {
 
     @Test
-    public void testOriginal() {
+    public void test1() {
         Runnable r1 = new Runnable() {
             @Override
             public void run() {
@@ -18,12 +20,35 @@ public class LambdaTest {
         };
 
         r1.run();
+
+        //使用Lambda表达式重写
+        Runnable r2 = () -> System.out.println("我爱北京故宫");
+        r2.run();
     }
 
-    //使用Lambda表达式重写
+
     @Test
-    public void testLambda() {
-        Runnable r1 = () -> System.out.println("我爱北京故宫");
-        r1.run();
+    public void test2() {
+        Comparator<Integer> com1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        };
+
+        int compare1 = com1.compare(12, 21);
+        System.out.println(compare1);
+
+        //使用Lambda表达式重写
+        Comparator<Integer> com2 = (o1, o2) -> Integer.compare(o1, o2);
+
+        int compare2 = com2.compare(32, 21);
+        System.out.println(compare2);
+
+        //方法引用
+        Comparator<Integer> com3 = Integer :: compare;
+
+        int compare3 = com3.compare(32, 21);
+        System.out.println(compare3);
     }
 }
