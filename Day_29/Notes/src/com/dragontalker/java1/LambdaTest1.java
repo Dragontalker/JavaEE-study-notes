b99c09c1c21ca68031034c4ab3a40d78380dff1e
Richard <richard.yang.tong@gmail.com>
2021-05-25 11:53:06 -0400

    Added notes.

---
+++ Day_29/Notes/src/com/dragontalker/java1/LambdaTest1.java
@@ -0,0 +1,15 @@
+package com.dragontalker.java1;
+
+/**
+ * Lambda表达式的使用
+ *
+ * 1. 举例: (o1, o2) -> Integer.compare(o1, o2);
+ *
+ * 2. 格式:
+ *  -> : Lambda操作符 或 箭头操作符
+ *  ->的左边: Lambda形参列表(其实就是接口中的抽象方法的形参列表)
+ *  ->的右边: Lambda体(其实就是重写的抽象方法的方法体)
+ */
+
+public class LambdaTest1 {
+}
