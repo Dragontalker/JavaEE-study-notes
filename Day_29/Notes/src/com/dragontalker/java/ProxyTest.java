3ed2eaa14a66af52a727a9a9a54c58537b7f9a5f
Richard <richard.yang.tong@gmail.com>
2021-05-25 09:33:30 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java/ProxyTest.java
+++ Day_29/Notes/src/com/dragontalker/java/ProxyTest.java
@@ -50,7 +50,14 @@ class ProxyFactory {
 
 class MyInvocationHandler implements InvocationHandler {
 
+    private Object obj; //赋值时, 也需要被代理类的对象进行赋值
+
+    public void bind(Object obj) {
+        this.obj = obj;
+    }
+
     //当我们通过代理类的对象, 调用方法a时, 就会自动调用如下的方法: invoke()
+    //将被代理类要执行的方法a的功能就声明在invoke()中
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return null;
