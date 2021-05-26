9c03fd99272acbd2a278d45fcc31e5f430a4531c
Richard <richard.yang.tong@gmail.com>
2021-05-26 11:56:57 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test.java
@@ -2,10 +2,18 @@ package com.dragontalker.java;
 
 import org.junit.Test;
 
+import java.util.Comparator;
+
 public class Java9Test {
 
+    //java9特性五: 钻石操作符的升级
     @Test
     public void test1() {
-
+        Comparator<Object> com = new Comparator<>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        };
     }
 }
