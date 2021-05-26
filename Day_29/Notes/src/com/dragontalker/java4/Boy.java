7bb4df0d713049d54bdf4c214a0238d4749d0aca
Richard <richard.yang.tong@gmail.com>
2021-05-25 22:06:38 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java4/Boy.java
+++ Day_29/Notes/src/com/dragontalker/java4/Boy.java
@@ -1,4 +1,26 @@
 package com.dragontalker.java4;
 
 public class Boy {
+    private Girl girl;
+
+    public Boy() {}
+
+    public Boy(Girl girl) {
+        this.girl = girl;
+    }
+
+    public Girl getGirl() {
+        return girl;
+    }
+
+    public void setGirl(Girl girl) {
+        this.girl = girl;
+    }
+
+    @Override
+    public String toString() {
+        return "Boy{" +
+                "girl=" + girl +
+                '}';
+    }
 }
