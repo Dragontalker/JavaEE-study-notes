29cdfd511317855e45a8d6929eef80d823e6953c
Richard <richard.yang.tong@gmail.com>
2021-05-26 18:21:03 -0400

    Added notes.

---
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test1.java
@@ -0,0 +1,25 @@
+package com.dragontalker.java;
+
+import org.junit.Test;
+
+import java.lang.reflect.InaccessibleObjectException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Java9Test1 {
+
+    //java9新特性八: 集合工厂方法: 创建只读集合
+    //Java8中的写法:
+    @Test
+    public void test1() {
+        List<String> nameList = new ArrayList<>();
+        nameList.add("Joe");
+        nameList.add("Bob");
+        nameList.add("Bill");
+        System.out.println(nameList);
+        nameList = Collections.unmodifiableList(nameList);
+        //nameList.add("Tom"); 报异常: UnsupportedOperationException
+        System.out.println(nameList);
+    }
+}
--- Day_30_Last_Day/Notes/src/module-info.java
+++ Day_30_Last_Day/Notes/src/module-info.java
@@ -1,5 +1,6 @@
 module Notes {
 
+    exports com.dragontalker.java;
     requires Java9Test;
     requires junit;
 }
