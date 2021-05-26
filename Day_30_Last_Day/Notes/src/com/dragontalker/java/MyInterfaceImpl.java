570925c014e994ad81995cc17f4d7918de998795
Richard <richard.yang.tong@gmail.com>
2021-05-26 11:51:46 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java/MyInterfaceImpl.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/MyInterfaceImpl.java
@@ -12,6 +12,10 @@ public class MyInterfaceImpl implements MyInterface{
     }
 
     public static void main(String[] args) {
+        //接口中的静态方法只能由接口自己调用
+        MyInterface.staticMethod();
 
+        //接口的实现类不能调用接口的静态方法
+//        MyInterfaceImpl.staticMethod();
     }
 }
