0b4e2d325e31e4f8452914f68a210614992ef847
Richard <richard.yang.tong@gmail.com>
2021-05-26 18:45:17 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test2.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test2.java
@@ -13,5 +13,8 @@ public class Java9Test2 {
         //takeWhile(): 返回从开头开始的按指定规则尽量多的元素
         List<Integer> list = Arrays.asList(23, 43, 45, 55, 61, 54, 32, 2, 45, 89, 7);
         //list.stream().takeWhile(x -> x < 60).forEach(System.out::println);
+
+        //dropWhile(): 返回剩余的元素
+        list.stream().dropWhile(x -> x < 60).forEach(System.out::println);
     }
 }
