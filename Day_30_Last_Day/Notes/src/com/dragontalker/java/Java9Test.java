582ecf0d4583bf6ce74a76418c436733ecf90b18
Richard <richard.yang.tong@gmail.com>
2021-05-26 17:33:04 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test.java
@@ -2,6 +2,7 @@ package com.dragontalker.java;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Java9Test {
@@ -9,11 +10,16 @@ public class Java9Test {
     //java9特性五: 钻石操作符的升级
     @Test
     public void test1() {
+        //jdk8要求<>不能为空
+        //钻石操作符与匿名内部类在java 8中不能共存, 在java9可以
         Comparator<Object> com = new Comparator<>() {
             @Override
             public int compare(Object o1, Object o2) {
                 return 0;
             }
         };
+
+        //类型推断: jdk7开始就有了
+        ArrayList<String> list = new ArrayList<>();
     }
 }
