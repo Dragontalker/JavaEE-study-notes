8222eb55e3c995402724a9186f48626c836f380d
Richard <richard.yang.tong@gmail.com>
2021-05-26 18:12:24 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test.java
@@ -61,6 +61,7 @@ public class Java9Test {
 
         //java 9中资源关闭操作
         //需要自动关闭的资源的实例化可以放在try的一对小括号外
+        //此时的资源属性是常量, 不可修改
         InputStreamReader reader = new InputStreamReader(System.in);
         try(reader) {
             char[] cbuf = new char[20];
