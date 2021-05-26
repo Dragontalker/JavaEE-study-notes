857af425e4c91a374234f545684ae531fe320209
Richard <richard.yang.tong@gmail.com>
2021-05-26 18:30:01 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test1.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java/Java9Test1.java
@@ -27,4 +27,12 @@ public class Java9Test1 {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         //list.add(6); 报异常: UnsupportedOperationException
     }
+
+    //java9新特性八: 集合工厂方法: 创建只读集合
+    @Test
+    public void test4() {
+        List<Integer> list1 = List.of(1, 2, 3, 4, 5);
+        //list.add(6);
+        System.out.println(list1);
+    }
 }
