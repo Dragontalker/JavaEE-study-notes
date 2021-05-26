82bd183e8fac08281864cdb5a1f8768e78c5bb1a
Richard <richard.yang.tong@gmail.com>
2021-05-25 21:19:30 -0400

    Added notes.

---
+++ Day_29/Notes/src/com/dragontalker/java3/StreamTest2.java
@@ -0,0 +1,16 @@
+package com.dragontalker.java3;
+
+import org.junit.Test;
+
+/**
+ * 测试Stream的终止操作
+ */
+
+public class StreamTest2 {
+
+    //1 - 匹配与查找
+    @Test
+    public void test1() {
+
+    }
+}
