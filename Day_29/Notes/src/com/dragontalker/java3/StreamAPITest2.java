717e9d270e947bb63e87acf9f9da70c5ba90003b
Richard <richard.yang.tong@gmail.com>
2021-05-25 21:25:27 -0400

    Added notes.

--- Day_29/Notes/src/com/dragontalker/java3/StreamAPITest2.java
+++ Day_29/Notes/src/com/dragontalker/java3/StreamAPITest2.java
@@ -20,5 +20,10 @@ public class StreamAPITest2 {
         //练习1: 是否所有的员工的年龄都大于18
         boolean allMatch = employees.stream().allMatch(e -> e.getAge() > 18);
         System.out.println(allMatch);
+
+        //anyMatch(Predicate p) - 检查是否至少匹配一个元素
+        //练习2: 是否存在员工的工资大于10000
+        boolean anyMatch = employees.stream().anyMatch(e -> e.getSalary() > 10000);
+        System.out.println(anyMatch);
     }
 }
