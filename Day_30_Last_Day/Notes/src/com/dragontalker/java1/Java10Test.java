78f978efa60178c2640718f88e4f19e18c39416e
Richard <richard.yang.tong@gmail.com>
2021-05-26 20:42:46 -0400

    Added notes.

--- Day_30_Last_Day/Notes/src/com/dragontalker/java1/Java10Test.java
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java1/Java10Test.java
@@ -2,6 +2,9 @@ package com.dragontalker.java1;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Java10Test {
     /*
     局部变量的类型推断
@@ -11,7 +14,9 @@ public class Java10Test {
         //声明变量时, 根据所附的值, 推断变量的类型
         var num = 10;
 
-
+        var list = new ArrayList<String>();
+        list.add("Hello!");
+        System.out.println(list);
     }
 
 }
--- Day_30_Last_Day/Notes/src/module-info.java
+++ Day_30_Last_Day/Notes/src/module-info.java
@@ -1,6 +1,7 @@
 module Notes {
 
     exports com.dragontalker.java;
+    exports com.dragontalker.java1;
     requires Java9Test;
     requires junit;
 }
