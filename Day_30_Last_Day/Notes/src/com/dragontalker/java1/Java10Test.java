2a0a6db71611e0f000dfa7f8b4ef0d784e68598e
Richard <richard.yang.tong@gmail.com>
2021-05-26 20:40:51 -0400

    Added notes.

---
+++ Day_30_Last_Day/Notes/src/com/dragontalker/java1/Java10Test.java
@@ -0,0 +1,17 @@
+package com.dragontalker.java1;
+
+import org.junit.Test;
+
+public class Java10Test {
+    /*
+    局部变量的类型推断
+     */
+    @Test
+    public void test1() {
+        //声明变量时, 根据所附的值, 推断变量的类型
+        var num = 10;
+
+
+    }
+
+}
